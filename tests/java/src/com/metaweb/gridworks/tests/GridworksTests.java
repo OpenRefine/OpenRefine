f0ba42f355c8229df4300076926ab9fcfb432831
Stefano Mazzocchi <stefano@apache.org>
2010-04-09 00:23:14 +0000

    make sure to check the tests as well
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@431 7d457c2a-affb-35e4-300a-418c747d4874

--- tests/java/src/com/metaweb/gridworks/tests/GridworksTests.java
+++ tests/java/src/com/metaweb/gridworks/tests/GridworksTests.java
@@ -1,9 +1,14 @@
 package com.metaweb.gridworks.tests;
 
+import static org.junit.Assert.assertSame;
+
 import org.junit.Test;
 
 public class GridworksTests {
 
+    // this is just a placeholder for now
+    
     @Test public void test() {
+        assertSame("Just a placeholder for now", Integer.parseInt("1"), 1);
     }
 }
