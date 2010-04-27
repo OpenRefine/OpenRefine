4f1aaff4869c747e01d1430389a71ae0674680e7
David Huynh <dfhuynh@gmail.com>
2010-04-27 23:21:21 +0000

    Support new fields for Recon objects.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@561 7d457c2a-affb-35e4-300a-418c747d4874

--- src/main/java/com/metaweb/gridworks/model/Recon.java
+++ src/main/java/com/metaweb/gridworks/model/Recon.java
@@ -144,12 +144,18 @@ public class Recon implements HasFields, Jsonizable {
             return candidates != null && candidates.size() > 0 ? candidates.get(0) : null;
         } else if ("judgment".equals(name) || "judgement".equals(name)) {
             return judgmentToString();
+        } else if ("judgmentAction".equals(name) || "judgementAction".equals(name)) {
+            return judgmentAction;
+        } else if ("judgmentBatchSize".equals(name) || "judgementBatchSize".equals(name)) {
+            return judgmentBatchSize;
         } else if ("matched".equals(name)) {
             return judgment == Judgment.Matched;
         } else if ("new".equals(name)) {
             return judgment == Judgment.New;
         } else if ("match".equals(name)) {
             return match;
+        } else if ("matchRank".equals(name)) {
+            return matchRank;
         } else if ("features".equals(name)) {
             return new Features();
         }
