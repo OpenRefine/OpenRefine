7ba97c61ad91c0fe21565b3b81752ec07da4668f
David Huynh <dfhuynh@gmail.com>
2010-06-15 23:21:08 +0000

    More work on the sample extension
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@974 7d457c2a-affb-35e4-300a-418c747d4874

---
+++ extensions/sample-extension/module/MOD-INF/controller.js
@@ -0,0 +1,46 @@
+var html = "text/html";
+var encoding = "UTF-8";
+var ClientSideResourceManager = Packages.com.metaweb.gridworks.ClientSideResourceManager;
+
+/*
+ * Function invoked to initialize the extension.
+ */
+/*
+function init() {
+    # Packages.java.lang.System.err.println("Initializing sample extension");
+    # Packages.java.lang.System.err.println(module.getMountPoint());
+    
+    // Script files to inject into /project page
+    ClientSideResourceManager.addPaths(
+        "project/scripts",
+        module,
+        [
+            "scripts/project-injection.js"
+        ]
+    );
+    
+    // Style files to inject into /project page
+    ClientSideResourceManager.addPaths(
+        "project/styles",
+        module,
+        [
+            "styles/project-injection.less"
+        ]
+    );
+}
+*/
+
+/*
+ * Function invoked to handle each request in a custom way.
+ */
+function process(path, request, response) {
+    // Analyze path and handle this request yourself.
+    
+    if (path == "/" || path == "") {
+        send(request, response, "index.vt", context);
+    }
+}
+
+function send(request, response, template, context) {
+    butterfly.sendTextFromTemplate(request, response, context, template, encoding, html);
+}
---
+++ extensions/sample-extension/module/scripts/project-injection.js
@@ -0,0 +1,3 @@
+// This file is added to the /project page
+
+var SampleExtension = {};
