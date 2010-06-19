4a94c22412394484aace4c4880ae9422ce1dc4d9
Stefano Mazzocchi <stefano@apache.org>
2010-06-19 02:31:32 +0000

    Eclipse was complaining about the @override
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@993 7d457c2a-affb-35e4-300a-418c747d4874

--- extensions/jython/src/com/metaweb/gridworks/jython/JythonEvaluable.java
+++ extensions/jython/src/com/metaweb/gridworks/jython/JythonEvaluable.java
@@ -25,7 +25,6 @@ public class JythonEvaluable implements Evaluable {
     static public LanguageSpecificParser createParser() {
         return new LanguageSpecificParser() {
             
-            @Override
             public Evaluable parse(String s) throws ParsingException {
                 return new JythonEvaluable(s);
             }
