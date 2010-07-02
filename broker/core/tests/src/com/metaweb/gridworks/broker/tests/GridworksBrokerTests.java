27203844b0d6ea6060f591685862e99576eccc23
Stefano Mazzocchi <stefano@apache.org>
2010-07-02 09:30:36 +0000

    adding skeleton for broker tests
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@1066 7d457c2a-affb-35e4-300a-418c747d4874

---
+++ broker/core/tests/src/com/metaweb/gridworks/broker/tests/GridworksBrokerTests.java
@@ -0,0 +1,15 @@
+package com.metaweb.gridworks.broker.tests;
+
+import org.slf4j.Logger;
+import org.testng.annotations.BeforeSuite;
+
+public class GridworksBrokerTests {
+
+    protected Logger logger;
+
+    @BeforeSuite
+    public void init() {
+        //System.setProperty("log4j.configuration", "tests.log4j.properties");
+    }
+            
+}
