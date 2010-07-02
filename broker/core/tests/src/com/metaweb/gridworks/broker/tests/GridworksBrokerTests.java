689d743d0bcad574867ca62a7d632041dbf29c3b
Stefano Mazzocchi <stefano@apache.org>
2010-07-02 09:30:45 +0000

    tests work in eclipse
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@1067 7d457c2a-affb-35e4-300a-418c747d4874

--- broker/core/tests/src/com/metaweb/gridworks/broker/tests/GridworksBrokerTests.java
+++ broker/core/tests/src/com/metaweb/gridworks/broker/tests/GridworksBrokerTests.java
@@ -1,15 +1,84 @@
 package com.metaweb.gridworks.broker.tests;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.metaweb.gridworks.broker.GridworksBroker;
+import com.metaweb.gridworks.broker.GridworksBrokerImpl;
 
 public class GridworksBrokerTests {
 
     protected Logger logger;
 
+    protected File data;
+    
     @BeforeSuite
-    public void init() {
+    public void suite_init() {
         //System.setProperty("log4j.configuration", "tests.log4j.properties");
+        data = new File("data");
+        if (!data.exists()) data.mkdirs();
     }
-            
+
+    @AfterSuite
+    public void suite_destroy() {
+    }
+
+    @BeforeTest
+    public void test_init() {
+        logger = LoggerFactory.getLogger(this.getClass());
+    }
+        
+    // System under test
+    GridworksBroker SUT = null;
+
+    // mocks
+    HttpServletRequest request = null;
+    HttpServletResponse response = null;
+    ServletConfig config = null;
+    
+    @BeforeMethod
+    public void setup() throws Exception {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        config = mock(ServletConfig.class);
+
+        when(config.getInitParameter("gridworks.data")).thenReturn(data.getAbsolutePath());
+
+        SUT = new GridworksBrokerImpl();
+        SUT.init(config);
+    }
+
+    @AfterMethod
+    public void teardown() {
+        SUT = null;
+        
+        request = null;
+        response = null;
+        config = null;
+    }
+    
+    // ------------------------------------------------------------------------------------
+
+    @Test
+    public void testLifeCycle() {
+        Assert.assertTrue(true);
+    }
+
+    
 }
