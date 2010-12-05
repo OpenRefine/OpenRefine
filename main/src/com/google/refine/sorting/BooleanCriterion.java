9aaa1c9919162145e25c9ed748ae3316b4d09938
Tom Morris <tfmorris@gmail.com>
2010-12-05 20:50:03 +0000

    Replace tabs with spaces.  No functional change.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@1947 7d457c2a-affb-35e4-300a-418c747d4874

--- main/src/com/google/refine/sorting/BooleanCriterion.java
+++ main/src/com/google/refine/sorting/BooleanCriterion.java
@@ -37,29 +37,29 @@ import com.google.refine.expr.EvalError;
 import com.google.refine.expr.ExpressionUtils;
 
 public class BooleanCriterion extends Criterion {
-	final static protected EvalError s_error = new EvalError("Not a boolean");
-	
-	@Override
-	public KeyMaker createKeyMaker() {
-		return new KeyMaker() {
-			@Override
-			protected Object makeKey(Object value) {
-				if (ExpressionUtils.isNonBlankData(value)) {
-					if (value instanceof Boolean) {
-						return value;
-					} else if (value instanceof String) {
-						return Boolean.parseBoolean((String) value);
-					} else {
-						return s_error;
-					}
-				}
-				return value;
-			}
-			
-			@Override
-			public int compareKeys(Object key1, Object key2) {
-				return ((Boolean) key1).compareTo((Boolean) key2);
-			}
-		};
-	}
+    final static protected EvalError s_error = new EvalError("Not a boolean");
+
+    @Override
+    public KeyMaker createKeyMaker() {
+        return new KeyMaker() {
+            @Override
+            protected Object makeKey(Object value) {
+                if (ExpressionUtils.isNonBlankData(value)) {
+                    if (value instanceof Boolean) {
+                        return value;
+                    } else if (value instanceof String) {
+                        return Boolean.parseBoolean((String) value);
+                    } else {
+                        return s_error;
+                    }
+                }
+                return value;
+            }
+
+            @Override
+            public int compareKeys(Object key1, Object key2) {
+                return ((Boolean) key1).compareTo((Boolean) key2);
+            }
+        };
+    }
 }
