a81dcc50cc8f677b80987647598158d5fae5c4cf
David Huynh <dfhuynh@gmail.com>
2010-12-28 22:32:21 +0000

    Don't assert type /type/object as the result of any /type/object/* property.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@1969 7d457c2a-affb-35e4-300a-418c747d4874

--- extensions/freebase/src/com/google/refine/freebase/protograph/transpose/TripleLoaderTransposedNodeFactory.java
+++ extensions/freebase/src/com/google/refine/freebase/protograph/transpose/TripleLoaderTransposedNodeFactory.java
@@ -120,13 +120,13 @@ public class TripleLoaderTransposedNodeFactory implements TransposedNodeFactory 
     }
     protected void ensureFromTypesAsserted(Recon recon, String propertyID) {
         String fromTypeID = schemaHelper.getPropertyFromType(propertyID);
-        if (fromTypeID != null) {
+        if (fromTypeID != null && !"/type/object".equals(fromTypeID)) {
             ensureAllIncludedTypesAsserted(recon, fromTypeID);
         }
     }
     protected void ensureToTypesAsserted(Recon recon, String propertyID) {
         String toTypeID = schemaHelper.getPropertyToType(propertyID);
-        if (toTypeID != null) {
+        if (toTypeID != null && !"/type/object".equals(toTypeID)) {
             ensureAllIncludedTypesAsserted(recon, toTypeID);
         }
     }
