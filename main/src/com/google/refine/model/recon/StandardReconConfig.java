73acd497e9d7bf32dbb40fcfbebc09aea02e662a
Tom Morris <tfmorris@gmail.com>
2011-06-06 21:30:46 +0000

    Fix for issue 358 from Tomaz Solc.  Don't return a NaN when comparing two 0-length word lists.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@2088 7d457c2a-affb-35e4-300a-418c747d4874

--- main/src/com/google/refine/model/recon/StandardReconConfig.java
+++ main/src/com/google/refine/model/recon/StandardReconConfig.java
@@ -429,6 +429,10 @@ public class StandardReconConfig extends ReconConfig {
     }
     
     static protected double wordDistance(Set<String> longWords, Set<String> shortWords) {
+        if (longWords.size() == 0) {
+            return 0.0;
+        }
+
         double common = 0;
         for (String word : shortWords) {
             if (longWords.contains(word)) {
