94e0369af74778eba0132d7ddaa47ae4ef879ada
David Huynh <dfhuynh@gmail.com>
2011-11-07 07:27:35 +0000

    Added extension for importing PC-Axis files.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@2365 7d457c2a-affb-35e4-300a-418c747d4874

---
+++ extensions/pc-axis/module/scripts/pc-axis-parser-ui.js
@@ -0,0 +1,158 @@
+/*
+
+Copyright 2011, Google Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+ * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+ * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,           
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+Refine.PCAxisParserUI = function(controller, jobID, job, format, config,
+    dataContainerElmt, progressContainerElmt, optionContainerElmt) {
+
+  this._controller = controller;
+  this._jobID = jobID;
+  this._job = job;
+  this._format = format;
+  this._config = config;
+
+  this._dataContainer = dataContainerElmt;
+  this._progressContainer = progressContainerElmt;
+  this._optionContainer = optionContainerElmt;
+
+  this._timerID = null;
+  this._initialize();
+  this._updatePreview();
+};
+Refine.DefaultImportingController.parserUIs.PCAxisParserUI = Refine.PCAxisParserUI;
+
+Refine.PCAxisParserUI.prototype.confirmReadyToCreateProject = function() {
+  return true;
+};
+
+Refine.PCAxisParserUI.prototype.dispose = function() {
+  if (this._timerID !== null) {
+    window.clearTimeout(this._timerID);
+    this._timerID = null;
+  }
+};
+
+Refine.PCAxisParserUI.prototype.getOptions = function() {
+  var options = {
+    encoding: $.trim(this._optionContainerElmts.encodingInput[0].value)
+  };
+
+  var parseIntDefault = function(s, def) {
+    try {
+      var n = parseInt(s,10);
+      if (!isNaN(n)) {
+        return n;
+      }
+    } catch (e) {
+      // Ignore
+    }
+    return def;
+  };
+
+  if (this._optionContainerElmts.limitCheckbox[0].checked) {
+    options.limit = parseIntDefault(this._optionContainerElmts.limitInput[0].value, -1);
+  } else {
+    options.limit = -1;
+  }
+  if (this._optionContainerElmts.skipCheckbox[0].checked) {
+    options.skipDataLines = parseIntDefault(this._optionContainerElmts.skipInput[0].value, -1);
+  } else {
+    options.skipDataLines = -1;
+  }
+  options.includeFileSources = this._optionContainerElmts.includeFileSourcesCheckbox[0].checked;
+
+  return options;
+};
+
+Refine.PCAxisParserUI.prototype._initialize = function() {
+  var self = this;
+
+  this._optionContainer.unbind().empty().html(
+      DOM.loadHTML("pc-axis", "scripts/pc-axis-parser-ui.html"));
+  this._optionContainerElmts = DOM.bind(this._optionContainer);
+  this._optionContainerElmts.previewButton.click(function() { self._updatePreview(); });
+
+  this._optionContainerElmts.encodingInput
+    .attr('value', this._config.encoding || '')
+    .click(function() {
+      Encoding.selectEncoding($(this), function() {
+        self._updatePreview();
+      });
+    });
+
+  if (this._config.limit > 0) {
+    this._optionContainerElmts.limitCheckbox.attr("checked", "checked");
+    this._optionContainerElmts.limitInput[0].value = this._config.limit.toString();
+  }
+  if (this._config.skipDataLines > 0) {
+    this._optionContainerElmts.skipCheckbox.attr("checked", "checked");
+    this._optionContainerElmts.skipInput.value[0].value = this._config.skipDataLines.toString();
+  }
+  if (this._config.includeFileSources) {
+    this._optionContainerElmts.includeFileSourcesCheckbox.attr("checked", "checked");
+  }
+
+  var onChange = function() {
+    self._scheduleUpdatePreview();
+  };
+  this._optionContainer.find("input").bind("change", onChange);
+  this._optionContainer.find("select").bind("change", onChange);
+};
+
+Refine.PCAxisParserUI.prototype._scheduleUpdatePreview = function() {
+  if (this._timerID !== null) {
+    window.clearTimeout(this._timerID);
+    this._timerID = null;
+  }
+
+  var self = this;
+  this._timerID = window.setTimeout(function() {
+    self._timerID = null;
+    self._updatePreview();
+  }, 500); // 0.5 second
+};
+
+Refine.PCAxisParserUI.prototype._updatePreview = function() {
+  var self = this;
+
+  this._progressContainer.show();
+
+  this._controller.updateFormatAndOptions(this.getOptions(), function(result) {
+    if (result.status == "ok") {
+      self._controller.getPreviewData(function(projectData) {
+        self._progressContainer.hide();
+
+        new Refine.PreviewTable(projectData, self._dataContainer.unbind().empty());
+      });
+    }
+  });
+};
