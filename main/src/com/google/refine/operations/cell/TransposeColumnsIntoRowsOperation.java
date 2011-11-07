dbeaefb00b4ac9160bdf2c16d86acfc3ac027887
David Huynh <dfhuynh@gmail.com>
2011-11-07 19:53:26 +0000

    Minor bug fix to previous check-in: made sure blank cells in the 2 newly generated columns don't get filled in.
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@2368 7d457c2a-affb-35e4-300a-418c747d4874

--- main/src/com/google/refine/operations/cell/TransposeColumnsIntoRowsOperation.java
+++ main/src/com/google/refine/operations/cell/TransposeColumnsIntoRowsOperation.java
@@ -339,12 +339,17 @@ public class TransposeColumnsIntoRowsOperation extends AbstractOperation {
                 for (int r2 = firstNewRowIndex + 1; r2 < newRows.size(); r2++) {
                     Row newRow = newRows.get(r2);
                     for (int c = 0; c < newColumns.size(); c++) {
-                        Column column = newColumns.get(c);
-                        int cellIndex = column.getCellIndex();
-                        
-                        Cell cellToCopy = firstNewRow.getCell(cellIndex);
-                        if (cellToCopy != null && newRow.getCell(cellIndex) == null) {
-                            newRow.setCell(cellIndex, cellToCopy);
+                        if (c < startColumnIndex ||
+                            (_combinedColumnName != null ?
+                                c > startColumnIndex :
+                                c > startColumnIndex + 1)) {
+                            Column column = newColumns.get(c);
+                            int cellIndex = column.getCellIndex();
+                            
+                            Cell cellToCopy = firstNewRow.getCell(cellIndex);
+                            if (cellToCopy != null && newRow.getCell(cellIndex) == null) {
+                                newRow.setCell(cellIndex, cellToCopy);
+                            }
                         }
                     }
                 }
