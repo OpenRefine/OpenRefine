b409ef5670e961e793694c664f1d85943c776c47
Tom Morris <tfmorris@gmail.com>
2011-12-09 23:50:40 +0000

    Issue 491 - fix off-by-one error in column counts
    git-svn-id: http://google-refine.googlecode.com/svn/trunk@2405 7d457c2a-affb-35e4-300a-418c747d4874

--- main/src/com/google/refine/importers/ExcelImporter.java
+++ main/src/com/google/refine/importers/ExcelImporter.java
@@ -174,7 +174,7 @@ public class ExcelImporter extends TabularImportingParserBase {
                     org.apache.poi.ss.usermodel.Row row = sheet.getRow(nextRow++);
                     if (row != null) {
                         short lastCell = row.getLastCellNum();
-                        for (short cellIndex = 0; cellIndex <= lastCell; cellIndex++) {
+                        for (short cellIndex = 0; cellIndex < lastCell; cellIndex++) {
                             Cell cell = null;
                             
                             org.apache.poi.ss.usermodel.Cell sourceCell = row.getCell(cellIndex);
@@ -219,6 +219,13 @@ public class ExcelImporter extends TabularImportingParserBase {
             
             if (HSSFDateUtil.isCellDateFormatted(cell)) {
                 value = HSSFDateUtil.getJavaDate(d);
+                // TODO: If we had a time datatype, we could use something like the following
+                // to distinguish times from dates (although Excel doesn't really make the distinction)
+                // Another alternative would be to look for values < 0.60
+//                String format = cell.getCellStyle().getDataFormatString();
+//                if (!format.contains("d") && !format.contains("m") && !format.contains("y") ) {
+//                    // It's just a time
+//                }
             } else {
                 value = d;
             }
