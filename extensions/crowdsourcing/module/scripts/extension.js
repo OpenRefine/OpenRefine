7b3d09e5b3729610733a625c8880b66d9878d1ed
sparkica <mateja.verlic@zemanta.com>
2012-10-22 14:18:58 +0200

    Adding crowdsourcing extension

---
+++ extensions/crowdsourcing/module/scripts/extension.js
@@ -0,0 +1,76 @@
+var ZemantaExtension = {handlers: {}, util: {}};
+
+
+ZemantaExtension.util.parseCrowdFlowerApiKey = function (prefs) {
+	var apiKey = "";
+	if(prefs != null) {
+		$.each(prefs, function(key, val) {
+			if(key === "crowdflower-api-key") {
+				apiKey = val;
+			}
+		});
+	}
+	return apiKey;
+};
+
+ZemantaExtension.util.loadCrowdFlowerApiKeyFromSettings = function(getCrowdFlowerApiKey) {
+	$.post(
+		      "/command/core/get-all-preferences",
+		      {},
+		      function (data) {
+		    	  getCrowdFlowerApiKey(ZemantaExtension.util.parseCrowdFlowerApiKey(data));
+		    	  },
+		      "json"
+	 );
+	
+};
+
+ZemantaExtension.handlers.storeCrowdFlowerAPIKey = function() {
+	
+	new ZemantaSettingsDialog(function(newApiKey) {
+		$.post(
+	          "/command/core/set-preference",
+	          {
+	            name : "crowdflower-api-key",
+	            value : JSON.stringify(newApiKey)
+	          },
+	          function(o) {
+	            if (o.code == "error") {
+	              alert(o.message);
+	            }
+	          },
+	          "json"
+		);
+	});
+};
+
+ZemantaExtension.handlers.doNothing = function() {
+	alert("Crowdsourcing extension active...");
+};
+
+ZemantaExtension.handlers.openPreferences = function() {
+	alert("Open preferences?...");
+	window.location = "/preferences";
+};
+
+ExtensionBar.addExtensionMenu({
+	"id": "zemanta",
+	"label": "Zemanta",
+	"submenu": [
+   		 {
+			 "id": "zemanta/openrefine-settings",
+			 label: "OpenRefine settings",
+			 click: ZemantaExtension.handlers.openPreferences
+		 },
+		 {
+			 "id": "zemanta/crowdflower-settings",
+			 label: "CrowdFlower settings",
+			 click: ZemantaExtension.handlers.storeCrowdFlowerAPIKey
+		 },
+		 {
+			 "id": "zemanta/az-mechturk-settings",
+			 label: "Amazon Mechanical Turk settings",
+			 click: ZemantaExtension.handlers.doNothing
+		 }
+		]
+});
---
+++ extensions/crowdsourcing/src/com/google/refine/com/zemanta/crowdsourcing/CrowdFlowerClient.java
@@ -0,0 +1,8 @@
+package com.google.refine.com.zemanta.crowdsourcing;
+
+
+public class CrowdFlowerClient {
+    static public int stringArrayLength(String[] a) {
+        return a.length;
+    }
+}
