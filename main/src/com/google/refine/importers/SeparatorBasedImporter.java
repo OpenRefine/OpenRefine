579d71b7eb0162859034601da5a81dfab23aba9a
Tom Morris <tfmorris@gmail.com>
2013-08-07 17:07:17 -0400

    Switch back to NUL character for quote now that OpenCSV handles it - fixes #653

--- main/src/com/google/refine/importers/SeparatorBasedImporter.java
+++ main/src/com/google/refine/importers/SeparatorBasedImporter.java
@@ -101,7 +101,7 @@ public class SeparatorBasedImporter extends TabularImportingParserBase {
         final CSVParser parser = new CSVParser(
             sep.toCharArray()[0],//HACK changing string to char - won't work for multi-char separators.
             CSVParser.DEFAULT_QUOTE_CHARACTER,
-            (char) 127, // we don't want escape processing try DEL as a rare character until we can turn it off
+            (char) 0, // we don't want escape processing
             strictQuotes,
             CSVParser.DEFAULT_IGNORE_LEADING_WHITESPACE,
             !processQuotes);
