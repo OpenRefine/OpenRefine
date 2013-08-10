4fd20f1b4eb75bc71a77878a78edec8d8b3311d4
Tom Morris <tfmorris@gmail.com>
2013-08-10 17:56:09 -0400

    Update to Suggest 4.3 - fixes #762

--- main/webapp/modules/core/MOD-INF/controller.js
+++ main/webapp/modules/core/MOD-INF/controller.js
@@ -384,7 +384,7 @@ function init() {
       "externals/jquery-1.7.2.min.js",
       "externals/jquery.cookie.js",
       "externals/jquery.eventstack-0.3.js",
-      "externals/suggest/suggest-4_2.js",
+      "externals/suggest/suggest-4_3.min.js",
       "externals/jquery-ui/jquery-ui-1.8.20.custom.min.js",
       "externals/imgareaselect/jquery.imgareaselect.js",
       "externals/date.js",
@@ -450,7 +450,7 @@ function init() {
     "project/styles",
     module,
     [
-      "externals/suggest/css/suggest-4_2.css",
+      "externals/suggest/css/suggest-4_3.min.css",
       "externals/jquery-ui/css/ui-lightness/jquery-ui-1.8.20.custom.css",
       "externals/imgareaselect/css/imgareaselect-default.css",
 
@@ -490,7 +490,7 @@ function init() {
     [
       "externals/jquery-1.7.2.min.js",
       "externals/jquery.cookie.js",
-      "externals/suggest/suggest-4_2.js",
+      "externals/suggest/suggest-4_3.min.js",
       "externals/jquery-ui/jquery-ui-1.8.20.custom.min.js",
       "externals/imgareaselect/jquery.imgareaselect.js",
       "externals/date.js",
@@ -502,7 +502,7 @@ function init() {
     "preferences/styles",
     module,
     [
-      "externals/suggest/css/suggest-4_2.css",
+      "externals/suggest/css/suggest-4_3.min.css",
       "externals/jquery-ui/css/ui-lightness/jquery-ui-1.8.20.custom.css",
       "styles/jquery-ui-overrides.less",
       "styles/common.less",
