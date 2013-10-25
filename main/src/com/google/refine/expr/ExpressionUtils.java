6babd3128145778e283e2d866f8b994d744aac0d
PointyShinyBurning <PointyShinyBurning@gmail.com>
2013-10-25 17:35:04 +0100

    Text transform can set "" on null cells and vice versa.
    Previously if a transformation expression resulted in "" or null and the
    cell's previous value was "" or null then nothing would be changed.
    
    Partial fix for #820 ( 1 on his list) that opens smallest possible can
    of worms re blank/null handling. May break backwards compatibility on
    projects that relied on old behaviour, though I think it unlikely anyone
    has such a thing.

--- main/src/com/google/refine/expr/ExpressionUtils.java
+++ main/src/com/google/refine/expr/ExpressionUtils.java
@@ -124,12 +124,8 @@ public class ExpressionUtils {
     }
 
     static public boolean sameValue(Object v1, Object v2) {
-        if (v1 == null) {
-            return (v2 == null)
-                    || (v2 instanceof String && ((String) v2).length() == 0);
-        } else if (v2 == null) {
-            return (v1 == null)
-                    || (v1 instanceof String && ((String) v1).length() == 0);
+        if (v1 == null || v2 == null) {
+            return (v1 == v2);
         } else {
             return v1.equals(v2);
         }
