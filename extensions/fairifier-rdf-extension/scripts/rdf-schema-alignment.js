5d7507440b4b1cdca50145d54fb5f9888e5260ee
shamanou <mandarijnopw8@gmail.com>
2017-03-04 13:55:57 +0100

    minor bug fixes

--- extensions/fairifier-rdf-extension/scripts/rdf-schema-alignment.js
+++ extensions/fairifier-rdf-extension/scripts/rdf-schema-alignment.js
@@ -18,7 +18,7 @@ function RdfSchemaAlignmentDialog(schema){
 	//initialize vocabularyManager
     this._prefixesManager = new RdfPrefixesManager(this,this._schema.prefixes);
     
-    this._replaceBaseUri(RdfSchemaAlignment._defaultNamespace || URL.getHostname()+'/',true);
+    this._replaceBaseUri(RdfSchemaAlignment._defaultNamespace ||window.location.protocol + window.location.host + "/",true);
 };
 
 RdfSchemaAlignmentDialog.prototype._createDialog = function() {
