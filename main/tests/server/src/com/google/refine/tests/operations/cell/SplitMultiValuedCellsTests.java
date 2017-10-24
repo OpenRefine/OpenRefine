219e747147914fd8b42d6993f58642f92a75b382
Owen Stephens <owen@ostephens.com>
2017-10-24 08:54:58 +0100

    Removed unused dependency

--- main/tests/server/src/com/google/refine/tests/operations/cell/SplitMultiValuedCellsTests.java
+++ main/tests/server/src/com/google/refine/tests/operations/cell/SplitMultiValuedCellsTests.java
@@ -63,7 +63,6 @@ import com.google.refine.model.ModelException;
 import com.google.refine.model.Project;
 import com.google.refine.process.Process;
 import com.google.refine.operations.cell.MultiValuedCellSplitOperation;
-import com.google.refine.util.JSONUtilities;
 import com.google.refine.tests.RefineServletStub;
 import com.google.refine.tests.RefineTest;
 import com.google.refine.tests.util.TestUtils;
