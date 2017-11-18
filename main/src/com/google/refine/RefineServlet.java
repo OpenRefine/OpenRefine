ae9e0af8b9503f3c2c818f8676e0202189b357c6
Qi Cui <jackyq2015@gmail.com>
2017-11-18 14:15:10 -0500

    bump to version 2.8

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -60,7 +60,7 @@ import edu.mit.simile.butterfly.Butterfly;
 import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
-    static private String ASSIGNED_VERSION = "2.7";
+    static private String ASSIGNED_VERSION = "2.8";
     
     static public String VERSION = "";
     static public String REVISION = "";
