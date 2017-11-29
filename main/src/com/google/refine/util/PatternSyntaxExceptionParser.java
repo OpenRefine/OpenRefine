0b433fc5eced4e8f2315a05ec0a85c215517be07
Owen Stephens <owen@ostephens.com>
2017-11-29 00:59:49 +0000

    Updated copyright statement

--- main/src/com/google/refine/util/PatternSyntaxExceptionParser.java
+++ main/src/com/google/refine/util/PatternSyntaxExceptionParser.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2010, Google Inc.
+Copyright 2017, Owen Stephens.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -13,7 +13,7 @@ notice, this list of conditions and the following disclaimer.
 copyright notice, this list of conditions and the following disclaimer
 in the documentation and/or other materials provided with the
 distribution.
-    * Neither the name of Google Inc. nor the names of its
+    * Neither the name of the copyright holder nor the names of its
 contributors may be used to endorse or promote products derived from
 this software without specific prior written permission.
 
