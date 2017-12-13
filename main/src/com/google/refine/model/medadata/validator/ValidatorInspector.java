5a91db4c20528000fea25f6da0ea59b25b9225d6
Jacky <jackyq2015@gmail.com>
2017-12-12 19:54:03 -0500

    rename the UT

--- main/src/com/google/refine/model/medadata/validator/ValidatorInspector.java
+++ main/src/com/google/refine/model/medadata/validator/ValidatorInspector.java
@@ -53,9 +53,12 @@ public class ValidatorInspector {
         
         // do the inspect in another loop:
         for(String columnName : columnNames) {
-            for (Validator validator : columnToCheckersMap.get(columnName)) {
-                JSONArray result = validator.validate();
-                JSONUtilities.concatArray(validateReport, result);
+            List<Validator> validators = columnToCheckersMap.get(columnName);
+            if (validators != null) {
+                for (Validator validator : validators) {
+                    JSONArray result = validator.validate();
+                    JSONUtilities.concatArray(validateReport, result);
+                }
             }
         }
         logger.info("Inspector finished the validation.");
@@ -78,16 +81,18 @@ public class ValidatorInspector {
         int columnIndex = project.columnModel.getColumnIndexByName(columnName);
         
         validatorList.add(new TypeorFormatError(project, columnIndex, options));
-        for (Entry<String, Object> entry : field.getConstraints().entrySet()) {
-            Class<Validator> clazz = constraintHandlersMap.get(entry.getKey());
-            try {
-                options.put(CONSTRAINT_KEY, new JSONObject().put(entry.getKey(), entry.getValue()));
-                Constructor<Validator> c = clazz.getConstructor(Project.class, String.class, JSONObject.class);
-                validatorList.add(c.newInstance(project, columnIndex, options));
-            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-                    | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-                logger.error("failed to do compileChecks:" + ExceptionUtils.getStackTrace(e));
-                result = false;
+        if (field.getConstraints() != null) {
+            for (Entry<String, Object> entry : field.getConstraints().entrySet()) {
+                Class<Validator> clazz = constraintHandlersMap.get(entry.getKey());
+                try {
+                    options.put(CONSTRAINT_KEY, new JSONObject().put(entry.getKey(), entry.getValue()));
+                    Constructor<Validator> c = clazz.getConstructor(Project.class, String.class, JSONObject.class);
+                    validatorList.add(c.newInstance(project, columnIndex, options));
+                } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                        | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                    logger.error("failed to do compileChecks:" + ExceptionUtils.getStackTrace(e));
+                    result = false;
+                }
             }
         }
         
--- main/tests/server/src/com/google/refine/tests/commands/project/ValidateOperationTests.java
+++ main/tests/server/src/com/google/refine/tests/commands/project/ValidateOperationTests.java
@@ -1,10 +1,6 @@
 package com.google.refine.tests.commands.project;
 
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -12,10 +8,6 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.everit.json.schema.ValidationException;
 import org.json.JSONException;
@@ -25,7 +17,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -34,42 +25,28 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
 import com.google.refine.ProjectManager;
-import com.google.refine.commands.project.ValidateSchemaCommand;
 import com.google.refine.importers.SeparatorBasedImporter;
-import com.google.refine.importing.ImportingManager;
-import com.google.refine.model.Project;
 import com.google.refine.model.medadata.DataPackageMetadata;
 import com.google.refine.model.medadata.MetadataFactory;
 import com.google.refine.model.medadata.MetadataFormat;
-import com.google.refine.model.medadata.ProjectMetadata;
 import com.google.refine.model.medadata.validator.ValidateOperation;
-import com.google.refine.tests.RefineServletStub;
 import com.google.refine.tests.importers.TsvCsvImporterTests;
 
-import io.frictionlessdata.tableschema.Schema;
 import io.frictionlessdata.tableschema.exceptions.ForeignKeyException;
 import io.frictionlessdata.tableschema.exceptions.PrimaryKeyException;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(MetadataFactory.class)
-public class ValidateSchemaCommandTests extends TsvCsvImporterTests  {
+public class ValidateOperationTests extends TsvCsvImporterTests  {
     
-    Logger logger = LoggerFactory.getLogger(ValidateSchemaCommandTests.class.getClass());
-    
-    // System Under Test
-    ValidateSchemaCommand SUT = null;
+    Logger logger = LoggerFactory.getLogger(ValidateOperationTests.class.getClass());
     
     SeparatorBasedImporter parser = null;
 
     // variables
-    long PROJECT_ID_LONG = 1234;
-    String PROJECT_ID = "1234";
     private DataPackageMetadata dataPackageMetadata;
-//    private ProjectMetadata projectMetadata;
 
     // mocks
-    HttpServletRequest request = null;
-    HttpServletResponse response = null;
     ProjectManager projMan = null;
     PrintWriter pw = null;
     
@@ -78,57 +55,22 @@ public class ValidateSchemaCommandTests extends TsvCsvImporterTests  {
     
     @Before
     public void SetUp() throws JSONException, IOException, ValidationException, PrimaryKeyException, ForeignKeyException {
-        
-        /** 
-        servlet = new RefineServletStub();
-        ImportingManager.initialize(servlet);
-        project = new Project();
-        metadata = new ProjectMetadata();
-        job = ImportingManager.createJob();
-        
-        options = Mockito.mock(JSONObject.class);
-        */
         super.setUp();
-        
         
         // mockup
         projMan = mock(ProjectManager.class);
         ProjectManager.singleton = projMan;
         
-        // reseat project object
-//        projectMock = mock(Project.class);
-        
         pw = mock(PrintWriter.class);
 
-        request = mock(HttpServletRequest.class);
-        response = mock(HttpServletResponse.class);
-        SUT = new ValidateSchemaCommand();
-        
         dataPackageMetadata = new DataPackageMetadata();
         String content = getJSONContent("datapackage-sample.json");
         dataPackageMetadata.loadFromStream(new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8.name())));
         
         // mock dependencies
-//        when(request.getParameter("project")).thenReturn(PROJECT_ID);
-        String options = "{\"columnNames\": [\"Country Name\",\"Country Code\",\"Year\",\"Value\"]}";
-        when(request.getParameter("options")).thenReturn(options);
-        
-//        when(projMan.getProject(anyLong())).thenReturn(projectMock);
-        
-        // below two should return the same object
-/*        when(projectMock.getMetadata(MetadataFormat.DATAPACKAGE_METADATA)).thenReturn(dataPackageMetadata);
-        when(projectMock.getDataPackageMetadata()).thenReturn(dataPackageMetadata);
-        when(projectMock.getSchema()).thenReturn(
-                new Schema(dataPackageMetadata.getPackage().getResources().get(0).getSchema(), true));*/
         //given
         PowerMockito.mockStatic(MetadataFactory.class);
         BDDMockito.given(MetadataFactory.buildMetadata(MetadataFormat.DATAPACKAGE_METADATA)).willReturn(dataPackageMetadata);
-        
-        try {
-            when(response.getWriter()).thenReturn(pw);
-        } catch (IOException e1) {
-            Assert.fail();
-        }
         
         parser = new SeparatorBasedImporter();
         readData();
@@ -136,14 +78,10 @@ public class ValidateSchemaCommandTests extends TsvCsvImporterTests  {
 
     @After
     public void TearDown() {
-        SUT = null;
-
         projMan = null;
         ProjectManager.singleton = null;
         project = null;
         pw = null;
-        request = null;
-        response = null;
     }
 
     /**
@@ -151,29 +89,13 @@ public class ValidateSchemaCommandTests extends TsvCsvImporterTests  {
      * @throws IOException 
      */
     @Test
-    public void setStartProcess()  {
+    public void setStartProcessTest()  {
         // run
         String options = "{\"columnNames\": [\"Country Name\",\"Country Code\",\"Year\",\"Value\"]}";
         JSONObject optionObj = new JSONObject(options);
-        new ValidateOperation(project, optionObj).startProcess();
-        /**
-        try {
-            SUT.doPost(request, response);
-        } catch (ServletException e) {
-            Assert.fail();
-        } catch (IOException e) {
-            Assert.fail();
-        }
-
-        // verify
-        try {
-            verify(response, times(1)).getWriter();
-        } catch (IOException e) {
-            Assert.fail();
-        }
-        verify(pw, times(1)).write("{ \"code\" : \"ok\" }");
-        */
         
+        // SUT
+        new ValidateOperation(project, optionObj).startProcess();
     }
     
      
@@ -191,26 +113,13 @@ public class ValidateSchemaCommandTests extends TsvCsvImporterTests  {
          //create input to test with
          String input =  getFileContent("gdp.csv");
          
-         prepareOptions(sep, -1, 0, 0, 2, false, false);
+         prepareOptions(sep, -1, 0, 0, 1, false, false);
          parseOneFile(parser, new StringReader(input));
 
          setDataPackageMetaData();
          
          Assert.assertEquals(project.columnModel.columns.size(), 4);
-//         Assert.assertNotNull(project.getProjectMetadata());
          Assert.assertNotNull(project.getSchema());
-         /**
-         Assert.assertEquals(project.columnModel.columns.get(0).getName(), "col1 sub1");
-         Assert.assertEquals(project.columnModel.columns.get(1).getName(), "col2 sub2");
-         Assert.assertEquals(project.columnModel.columns.get(2).getName(), "col3 sub3");
-         Assert.assertEquals(project.rows.size(), 1);
-         Assert.assertEquals(project.rows.get(0).cells.size(), 3);
-         Assert.assertEquals(project.rows.get(0).cells.get(0).value, "data1");
-         Assert.assertEquals(project.rows.get(0).cells.get(1).value, "data2");
-         Assert.assertEquals(project.rows.get(0).cells.get(2).value, "data3");
-         */
-         
-//         when(SUT.getProject(request)).thenReturn((project);
      }
      
      private void setDataPackageMetaData() {
