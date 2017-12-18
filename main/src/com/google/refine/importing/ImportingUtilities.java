278f24c1471dd8902f565af184128cf4356ae450
Tiago Fernandez <tiago.fernandez@gmail.com>
2017-12-18 11:38:07 +0100

    Append options metadata while creating project

--- main/src/com/google/refine/importing/ImportingUtilities.java
+++ main/src/com/google/refine/importing/ImportingUtilities.java
@@ -1008,6 +1008,8 @@ public class ImportingUtilities {
         ProjectMetadata pm = new ProjectMetadata();
         pm.setName(JSONUtilities.getString(optionObj, "projectName", "Untitled"));
         pm.setTags(JSONUtilities.getStringArray(optionObj, "projectTags"));
+        pm.appendImportOptionMetadata(optionObj);
+
         String encoding = JSONUtilities.getString(optionObj, "encoding", "UTF-8");
         if ("".equals(encoding)) {
             // encoding can be present, but empty, which won't trigger JSONUtilities default processing
