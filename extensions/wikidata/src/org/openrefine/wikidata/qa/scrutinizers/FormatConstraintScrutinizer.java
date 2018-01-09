4021fb21ed8716ef8ef57d066eef49f92a457f72
Antonin Delpeuch <antonin@delpeuch.eu>
2018-01-09 10:21:50 +0000

    Add format scrutinizer

---
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/FormatConstraintScrutinizer.java
@@ -0,0 +1,60 @@
+package org.openrefine.wikidata.qa.scrutinizers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.openrefine.wikidata.qa.ConstraintFetcher;
+import org.wikidata.wdtk.datamodel.interfaces.Snak;
+import org.wikidata.wdtk.datamodel.interfaces.StringValue;
+
+
+public class FormatConstraintScrutinizer extends SnakScrutinizer {
+    
+    private Map<String, Pattern> _patterns;
+    private ConstraintFetcher _fetcher;
+    
+    public FormatConstraintScrutinizer() {
+        _patterns = new HashMap<>();
+        _fetcher = new ConstraintFetcher();
+    }
+    
+    /**
+     * Loads the regex for a property and compiles it to a pattern
+     * (this is cached upstream, plus we are doing it only once per
+     * property and batch).
+     * @param pid the id of the property to fetch the constraints for
+     * @return
+     */
+    protected Pattern getPattern(String pid) {
+        if(_patterns.containsKey(pid)) {
+            return _patterns.get(pid);
+        } else {
+            String regex = _fetcher.getFormatRegex(pid);
+            Pattern pattern = null;
+            if (regex != null) {
+                pattern = Pattern.compile(regex);
+            }
+            _patterns.put(pid, pattern);
+            return pattern;
+        }
+    }
+
+    @Override
+    public void scrutinize(Snak snak, boolean added) {
+        if(StringValue.class.isInstance(snak.getValue())) {
+            String value = ((StringValue) snak.getValue()).getString();
+            String pid = snak.getPropertyId().getId();
+            Pattern pattern = getPattern(pid);
+            if (!pattern.matcher(value).matches()) {
+                if (added) {
+                    important("add-statements-with-invalid-format");
+                } else {
+                    info("remove-statements-with-invalid-format");
+                }
+            }
+        }
+
+    }
+
+}
