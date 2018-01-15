3c25647f050fe54e97fe3fb12492e682197089af
Jacky <jackyq2015@gmail.com>
2018-01-14 22:04:26 -0500

    add find function. issue 1296

---
+++ main/src/com/google/refine/expr/functions/strings/Find.java
@@ -0,0 +1,85 @@
+/*
+
+Copyright 2010, Google Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+    * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,           
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package com.google.refine.expr.functions.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONWriter;
+
+import com.google.refine.expr.EvalError;
+import com.google.refine.grel.ControlFunctionRegistry;
+import com.google.refine.grel.Function;
+
+public class Find implements Function {
+
+    @Override
+    public Object call(Properties bindings, Object[] args) {
+        List<String> allMatches = new ArrayList<String>();
+        
+        if (args.length == 2) {
+            Object s = args[0];
+            Object p = args[1];
+            
+            if (s != null && p != null && (p instanceof String || p instanceof Pattern)) {
+                
+                Pattern pattern = (p instanceof String) ? Pattern.compile((String) p) : (Pattern) p;
+
+                Matcher matcher = pattern.matcher(s.toString());
+                
+                while (matcher.find()) {
+                    allMatches.add(matcher.group());
+                } 
+            }
+            
+            return allMatches.toArray(new String[0]);
+        }
+        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects a string or a regexp");
+    }
+    
+    @Override
+    public void write(JSONWriter writer, Properties options)
+        throws JSONException {
+    
+        writer.object();
+        writer.key("description"); writer.value("Returns an array of the groups matching the given regular expression");
+        writer.key("params"); writer.value("string or regexp");
+        writer.key("returns"); writer.value("array of strings");
+        writer.endObject();
+    }
+}
