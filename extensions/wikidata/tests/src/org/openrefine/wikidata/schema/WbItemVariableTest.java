75b1863c1cc423167a1e0b2bb5282e3ab110b8e7
Antonin Delpeuch <antonin@delpeuch.eu>
2018-02-01 08:58:30 +0000

    Testing for variables which generate wikibase values

---
+++ extensions/wikidata/tests/src/org/openrefine/wikidata/schema/WbItemVariableTest.java
@@ -0,0 +1,51 @@
+package org.openrefine.wikidata.schema;
+
+import java.util.Collections;
+
+import org.openrefine.wikidata.schema.entityvalues.ReconItemIdValue;
+import org.testng.annotations.Test;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
+
+import com.google.refine.model.Cell;
+import com.google.refine.model.Recon;
+import com.google.refine.model.ReconCandidate;
+
+public class WbItemVariableTest extends WbVariableTest<ItemIdValue> {
+
+    @Override
+    public WbVariableExpr<ItemIdValue> initVariableExpr() {
+        return new WbItemVariable();
+    }
+    
+    @Test
+    public void testReconciledCell() {
+        Recon recon = Recon.makeWikidataRecon(3782378L);
+        recon.judgment = Recon.Judgment.Matched;
+        recon.match = new ReconCandidate("Q123", "some item", null, 100.0);
+        Cell cell = new Cell("some value", recon);
+        evaluatesTo(new ReconItemIdValue(recon, "some value"), cell);
+    }
+    
+    @Test
+    public void testNewItemCell() {
+        Recon recon = Recon.makeWikidataRecon(3782378L);
+        recon.judgment = Recon.Judgment.New;
+        recon.candidates = Collections.singletonList(new ReconCandidate("Q123", "some item", null, 100.0));
+        Cell cell = new Cell("some value", recon);
+        evaluatesTo(new ReconItemIdValue(recon, "some value"), cell);
+    }
+    
+    @Test
+    public void testUnmatchedCell() {
+        Recon recon = Recon.makeWikidataRecon(3782378L);
+        recon.judgment = Recon.Judgment.None;
+        recon.candidates = Collections.singletonList(new ReconCandidate("Q123", "some item", null, 100.0));
+        Cell cell = new Cell("some value", recon);
+        isSkipped(cell);
+    }
+    
+    @Test
+    public void testUnreconciledCell() {
+        isSkipped("some value");
+    }
+}
