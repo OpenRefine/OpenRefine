290f3d194bb01ba3673f6883d006eb4f4707f690
Antonin Delpeuch <antonin@delpeuch.eu>
2018-02-01 09:03:12 +0000

    Check for language code validity in WbLanguageVariable

--- extensions/wikidata/src/org/openrefine/wikidata/schema/WbLanguageVariable.java
+++ extensions/wikidata/src/org/openrefine/wikidata/schema/WbLanguageVariable.java
@@ -1,6 +1,7 @@
 package org.openrefine.wikidata.schema;
 
 import org.openrefine.wikidata.schema.exceptions.SkipSchemaExpressionException;
+import org.wikidata.wdtk.datamodel.interfaces.WikimediaLanguageCodes;
 
 import com.google.refine.model.Cell;
 
@@ -13,8 +14,14 @@ public class WbLanguageVariable extends WbVariableExpr<String> {
     public String fromCell(Cell cell, ExpressionContext ctxt)
             throws SkipSchemaExpressionException {
         if (cell.value != null && !cell.value.toString().isEmpty()) {
-            // TODO some validation here?
-            return cell.value.toString();
+            String code = cell.value.toString().trim();
+            try {
+                // this just checks that the language code is known
+                WikimediaLanguageCodes.getLanguageCode(code);
+                return cell.value.toString();
+            } catch(IllegalArgumentException e) {
+                ;
+            }
         }
         throw new SkipSchemaExpressionException();
     }
