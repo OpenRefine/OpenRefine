d7564a4690f20db46193ce327b3362bd6114f9f6
Antonin Delpeuch <antonin@delpeuch.eu>
2018-06-13 15:37:51 +0100

    Change default precision of Wikibase dates to match the UI's defaults.

--- extensions/wikidata/tests/src/org/openrefine/wikidata/schema/WbDateVariableTest.java
+++ extensions/wikidata/tests/src/org/openrefine/wikidata/schema/WbDateVariableTest.java
@@ -33,9 +33,10 @@ import com.google.refine.model.Cell;
 public class WbDateVariableTest extends WbVariableTest<TimeValue> {
 
     private TimeValue year = Datamodel.makeTimeValue(2018, (byte) 1, (byte) 1, (byte) 0, (byte) 0, (byte) 0, (byte) 9,
-            0, 1, 0, TimeValue.CM_GREGORIAN_PRO);
-    private TimeValue day = Datamodel.makeTimeValue(2018, (byte) 2, (byte) 27, TimeValue.CM_GREGORIAN_PRO);
-    private TimeValue minute = Datamodel.makeTimeValue(2001, (byte) 2, (byte) 3, (byte)4, (byte)5, (byte)0, (byte)13, (byte)0, (byte)1, (byte)0, TimeValue.CM_GREGORIAN_PRO);
+            0, 0, 0, TimeValue.CM_GREGORIAN_PRO);
+    private TimeValue day = Datamodel.makeTimeValue(2018, (byte) 2, (byte) 27, (byte) 0, (byte) 0, (byte) 0, (byte) 11,
+            0, 0, 0,  TimeValue.CM_GREGORIAN_PRO);
+    private TimeValue minute = Datamodel.makeTimeValue(2001, (byte) 2, (byte) 3, (byte)4, (byte)5, (byte)0, (byte)13, (byte)0, (byte)0, (byte)0, TimeValue.CM_GREGORIAN_PRO);
 
     
     @Override
