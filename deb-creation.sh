38504a030d87da53613df790638b20fb451ee905
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-07-17 10:28:23 -0400

    moved to /opt

--- deb-creation.sh
+++ deb-creation.sh
@@ -1,8 +1,8 @@
 rm -rf deb-package
 rm openrefine.deb
 mkdir deb-package
-mkdir -p deb-package/usr/local/bin/openrefine
-rsync -av --progress * deb-package/usr/local/bin/openrefine/ --exclude-from=deb-exclude
+mkdir -p deb-package/opt/openrefine
+rsync -av --progress * deb-package/opt/openrefine/ --exclude-from=deb-exclude
 rsync -av --progress DEBIAN/ deb-package/DEBIAN/
 dpkg-deb --build deb-package
 mv deb-package.deb openrefine.deb
