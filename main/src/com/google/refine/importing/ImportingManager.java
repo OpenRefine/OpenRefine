8ee71c367eee6ef1acc15677f6f20ee67c0911d5
Thad Guidry <thadguidry@gmail.com>
2018-08-10 16:33:54 -0500

    improve log descriptions for stale import job removals

--- main/src/com/google/refine/importing/ImportingManager.java
+++ main/src/com/google/refine/importing/ImportingManager.java
@@ -307,12 +307,13 @@ public class ImportingManager {
         synchronized(jobs) {
             keys = new ArrayList<Long>(jobs.keySet());
         }
+        logger.info("Cleaning Stale Import Jobs ...");
         for (Long id : keys) {
             ImportingJob job = jobs.get(id);
             if (job != null && !job.updating && now - job.lastTouched > STALE_PERIOD) {
                 job.dispose();
                 jobs.remove(id);
-                logger.info("Disposed " + id);
+                logger.info("Removed Job ID " + id);
             }
         }
     }
