dac3437e1195b3f63fc154fbbaefdfa5c82f2950
Thad Guidry <thadguidry@gmail.com>
2018-08-11 19:30:51 -0500

    Removed unnecessary log line and instead changed primary log line

--- main/src/com/google/refine/importing/ImportingManager.java
+++ main/src/com/google/refine/importing/ImportingManager.java
@@ -307,13 +307,12 @@ public class ImportingManager {
         synchronized(jobs) {
             keys = new ArrayList<Long>(jobs.keySet());
         }
-        logger.info("Cleaning Stale Import Jobs ...");
         for (Long id : keys) {
             ImportingJob job = jobs.get(id);
             if (job != null && !job.updating && now - job.lastTouched > STALE_PERIOD) {
                 job.dispose();
                 jobs.remove(id);
-                logger.info("Removed Job ID " + id);
+                logger.info("Removed Stale Import Job ID " + id);
             }
         }
     }
