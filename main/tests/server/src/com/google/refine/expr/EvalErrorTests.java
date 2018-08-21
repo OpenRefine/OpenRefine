debe9bde373f7797a0d22c567d0a4a331bb6dbdd
Antonin Delpeuch <antonin@delpeuch.eu>
2018-08-21 15:43:36 +0100

    Serialization test for EvalError

---
+++ main/tests/server/src/com/google/refine/expr/EvalErrorTests.java
@@ -0,0 +1,13 @@
+package com.google.refine.expr;
+
+import org.testng.annotations.Test;
+
+import com.google.refine.tests.util.TestUtils;
+
+public class EvalErrorTests {
+    @Test
+    public void serializeEvalError() {
+        EvalError e = new EvalError("this is a critical error");
+        TestUtils.isSerializedTo(e, "{\"type\":\"error\",\"message\":\"This is a critical error\"}");
+    }
+}
