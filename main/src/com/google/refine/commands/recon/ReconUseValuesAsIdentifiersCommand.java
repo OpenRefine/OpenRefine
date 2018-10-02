2a68e761cbb5b0d687558d0a11a33cd570fc3086
Antonin Delpeuch <antonin@delpeuch.eu>
2018-10-02 12:31:59 +0100

    Implementation of UseValuesAsIdentifiers operation and command

---
+++ main/src/com/google/refine/commands/recon/ReconUseValuesAsIdentifiersCommand.java
@@ -0,0 +1,31 @@
+package com.google.refine.commands.recon;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
+
+import com.google.refine.browsing.EngineConfig;
+import com.google.refine.commands.EngineDependentCommand;
+import com.google.refine.model.AbstractOperation;
+import com.google.refine.model.Project;
+import com.google.refine.model.ReconCandidate;
+import com.google.refine.model.recon.StandardReconConfig;
+import com.google.refine.operations.recon.ReconMatchSpecificTopicOperation;
+import com.google.refine.operations.recon.ReconUseValuesAsIdentifiersOperation;
+
+public class ReconUseValuesAsIdentifiersCommand extends EngineDependentCommand {
+    @Override
+    protected AbstractOperation createOperation(Project project,
+            HttpServletRequest request, EngineConfig engineConfig) throws Exception {
+        
+        String columnName = request.getParameter("columnName");
+        
+        return new ReconUseValuesAsIdentifiersOperation(
+            engineConfig, 
+            columnName, 
+            request.getParameter("service"),
+            request.getParameter("identifierSpace"),
+            request.getParameter("schemaSpace")
+        );
+    }
+}
