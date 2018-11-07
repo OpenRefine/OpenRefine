3ad7d9e19d7a006101979e70fdae58fa7a4110ab
Antonin Delpeuch <antonin@delpeuch.eu>
2018-11-07 12:46:15 +0000

    Set version to 3.1-beta

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -60,7 +60,7 @@ import edu.mit.simile.butterfly.Butterfly;
 import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
-    static private String ASSIGNED_VERSION = "3.0";
+    static private String ASSIGNED_VERSION = "3.1-beta";
     
     static public String VERSION = "";
     static public String REVISION = "";
