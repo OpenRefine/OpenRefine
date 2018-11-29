36765e7e620c241f71af7aeffdcb97dd0c4e229d
Antonin Delpeuch <antonin@delpeuch.eu>
2018-11-29 11:59:30 +0900

    Bump version to 3.2-SNAPSHOT

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -64,7 +64,7 @@ import edu.mit.simile.butterfly.Butterfly;
 import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
-    static private String ASSIGNED_VERSION = "3.1";
+    static private String ASSIGNED_VERSION = "3.2";
     
     static public String VERSION = "";
     static public String REVISION = "";
