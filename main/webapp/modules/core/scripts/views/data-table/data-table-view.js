2db567ab0f1bc53de02b1adce63475a56e27ede7
Felix Lohmeier <mail@felixlohmeier.de>
2019-03-08 20:26:18 +0100

    added facet "Count non-blank records/column" to menu All

--- main/webapp/modules/core/scripts/views/data-table/data-table-view.js
+++ main/webapp/modules/core/scripts/views/data-table/data-table-view.js
@@ -684,6 +684,23 @@ DataTableView.prototype._createMenuForAllColumns = function(elmt) {
               }
             );
           }
+        },
+        {
+          label: $.i18n('core-views/count-non-blank-records'),
+          id: "core/count-non-blank-records",
+          click: function() {
+            ui.browsingEngine.addFacet(
+              "list", 
+              {
+                "name" : $.i18n('core-views/non-blank-records'),
+                "columnName" : "", 
+                "expression" : "filter(row.columnNames,cn,isNonBlank(if(row.record.fromRowIndex==row.index,row.record.cells[cn].value.join(\"\"),null)))"
+              },
+              {
+                "scroll" : false
+              }
+            );
+          }
         }
       ]
     },
