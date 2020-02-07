1caade1a27a4a051c8b8c0e43cfa909d7364e316
Antonin Delpeuch <antonin@delpeuch.eu>
2020-02-07 17:21:08 +0000

    Set version to 'Spark'

--- or-model/src/main/java/org/openrefine/RefineModel.java
+++ or-model/src/main/java/org/openrefine/RefineModel.java
@@ -14,7 +14,7 @@ import java.util.Map;
  *
  */
 public class RefineModel {
-	public static String VERSION = "3.3-beta";
+	public static String VERSION = "Spark";
 	
     private static class ClassMapping {
         public final String from;
