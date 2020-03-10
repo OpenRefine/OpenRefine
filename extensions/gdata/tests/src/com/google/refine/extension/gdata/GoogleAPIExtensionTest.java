25b5f67f0399700cf3d0d50b1fb2f3c5b417ede4
afkbrb <2w6f8c@gmail.com>
2020-03-10 19:32:14 +0800

    update test

--- extensions/gdata/tests/src/com/google/refine/extension/gdata/GoogleAPIExtensionTest.java
+++ extensions/gdata/tests/src/com/google/refine/extension/gdata/GoogleAPIExtensionTest.java
@@ -1,48 +1,38 @@
 package com.google.refine.extension.gdata;
 
+import com.google.refine.ProjectManager;
 import org.testng.annotations.Test;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.mockito.Mockito.mock;
 import static org.testng.Assert.*;
 
-public class SpreadsheetRETest {
+public class GoogleAPIExtensionTest {
 
-	/**
-	 * We cannot call {@link GoogleAPIExtension#extractSpreadSheetId(String url) } here,
-	 * otherwise, we'll get NullPointerException during its class initialization.
-	 *
-	 * So we just copy the regex and test it here.
-	 */
 	@Test
 	public void reTest() {
+		ProjectManager.singleton = mock(ProjectManager.class);
+
 		String spreadSheetId = "16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0";
 
 		String url1 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0/edit#gid=0";
-		assertEquals(extractSpreadSheetId(url1), spreadSheetId);
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url1), spreadSheetId);
 
 		String url2 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0/";
-		assertEquals(extractSpreadSheetId(url2), spreadSheetId);
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url2), spreadSheetId);
 
 		String url3 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0?foo=bar";
-		assertEquals(extractSpreadSheetId(url3), spreadSheetId);
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url3), spreadSheetId);
 
 		String url4 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0/?foo=bar";
-		assertEquals(extractSpreadSheetId(url4), spreadSheetId);
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url4), spreadSheetId);
 
 		String url5 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0#foo";
-		assertEquals(extractSpreadSheetId(url5), spreadSheetId);
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url5), spreadSheetId);
 
 		String url6 = "https://docs.google.com/spreadsheets/d/16L0JfpBWPfBJTqKtm-YU5-UBWLpkwXII-IRLMLnoKw0";
-		assertEquals(extractSpreadSheetId(url6), spreadSheetId);
-	}
-
-	private String extractSpreadSheetId(String url) {
-		Matcher matcher = Pattern.compile("(?<=/d/).*?(?=[/?#]|$)").matcher(url);
-		if (matcher.find()) {
-			return matcher.group();
-		}
-		return null;
+		assertEquals(GoogleAPIExtension.extractSpreadSheetId(url6), spreadSheetId);
 	}
 }
