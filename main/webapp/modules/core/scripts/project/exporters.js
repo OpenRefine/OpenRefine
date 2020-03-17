99a14ff4bf690f0d62719bc68a4e9938def43326
Abbe98 <albin.post@gmail.com>
2020-03-17 16:11:49 +0100

    Custom Tabular Exporter: add option to quote all strings #1869

--- main/webapp/modules/core/scripts/project/exporters.js
+++ main/webapp/modules/core/scripts/project/exporters.js
@@ -149,6 +149,9 @@ ExporterManager.prepareExportRowsForm = function(format, includeEngine, ext) {
   .attr("name", "format")
   .attr("value", format)
   .appendTo(form);
+  $('<input />')
+  .attr("name", "quoteAll")
+  .appendTo(form);
   if (includeEngine) {
     $('<input />')
     .attr("name", "engine")
