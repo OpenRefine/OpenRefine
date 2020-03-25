a79a6f270941897d3c99087b4d7329cf6a22faca
Subham Pramanik <subhampramanik@gmail.com>
2020-03-25 20:48:41 +0530

    refac: indentation

--- extensions/gdata/src/com/google/refine/extension/gdata/SpreadsheetSerializer.java
+++ extensions/gdata/src/com/google/refine/extension/gdata/SpreadsheetSerializer.java
@@ -81,7 +81,7 @@ final class SpreadsheetSerializer implements TabularSerializer {
         
         for (int c = 0; c < cells.size(); c++) {
             CellData cellData = cells.get(c);
-                cellDatas.add(cellData2sheetCellData(cellData));
+            cellDatas.add(cellData2sheetCellData(cellData));
         }
         
         rowData.setValues(cellDatas);
