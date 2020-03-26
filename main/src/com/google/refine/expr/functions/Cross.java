d2b42f8ed0030f97e47a339c2fc3bb4bb09991a0
Lu Liu <2w6f8c@gmail.com>
2020-03-26 15:57:10 +0800

    Support lookup by numbers for GREL cross function (#2468)
    * support int & long argument for cross function
    
    * support any types of a cell value

--- main/src/com/google/refine/expr/functions/Cross.java
+++ main/src/com/google/refine/expr/functions/Cross.java
@@ -56,10 +56,7 @@ public class Cross implements Function {
             long targetProjectID;
             ProjectLookup lookup;
 
-            if (v != null &&
-                    (v instanceof String || v instanceof WrappedCell) &&
-                    targetProjectName != null && targetProjectName instanceof String &&
-                    targetColumnName != null && targetColumnName instanceof String) {
+            if (v != null && targetProjectName instanceof String && targetColumnName instanceof String) {
                 try {
                     targetProjectID = ProjectManager.singleton.getProjectID((String) targetProjectName);
                 } catch (GetProjectIDException e) {
@@ -71,24 +68,25 @@ public class Cross implements Function {
                 } catch (LookupException e) {
                     return new EvalError(e.getMessage());
                 }
-                if (v instanceof String) {
-                    return lookup.getRows(v);
-                } else {
+
+                if (v instanceof WrappedCell) {
                     return lookup.getRows(((WrappedCell) v).cell.value);
+                } else {
+                    return lookup.getRows(v);
                 }
             }
         }
-        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects a string or cell, a project name to join with, and a column name in that project");
+        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects a cell or cell value, a project name to look up, and a column name in that project");
     }
 
     @Override
     public String getDescription() {
-        return "join with another project by column";
+        return "Looks up the given value in the target column of the target project, returns an array of matched rows, cell will be interpreted as cell.value";
     }
 
     @Override
     public String getParams() {
-        return "cell c or string value, string projectName, string columnName";
+        return "cell c or object value, string projectName, string columnName";
     }
 
     @Override
