e0d3a23ddce79a9afd1622733682a841cb633dfb
Nishtha3512 <51858166+Nishtha3512@users.noreply.github.com>
2020-04-11 23:17:33 +0530

    Fixes issue #2516 trim-input-url-whitespace

--- main/webapp/modules/core/scripts/index/default-importing-sources/sources.js
+++ main/webapp/modules/core/scripts/index/default-importing-sources/sources.js
@@ -91,7 +91,17 @@ UrlImportingSourceUI.prototype.attachUI = function(bodyDiv) {
 
   this._elmts.form.submit(function(evt){
     evt.preventDefault();
-    if(!isUrlValid(self._elmts.urlInput[0].value)) {
+    var importUrl = self._elmts.urlInput[0].value;
+    var validUrl =  False;
+    
+    if(isUrlValid(importUrl)) {
+      validUrl = true;
+    } else {
+      importUrl = self._elmts.urlInput[0].value.trim();
+      self._elmts.urlInput[0].value = importUrl;
+    }  
+    
+    if(!isUrlValid(importUrl)) {
       window.alert($.i18n('core-index-import/warning-web-address'));
     } else {
       self._controller.startImportJob(self._elmts.form, $.i18n('core-index-import/downloading-data'));
