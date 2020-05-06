a2e5c933ea44f4cb9e531c6a1df89aff744db529
joanneong <joanneongcf@u.nus.edu>
2020-05-06 23:38:34 +0800

    Remove unnecessary import statement

--- main/src/com/google/refine/browsing/util/ExpressionNominalValueGrouper.java
+++ main/src/com/google/refine/browsing/util/ExpressionNominalValueGrouper.java
@@ -36,7 +36,6 @@ package com.google.refine.browsing.util;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
