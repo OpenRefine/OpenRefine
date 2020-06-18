2a2ffcb4569604056b8f87c735e7256183818185
Tom Morris <tfmorris@gmail.com>
2020-06-18 16:18:53 -0400

    Update Google API dependencies for Sheets & Drive
    Remove unnecessary direct dependencies which are transitive
    dependencies of those.

--- extensions/gdata/src/com/google/refine/extension/gdata/GoogleAPIExtension.java
+++ extensions/gdata/src/com/google/refine/extension/gdata/GoogleAPIExtension.java
@@ -20,7 +20,7 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.sheets.v4.Sheets;
