baaad450039653176f6e1950f39f171a9704d831
darecoder1999 <ektamishra1999@gmail.com>
2020-06-24 22:34:04 +0530

    New implementation for Multivalue Scrutinizer
    Created inner class for Multivalue & mocks for unit tests
    
    New implementation for multivalue scrutinizer
    
    tests updated

--- extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/MultiValueScrutinizer.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/MultiValueScrutinizer.java
@@ -3,15 +3,36 @@ package org.openrefine.wikidata.qa.scrutinizers;
 import org.openrefine.wikidata.qa.QAWarning;
 import org.openrefine.wikidata.updates.ItemUpdate;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.Snak;
+import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
+import org.wikidata.wdtk.datamodel.interfaces.Value;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MultiValueScrutinizer extends EditScrutinizer {
 
     public static final String new_type = "multi-valued-property-is-required-for-new-item";
     public static final String existing_type = "multi-valued-property-is-required-for-existing-item";
+    public static String MULTI_VALUE_CONSTRAINT_QID = "Q21510857";
+    public static String MULTI_VALUE_CONSTRAINT_STATUS = "P2316";
+
+    class MultivalueConstraint {
+        Value constraintStatus;
+
+        MultivalueConstraint(Statement statement) {
+            List<SnakGroup> snakGroupList = statement.getClaim().getQualifiers();
+            for(SnakGroup group : snakGroupList) {
+                for (Snak snak : group.getSnaks()) {
+                    if (group.getProperty().getId().equals(MULTI_VALUE_CONSTRAINT_STATUS)){
+                        constraintStatus = snak.getValue();
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     public void scrutinize(ItemUpdate update) {
@@ -21,8 +42,12 @@ public class MultiValueScrutinizer extends EditScrutinizer {
             PropertyIdValue pid = statement.getClaim().getMainSnak().getPropertyId();
             if (propertyCount.containsKey(pid)) {
                 propertyCount.put(pid, propertyCount.get(pid) + 1);
-            } else if (_fetcher.hasMultiValue(pid)) {
-                propertyCount.put(pid, 1);
+            } else {
+                Statement constraintStatement = _fetcher.getConstraintsByType(pid, MULTI_VALUE_CONSTRAINT_QID).findFirst().orElse(null);
+                if (constraintStatement != null) {
+                    MultivalueConstraint constraint = new MultivalueConstraint(constraintStatement);
+                    propertyCount.put(pid, 1);
+                }
             }
         }
 
