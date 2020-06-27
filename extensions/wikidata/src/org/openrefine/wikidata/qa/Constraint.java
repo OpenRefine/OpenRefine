405a25aaef7d091ddad20b8682a6dfce613694f7
darecoder1999 <ektamishra1999@gmail.com>
2020-06-27 17:39:19 +0530

    Add constraint class

--- extensions/wikidata/src/org/openrefine/wikidata/qa/Constraint.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/Constraint.java
@@ -1,40 +1,51 @@
 package org.openrefine.wikidata.qa;
 
+import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Snak;
 import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.Value;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Constraint class is defined to parse the common parameters of property constraints
+ *
+ * Most of the scrutinizer will have inner class defining the structure of
+ * that constraint will extend this Constraint class
+ *
+ * @author Ekta Mishra
+ */
 public class Constraint {
 
     public static String CONSTRAINT_STATUS = "P2316";
     public static String CONSTRAINT_EXCEPTIONS = "P2303";
 
-    Value constraintStatus;
-    Set<Value> constraintExceptions;
+    ItemIdValue constraintStatus;
+    Set<EntityIdValue> constraintExceptions;
 
     Constraint(Statement statement) {
+        constraintExceptions = new HashSet<>();
         List<SnakGroup> snakGroupList = statement.getClaim().getQualifiers();
         for(SnakGroup group : snakGroupList) {
             for (Snak snak : group.getSnaks()) {
                 if (group.getProperty().getId().equals(CONSTRAINT_STATUS)) {
-                    constraintStatus = snak.getValue();
+                    constraintStatus = (ItemIdValue) snak.getValue();
                 }
                 else if (group.getProperty().getId().equals(CONSTRAINT_EXCEPTIONS)) {
-                    constraintExceptions.add(snak.getValue());
+                    constraintExceptions.add((EntityIdValue) snak.getValue());
                 }
             }
         }
     }
 
-    public Value getConstraintStatus(Statement statement) {
+    public ItemIdValue getConstraintStatus() {
         return this.constraintStatus;
     }
 
-    public Set<Value> getConstraintExceptions(Statement statement) {
+    public Set<EntityIdValue> getConstraintExceptions() {
         return this.constraintExceptions;
     }
 }
