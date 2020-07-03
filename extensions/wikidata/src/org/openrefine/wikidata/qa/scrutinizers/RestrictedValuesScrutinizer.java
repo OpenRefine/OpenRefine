b93f74311727861cb51d1fc5941f7ea7a01ccf9f
darecoder1999 <ektamishra1999@gmail.com>
2020-07-03 22:32:00 +0530

    Implemented RestrictedValueScrutinizer tests using mocks
    Added AllowedValueConstraint & DisallowedValueConstraint classes & updated the test cases too.

--- extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/RestrictedValuesScrutinizer.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/RestrictedValuesScrutinizer.java
@@ -1,17 +1,48 @@
 package org.openrefine.wikidata.qa.scrutinizers;
 
-import java.util.Set;
-
 import org.openrefine.wikidata.qa.QAWarning;
 import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Snak;
+import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
+import org.wikidata.wdtk.datamodel.interfaces.Statement;
 import org.wikidata.wdtk.datamodel.interfaces.Value;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public class RestrictedValuesScrutinizer extends SnakScrutinizer {
     
     public static String type = "forbidden-value";
+    public static String ALLOWED_VALUES_CONSTRAINT_QID = "Q21510859";
+    public static String ALLOWED_VALUES_CONSTRAINT_PID = "P2305";
+
+    public static String DISALLOWED_VALUES_CONSTRAINT_QID = "Q52558054";
+    public static String DISALLOWED_VALUES_CONSTRAINT_PID = "P2305";
+
+    class AllowedValueConstraint {
+        Set<Value> allowedValues;
+        AllowedValueConstraint(Statement statement) {
+            List<SnakGroup> specs = statement.getClaim().getQualifiers();
+            if (specs != null) {
+                List<Value> properties = _fetcher.findValues(specs, ALLOWED_VALUES_CONSTRAINT_PID);
+                allowedValues = new HashSet<>(properties);
+            }
+        }
+    }
+
+    class DisallowedValueConstraint {
+        Set<Value> disallowedValues;
+        DisallowedValueConstraint(Statement statement) {
+            List<SnakGroup> specs = statement.getClaim().getQualifiers();
+            if (specs != null) {
+                List<Value> properties = _fetcher.findValues(specs, DISALLOWED_VALUES_CONSTRAINT_PID);
+                disallowedValues = new HashSet<>(properties);
+            }
+        }
+    }
 
     @Override
     public void scrutinize(Snak snak, EntityIdValue entityId, boolean added) {
