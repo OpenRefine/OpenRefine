c68047a61423561d4b5d5d2bc47b63364b4718ae
Ekta Mishra <ektamishra1999@gmail.com>
2020-07-03 17:44:32 +0530

    Implemented QuantityScrutinizer tests using Mocks (#2862)
    * Implemented QunatityScrutinizer tests using Mockito
    
    Updated test cases and added AllowedUnitsConstraint class
    
    * Test cases updated

--- extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/QuantityScrutinizer.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/QuantityScrutinizer.java
@@ -1,9 +1,18 @@
 package org.openrefine.wikidata.qa.scrutinizers;
 
 import org.openrefine.wikidata.qa.QAWarning;
-import org.wikidata.wdtk.datamodel.interfaces.*;
+import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.QuantityValue;
+import org.wikidata.wdtk.datamodel.interfaces.Snak;
+import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
+import org.wikidata.wdtk.datamodel.interfaces.Statement;
+import org.wikidata.wdtk.datamodel.interfaces.Value;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Scrutinizer checking for units and bounds in quantities.
@@ -18,27 +27,51 @@ public class QuantityScrutinizer extends SnakScrutinizer {
     public static final String invalidUnitType = "invalid-unit";
     public static final String noUnitProvidedType = "no-unit-provided";
 
+    public static String NO_BOUNDS_CONSTRAINT_QID = "Q51723761";
+    public static String INTEGER_VALUED_CONSTRAINT_QID = "Q52848401";
+
+    public static String ALLOWED_UNITS_CONSTRAINT_QID = "Q21514353";
+    public static String ALLOWED_UNITS_CONSTRAINT_PID = "P2305";
+
+    class AllowedUnitsConstraint {
+        Set<ItemIdValue> allowedUnits;
+        AllowedUnitsConstraint(Statement statement) {
+            List<SnakGroup> specs = statement.getClaim().getQualifiers();
+            if (specs != null) {
+                List<Value> properties = _fetcher.findValues(specs, ALLOWED_UNITS_CONSTRAINT_PID);
+                allowedUnits = properties.stream()
+                        .map(e -> e == null ? null : (ItemIdValue) e)
+                        .collect(Collectors.toSet());
+            }
+        }
+    }
+
     @Override
     public void scrutinize(Snak snak, EntityIdValue entityId, boolean added) {
         if (snak.getValue() instanceof QuantityValue && added) {
             PropertyIdValue pid = snak.getPropertyId();
             QuantityValue value = (QuantityValue)snak.getValue();
-            
-            if(!_fetcher.boundsAllowed(pid) && (value.getUpperBound() != null || value.getLowerBound() != null)) {
+
+            if(!_fetcher.getConstraintsByType(pid, NO_BOUNDS_CONSTRAINT_QID).isEmpty() && (value.getUpperBound() != null || value.getLowerBound() != null)) {
                 QAWarning issue = new QAWarning(boundsDisallowedType, pid.getId(), QAWarning.Severity.IMPORTANT, 1);
                 issue.setProperty("property_entity", pid);
                 issue.setProperty("example_value", value.getNumericValue().toString());
                 issue.setProperty("example_item_entity", entityId);
                 addIssue(issue);
             }
-            if(_fetcher.integerValued(pid) && value.getNumericValue().scale() > 0) {
+            if(!_fetcher.getConstraintsByType(pid, INTEGER_VALUED_CONSTRAINT_QID).isEmpty() && value.getNumericValue().scale() > 0) {
                 QAWarning issue = new QAWarning(integerConstraintType, pid.getId(), QAWarning.Severity.IMPORTANT, 1);
                 issue.setProperty("property_entity", pid);
                 issue.setProperty("example_value", value.getNumericValue().toString());
                 issue.setProperty("example_item_entity", entityId);
                 addIssue(issue);
             }
-            Set<ItemIdValue> allowedUnits = _fetcher.allowedUnits(pid);
+            List<Statement> statementList = _fetcher.getConstraintsByType(pid, ALLOWED_UNITS_CONSTRAINT_QID);
+            Set<ItemIdValue> allowedUnits = null;
+            if (!statementList.isEmpty()) {
+                AllowedUnitsConstraint allowedUnitsConstraint = new AllowedUnitsConstraint(statementList.get(0));
+                allowedUnits = allowedUnitsConstraint.allowedUnits;
+            }
             ItemIdValue currentUnit = null;
             if (value.getUnitItemId() != null) {
                 currentUnit = value.getUnitItemId();
