f5786afa350c68abf0f001caa961a6b0e9afb52a
Tom Morris <tfmorris@gmail.com>
2020-07-03 15:20:01 -0400

    Increase test timeout - fixes #2875 (#2876)

--- main/tests/server/src/com/google/refine/operations/column/ColumnAdditionByFetchingURLsOperationTests.java
+++ main/tests/server/src/com/google/refine/operations/column/ColumnAdditionByFetchingURLsOperationTests.java
@@ -263,7 +263,7 @@ public class ColumnAdditionByFetchingURLsOperationTests extends RefineTest {
                 true,
                 headers);
 
-            runAndWait(op, 1000);
+            runAndWait(op, 3000);
 
             RecordedRequest request = server.takeRequest();
             Assert.assertEquals(request.getHeader("user-agent"), userAgentValue);
