a368cfe12e7857b8ee9f30377d28326faf9a545c
darecoder1999 <ektamishra1999@gmail.com>
2020-07-07 12:53:22 +0530

    done with implementation of ItemRequiresScrutinizer Class

--- extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/ItemRequiresScrutinizer.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/ItemRequiresScrutinizer.java
@@ -1,5 +1,6 @@
 package org.openrefine.wikidata.qa.scrutinizers;
 
+import org.openrefine.wikidata.qa.QAWarning;
 import org.openrefine.wikidata.updates.ItemUpdate;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Snak;
@@ -8,7 +9,11 @@ import org.wikidata.wdtk.datamodel.interfaces.Statement;
 import org.wikidata.wdtk.datamodel.interfaces.Value;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ItemRequiresScrutinizer extends EditScrutinizer {
 
@@ -40,7 +45,58 @@ public class ItemRequiresScrutinizer extends EditScrutinizer {
     }
 
     @Override
-    public void scrutinize(ItemUpdate edit) {
+    public void scrutinize(ItemUpdate update) {
+        Map<PropertyIdValue, Set<Value>> propertyIdValueValueMap = new HashMap<>();
+        for (Statement statement : update.getAddedStatements()){
+            PropertyIdValue pid = statement.getClaim().getMainSnak().getPropertyId();
+            Value value = statement.getClaim().getMainSnak().getValue();
+            Set<Value> values;
+            if (value != null) {
+                if (propertyIdValueValueMap.containsKey(pid)) {
+                    values = propertyIdValueValueMap.get(pid);
+                } else {
+                    values = new HashSet<>();
+                }
+                values.add(value);
+                propertyIdValueValueMap.put(pid, values);
+            }
+        }
 
+        for(PropertyIdValue propertyId : propertyIdValueValueMap.keySet()){
+            List<Statement> constraintDefinitions = _fetcher.getConstraintsByType(propertyId, ITEM_REQUIRES_CONSTRAINT_QID);
+            for (Statement statement : constraintDefinitions) {
+                ItemRequiresConstraint constraint = new ItemRequiresConstraint(statement);
+                PropertyIdValue itemRequiresPid = constraint.itemRequiresPid;
+                List<Value> itemList = constraint.itemList;
+                if (!propertyIdValueValueMap.containsKey(itemRequiresPid)) {
+                    QAWarning issue = new QAWarning(type, propertyId.getId()+itemRequiresPid.getId(), QAWarning.Severity.WARNING, 1);
+                    issue.setProperty("property_entity", propertyId);
+                    issue.setProperty("added_property_entity", itemRequiresPid);
+                    issue.setProperty("example_entity", update.getItemId());
+                    addIssue(issue);
+                } else if (raiseWarning(propertyIdValueValueMap, itemRequiresPid, itemList)) {
+                    QAWarning issue = new QAWarning(type, propertyId.getId()+itemRequiresPid.getId(), QAWarning.Severity.WARNING, 1);
+                    issue.setProperty("property_entity", propertyId);
+                    issue.setProperty("added_property_entity", itemRequiresPid);
+                    issue.setProperty("example_entity", update.getItemId());
+                    addIssue(issue);
+                }
+            }
+
+        }
+    }
+
+    private boolean raiseWarning(Map<PropertyIdValue, Set<Value>> propertyIdValueValueMap, PropertyIdValue itemRequiresPid, List<Value> itemList) {
+        if (itemList.isEmpty()){
+            return false;
+        }
+
+        for (Value value : itemList) {
+            if (propertyIdValueValueMap.get(itemRequiresPid).contains(value)){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
