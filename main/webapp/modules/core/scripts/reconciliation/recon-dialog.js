c1d60bf82f2293bba280d1f85e26a48784a11ddd
urvashigupta7 <urvashi072000@gmail.com>
2020-07-09 20:39:54 +0530

    addStandardServiceOnEnter

--- main/webapp/modules/core/scripts/reconciliation/recon-dialog.js
+++ main/webapp/modules/core/scripts/reconciliation/recon-dialog.js
@@ -213,7 +213,7 @@ ReconDialog.prototype._onAddStandardService = function() {
   };
 
   elmts.cancelButton.click(dismiss);
-  elmts.addButton.click(function() {
+  elmts.form.submit(function() {
     var url = $.trim(elmts.input[0].value);
     if (url.length > 0) {
       ReconciliationManager.registerStandardService(url, function(index) {
