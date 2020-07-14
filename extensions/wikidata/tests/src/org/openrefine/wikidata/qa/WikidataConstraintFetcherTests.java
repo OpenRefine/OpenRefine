c5e6ac9f9016410dc6b44acc7a700490c4f2eca1
Ekta Mishra <ektamishra1999@gmail.com>
2020-07-14 21:21:06 +0530

    Remove unused scrutinizer specific methods (#2935)
    After adding the code for fetching parameters for different scrutinizers in their corresponding classes, we should remove the redundant code from the ConstraintFetcher

--- extensions/wikidata/tests/src/org/openrefine/wikidata/qa/WikidataConstraintFetcherTests.java
+++ extensions/wikidata/tests/src/org/openrefine/wikidata/qa/WikidataConstraintFetcherTests.java
@@ -23,79 +23,14 @@
  ******************************************************************************/
 package org.openrefine.wikidata.qa;
 
-import java.util.regex.Pattern;
-
 import org.openrefine.wikidata.utils.EntityCacheStub;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.wikidata.wdtk.datamodel.helpers.Datamodel;
-import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 
 public class WikidataConstraintFetcherTests {
 
     private ConstraintFetcher fetcher;
 
-    private PropertyIdValue headOfGovernment;
-    private PropertyIdValue startTime;
-    private PropertyIdValue endTime;
-    private PropertyIdValue instanceOf;
-    private PropertyIdValue gridId;
-    private PropertyIdValue partOf;
-    private PropertyIdValue mother;
-    private PropertyIdValue child;
-
     public WikidataConstraintFetcherTests() {
         fetcher = new WikidataConstraintFetcher(new EntityCacheStub());
-        headOfGovernment = Datamodel.makeWikidataPropertyIdValue("P6");
-        startTime = Datamodel.makeWikidataPropertyIdValue("P580");
-        endTime = Datamodel.makeWikidataPropertyIdValue("P582");
-        instanceOf = Datamodel.makeWikidataPropertyIdValue("P31");
-        gridId = Datamodel.makeWikidataPropertyIdValue("P2427");
-        partOf = Datamodel.makeWikidataPropertyIdValue("P361");
-        mother = Datamodel.makeWikidataPropertyIdValue("P25");
-        child = Datamodel.makeWikidataPropertyIdValue("P40");
     }
 
-    @Test
-    public void testGetFormatConstraint() {
-        String regex = fetcher.getFormatRegex(gridId);
-        Pattern pattern = Pattern.compile(regex);
-
-        Assert.assertTrue(pattern.matcher("grid.470811.b").matches());
-        Assert.assertFalse(pattern.matcher("501100006367").matches());
-
-        Assert.assertNull(fetcher.getFormatRegex(instanceOf));
-    }
-
-    @Test
-    public void testGetInverseConstraint() {
-        Assert.assertEquals(fetcher.getInversePid(mother), child);
-    }
-
-    @Test
-    public void testAllowedQualifiers() {
-        Assert.assertTrue(fetcher.allowedQualifiers(headOfGovernment).contains(startTime));
-        Assert.assertTrue(fetcher.allowedQualifiers(headOfGovernment).contains(endTime));
-        Assert.assertFalse(fetcher.allowedQualifiers(headOfGovernment).contains(headOfGovernment));
-        Assert.assertNull(fetcher.allowedQualifiers(startTime));
-    }
-
-    @Test
-    public void testMandatoryQualifiers() {
-        Assert.assertTrue(fetcher.mandatoryQualifiers(headOfGovernment).contains(startTime));
-        Assert.assertFalse(fetcher.mandatoryQualifiers(headOfGovernment).contains(endTime));
-        Assert.assertNull(fetcher.allowedQualifiers(startTime));
-    }
-
-    @Test
-    public void testSingleValue() {
-        Assert.assertFalse(fetcher.hasSingleValue(headOfGovernment));
-        Assert.assertTrue(fetcher.hasSingleValue(mother));
-    }
-
-    @Test
-    public void testDistinctValues() {
-        Assert.assertFalse(fetcher.hasDistinctValues(partOf));
-        Assert.assertTrue(fetcher.hasDistinctValues(gridId));
-    }
 }
