a07c24c571127a88e26cf7fa72018cbbf801fe70
Antonin Delpeuch <antonin@delpeuch.eu>
2020-07-21 22:13:33 +0200

    Introduce ChangeData

--- or-model/src/main/java/org/openrefine/model/DatamodelRunner.java
+++ or-model/src/main/java/org/openrefine/model/DatamodelRunner.java
@@ -3,11 +3,15 @@ package org.openrefine.model;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.hadoop.fs.FileSystem;
 
+import org.openrefine.model.changes.ChangeData;
+import org.openrefine.model.changes.IndexedData;
 import org.openrefine.overlay.OverlayModel;
 
 /**
@@ -30,6 +34,16 @@ public interface DatamodelRunner {
     public GridState loadGridState(File path) throws IOException;
 
     /**
+     * Loads a {@link ChangeData} serialized at a given location.
+     * 
+     * @param path
+     *            the directory where the ChangeData is stored
+     * @throws IOException
+     *             when loading the grid failed
+     */
+    public <T extends Serializable> ChangeData<T> loadChangeData(File path, TypeReference<IndexedData<T>> expectedType) throws IOException;
+
+    /**
      * Returns a file system used by the implementation to read files to import.
      * 
      * @throws IOException
@@ -40,5 +54,10 @@ public interface DatamodelRunner {
      * Creates a {@link GridState} from an in-memory list of rows, which will be numbered from 0 to length-1.
      */
     public GridState create(ColumnModel columnModel, List<Row> rows, Map<String, OverlayModel> overlayModels);
+
+    /**
+     * Creates a {@link ChangeData} from an in-memory list of indexed data. The list is required to be sorted.
+     */
+    public <T extends Serializable> ChangeData<T> create(List<IndexedData<T>> changeData);
 
 }
