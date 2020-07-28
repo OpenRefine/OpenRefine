f117b6ccf51cf308fcd8fd070a02cc23635213b1
afkbrb <2w6f8c@gmail.com>
2020-07-28 11:05:30 +0800

    test multiple connections

--- extensions/wikidata/tests/src/org/openrefine/wikidata/commands/LoginCommandTest.java
+++ extensions/wikidata/tests/src/org/openrefine/wikidata/commands/LoginCommandTest.java
@@ -493,6 +493,46 @@ public class LoginCommandTest extends CommandTest {
         assertFalse(ConnectionManager.getInstance().isLoggedIn(apiEndpoint));
     }
 
+    @Test
+    public void testMultipleConnections() throws Exception {
+        BasicApiConnection connection = mock(BasicApiConnection.class);
+        whenNew(BasicApiConnection.class).withAnyArguments().thenReturn(connection);
+        when(connection.getCurrentUser()).thenReturn(username);
+
+        String wikibase1 = "https://www.wikibase1.org/w/api.php";
+        String wikibase2 = "https://www.wikibase2.org/w/api.php";
+
+        when(request.getParameter("csrf_token")).thenReturn(Command.csrfFactory.getFreshToken());
+        when(request.getParameter(USERNAME)).thenReturn(username);
+        when(request.getParameter(PASSWORD)).thenReturn(password);
+
+        // login to one endpoint first
+        when(request.getParameter(API_ENDPOINT)).thenReturn(wikibase1);
+        command.doPost(request, response);
+
+        // not logged in to another endpoint
+        assertFalse(ConnectionManager.getInstance().isLoggedIn(wikibase2));
+
+        // login to another endpoint
+        when(request.getParameter(API_ENDPOINT)).thenReturn(wikibase2);
+        command.doPost(request, response);
+
+        // logged in to both endpoints
+        assertTrue(ConnectionManager.getInstance().isLoggedIn(wikibase1));
+        assertTrue(ConnectionManager.getInstance().isLoggedIn(wikibase2));
+
+        // logout from the first endpoint
+        when(request.getParameter("logout")).thenReturn("true");
+        when(request.getParameter(API_ENDPOINT)).thenReturn(wikibase1);
+        command.doPost(request, response);
+
+        // logged out from the first endpoint
+        assertFalse(ConnectionManager.getInstance().isLoggedIn(wikibase1));
+
+        // still logged in to another endpoint
+        assertTrue(ConnectionManager.getInstance().isLoggedIn(wikibase2));
+    }
+
     private static Cookie[] makeRequestCookies() {
         List<Cookie> cookies = new ArrayList<>();
         cookieMap.forEach((key, value) -> cookies.add(new Cookie(apiEndpointPrefix + WIKIBASE_COOKIE_PREFIX + key, value)));
