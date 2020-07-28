e0bb9aa391e9347662be997b46d1049381c25726
afkbrb <2w6f8c@gmail.com>
2020-07-28 15:54:42 +0800

    update instance/subclass of related translations

--- extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/NewItemScrutinizer.java
+++ extensions/wikidata/src/org/openrefine/wikidata/qa/scrutinizers/NewItemScrutinizer.java
@@ -37,7 +37,7 @@ public class NewItemScrutinizer extends EditScrutinizer {
     public static final String noLabelType = "new-item-without-labels-or-aliases";
     public static final String noDescType = "new-item-without-descriptions";
     public static final String deletedStatementsType = "new-item-with-deleted-statements";
-    public static final String noTypeType = "new-item-without-P31-or-P279";
+    public static final String noTypeType = "new-item-without-instance-of-or-subclass-of";
     public static final String newItemType = "new-item-created";
 
     @Override
