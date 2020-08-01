5985f95ce14e45e5ef4a00924377b0e67ae5103d
Tom Morris <tfmorris@gmail.com>
2020-08-01 04:58:16 -0400

    Explicitly set SameSite=Lax on cookie - fixes #3007 (#3014)

--- extensions/gdata/module/scripts/gdata-extension.js
+++ extensions/gdata/module/scripts/gdata-extension.js
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 var GdataExtension = {};
 
 GdataExtension.isAuthorized = function() {
-  return $.cookie('oauth2_token') !== null;
+  return Cookies.get('oauth2_token') !== null;
 };
 
 GdataExtension.showAuthorizationDialog = function(onAuthorized, onNotAuthorized) {
