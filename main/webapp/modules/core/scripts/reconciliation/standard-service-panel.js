6940e8458773f5a0e07d845e63f2f127c914c5c8
Tom Morris <tfmorris@gmail.com>
2020-08-03 21:28:09 -0400

    Clean up formatting of service API link

--- main/webapp/modules/core/scripts/reconciliation/standard-service-panel.js
+++ main/webapp/modules/core/scripts/reconciliation/standard-service-panel.js
@@ -84,8 +84,7 @@ ReconStandardServicePanel.prototype._constructUI = function() {
   this._panel = $(DOM.loadHTML("core", "scripts/reconciliation/standard-service-panel.html")).appendTo(this._container);
   this._elmts = DOM.bind(this._panel);
   
-  this._elmts.or_proc_access.html($.i18n('core-recon/access'));
-  this._elmts.rawServiceLink.html($.i18n('core-recon/service-api'));
+  this._elmts.or_proc_access.html($.i18n('core-recon/access-service'));
   this._elmts.or_proc_cellType.html($.i18n('core-recon/cell-type'));
   this._elmts.or_proc_colDetail.html($.i18n('core-recon/col-detail'));
   this._elmts.or_proc_againstType.html($.i18n('core-recon/against-type'));
