437dc4d74110ce006b9f829fe05f461cc8ed1170
Antonin Delpeuch <antonin@delpeuch.eu>
2020-09-24 15:06:03 +0200

    Set version to 3.4.1

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -64,7 +64,7 @@ import edu.mit.simile.butterfly.Butterfly;
 import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
-    static private String ASSIGNED_VERSION = "3.4";
+    static private String ASSIGNED_VERSION = "3.4.1";
     
     static public String VERSION = "";
     static public String REVISION = "";
