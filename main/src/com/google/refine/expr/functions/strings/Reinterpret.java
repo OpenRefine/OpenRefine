fb96d22decdee48f7e1639fceaabd02b11c92079
Tom Morris <tfmorris@gmail.com>
2020-11-03 14:49:00 -0500

    Revert "Fix Reinterpret missing documentation"
    This reverts commit f7cece6103d284b705c4a258830549a041691306.

--- main/src/com/google/refine/expr/functions/strings/Reinterpret.java
+++ main/src/com/google/refine/expr/functions/strings/Reinterpret.java
@@ -66,7 +66,7 @@ public class Reinterpret implements Function {
                 return reinterpret(str, decoder, encoder);
             }
         }
-        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects String to reinterpret with a given target encoder and optional source decoder");
+        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects 2 or 3 arguments");
     }
 
     private Object reinterpret(String str, String decoder, String encoder) {
@@ -89,7 +89,7 @@ public class Reinterpret implements Function {
                 result = new String(bytes, encoder);
             }
         } catch (UnsupportedEncodingException e) {
-            return new EvalError(ControlFunctionRegistry.getFunctionName(this) + ": target encoding '" + encoder + "' is not available or recognized.");
+            return new EvalError(ControlFunctionRegistry.getFunctionName(this) + ": encoding '" + encoder + "' is not available or recognized.");
         }
                         
         return result;
@@ -97,12 +97,12 @@ public class Reinterpret implements Function {
     
     @Override
     public String getDescription() {
-        return "Returns s reinterpreted thru the given target encoder and optional source decoder.";
+        return "Returns s reinterpreted thru the given encoder.";
     }
     
     @Override
     public String getParams() {
-        return "string s, string target encoder, string source decoder";
+        return "string s, string encoder";
     }
     
     @Override
