4f97fd55a5972b18e166499b21b4ecc954150e66
Mahesh Jindal <30390269+maheshjindal@users.noreply.github.com>
2020-12-02 22:41:45 +0530

    Fix: Preventing addition of any empty cells with whitespaces while importing Xml Data with Tests #1095 (#3357)
    * Fix: Preventing addition of any empty cells with whitespaces while importing Xml data with Tests : Issue #1095
    
    * Chore: Using 'CharMatcher' to match whitespace pattern instead of using custom regex : Issue #1095

--- main/src/com/google/refine/importers/XmlImporter.java
+++ main/src/com/google/refine/importers/XmlImporter.java
@@ -46,6 +46,7 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import com.google.common.base.CharMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -209,7 +210,8 @@ public class XmlImporter extends TreeImportingParserBase {
     
     static public class XmlParser implements TreeReader {
         final protected XMLStreamReader parser;
-        
+        static final int WHITESPACE_CHARACTERS_TOKEN = 15;
+
         public XmlParser(InputStream inputStream) throws XMLStreamException, IOException {
             parser = createXMLStreamReader(inputStream);
         }
@@ -230,7 +232,15 @@ public class XmlImporter extends TreeImportingParserBase {
             } catch (XMLStreamException e) {
                 throw new TreeReaderException(e);
             }
-            
+            // Issue #1095 : Preventing addition of empty cells containing whitespaces in the table
+            // Whitespaces between tags will be parsed as Characters by default
+            // Updates the token if the text value is a whitespace
+            if (currentToken == XMLStreamConstants.CHARACTERS) {
+                String text = parser.getText();
+                if (!text.isEmpty() && CharMatcher.whitespace().matchesAllOf(text)) {
+                    currentToken = WHITESPACE_CHARACTERS_TOKEN;
+                }
+            }
             return mapToToken(currentToken);
         }
         
@@ -250,6 +260,7 @@ public class XmlImporter extends TreeImportingParserBase {
                 case XMLStreamConstants.COMMENT: return Token.Ignorable;
                 case XMLStreamConstants.CDATA: return Token.Ignorable;
                 case XMLStreamConstants.ATTRIBUTE: return Token.Ignorable;
+                case WHITESPACE_CHARACTERS_TOKEN: return Token.Ignorable;
                 default:
                     return Token.Ignorable;
             }
