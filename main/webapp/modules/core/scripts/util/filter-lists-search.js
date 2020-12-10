61f1942dd24076a6c6d7408e9afc59edacd5cc2f
mayralgr <ml17ft@gmail.com>
2020-12-09 22:43:05 -0600

    Add search in projects, to correct add/remove class

--- main/webapp/modules/core/scripts/util/filter-lists-search.js
+++ main/webapp/modules/core/scripts/util/filter-lists-search.js
@@ -23,42 +23,66 @@
             var listElement = $(this);
             console.log("list element", listElement)
             /* FILTER: trigger filter */
-            listElement.trigger("filter", text);
+            try{
+                listElement.trigger("filterSearch", text);
+            }
+            catch(e)
+            {
+                console.log(e)
+            }
 
-            $(settings.tagSelector).removeClass('current');
-            $(this).addClass('current');
+            // $(settings.tagSelector).removeClass('current');
+            // $(this).addClass('current');
             /* FILTER: select a text and filter */
-            listElement.bind("filter", function( e, text ) {
-                console.log("filtering", )
-                console.log("tres",listElement.tr)
-                // search in columns and rows, if contains show == true, else none
+            listElement.bind("filterSearch", function( e, text ) {
+                // get each project row
+                listElement.each( function () {
+                    $(this).removeClass(settings.selectedTagClass);
+                    var row = this
+                    // console.log("initial row", row)
+
+                    // get each column of the metadata
+                    var columns = $(this).find('td')
+                    columns.each( function () {
+                        // text inside the column
+                        var textInColumn = this.textContent
+                        // if the text in input is in the textcontent, row will be shown
+                        if(textInColumn.contains(text))
+                        {
+                            // console.log("contains text")
+
+                            if(!$(row).hasClass(settings.selectedTagClass))
+                            {
+                                // console.log("adding class",settings.selectedTagClass)
+                                $(row).addClass(settings.selectedTagClass);
+                            }
+                        }
+                    });
+                    // console.log("final row:" , row)
+                });
+                console.log("final list element", listElement)
+                $(this).trigger("filterSearchList",[ settings.selectedTagClass ]);
             });
 
-            /*listElement.bind("filter", function( e, text ){
-                console.log(element)
-                console.log("binding if contains : ", text)
-                if(settings.useAll){
-                    $(settings.projectSelector).removeClass(settings.selectedTagClass);
-                    console.log("adding class to", settings.projectSelector + '[href="' + text + '"]')
-                    $(settings.projectSelector + '[href="' + text + '"]').addClass(settings.selectedTagClass);
-                }
-                if(text == '' || text.length <1){
-                    $(this).trigger("show");
+            /* FILTERPORTFOLIO: pass in a class to show, all others will be hidden */
+            listElement.bind("filterSearchList", function( e, classToShow ) {
+                if( text == ''){
+                        $(this).trigger("showSearch");
                 }else{
-                    $(this).trigger("show", [ '.' + text ] );
-                    $(this).trigger("hide", [ ':not(.' + text + ')' ] );
+                        $(this).trigger("showSearch", [ '.' + classToShow ] );
+                        $(this).trigger("hideSearch", [ ':not(.' + classToShow + ')' ] );
                 }
-            });*/
+            });
 
-            /* SHOW: show a single class
-            $(this).bind("show", function( e, selectorToShow ){
-                    listElement.children(selectorToShow).animate(settings.show, settings.animationSpeed);
-            });*/
+            /* SHOW: show a single class*/
+            $(this).bind("showSearch", function( e, selectorToShow ){
+                    $("#tableBody").children(selectorToShow).animate(settings.show, settings.animationSpeed);
+            });
 
-            /* SHOW: hide a single class
-            $(this).bind("hide", function( e, selectorToHide ){
-                    listElement.children(selectorToHide).animate(settings.hide, settings.animationSpeed * 0.6);
-            });*/
+            /* SHOW: hide a single class*/
+            $(this).bind("hideSearch", function( e, selectorToHide ){
+                    $("#tableBody").children(selectorToHide).animate(settings.hide, settings.animationSpeed * 0.6);
+            });
 
             return this;
         };
