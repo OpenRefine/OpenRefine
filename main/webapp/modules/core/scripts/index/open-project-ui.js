086aca4cd21466bcdf2d57cfba58e0022d6c9471
mayralgr <ml17ft@gmail.com>
2020-12-13 12:02:45 -0600

    Add svg icon, focus to the input when click the search

--- main/webapp/modules/core/scripts/index/open-project-ui.js
+++ main/webapp/modules/core/scripts/index/open-project-ui.js
@@ -101,10 +101,11 @@ Refine.OpenProjectUI.prototype._buildProjectSearchPanel = function(){
     .attr('id','divImage')
     .appendTo(form)
   // Add img to the form
-  $('<img/>')
-    .attr('src', 'images/search.png')
+    var icon = $('<div/>')
+    .html('<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" width="100%"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>')
     .attr('id', 'searchIcon')
-    .addClass("magnifying_glass").text('Search').appendTo(divImage);
+    .addClass("magnifying_glass")
+    .appendTo(divImage);
 
     // Add div for input, in the form
   var divImage = $('<div/>')
@@ -174,6 +175,7 @@ Refine.OpenProjectUI.prototype._searchAnimation = function() {
       $("#divInput").show()
             icon.addClass("magnifying-glass-open")
       form.show()
+            form.focus()
     }
     var widthFormOpen = Math.floor($('#right-panel-body').width() * 2 / 3);
     form.animate({
