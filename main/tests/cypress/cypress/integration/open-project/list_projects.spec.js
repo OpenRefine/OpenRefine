49054b163283bff9b28cf3fb1ce02f2ce1c39905
Kush Trivedi <kushthedude@gmail.com>
2021-01-16 22:58:02 +0530

    tests: UX Test for project list deletion, sort, filter
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- main/tests/cypress/cypress/integration/open-project/list_projects.spec.js
+++ main/tests/cypress/cypress/integration/open-project/list_projects.spec.js
@@ -15,4 +15,28 @@ describe(__filename, function () {
 		cy.get('#projects-list table').contains(projectName).click();
 		cy.get('#project-name-button').contains(projectName);
 	});
+	it('Ensure projects are sorted on basis of names', function () {
+		const projectName = "projectA";
+		const projectName2 = "projectZ";
+		cy.loadProject('food.mini.csv', projectName);
+		cy.loadProject('food.mini.csv', projectName2);
+		cy.visitOpenRefine();
+		cy.navigateTo('Open Project');
+		cy.get('#projects-list table').contains("Name").click();
+		cy.get('#projects-list tbody>tr').eq(1).contains('projectA');
+		cy.get('#projects-list table').contains("Name").click();
+		cy.get('#projects-list tbody>tr').eq(1).contains('projectZ'); 
+	});
+	it('Ensure project is deleted from database as well as UI', function () {
+		const projectName = Date.now();
+		cy.loadProject('food.mini.csv', projectName);
+		cy.visitOpenRefine();
+		cy.navigateTo('Open Project');
+		cy.contains('td', projectName).siblings().find('.delete-project').click();
+		cy.get('#projects-list table>tbody>tr').should('not.contain', projectName);
+		cy.request('GET', 'http://127.0.0.1:3333/command/core/get-all-project-metadata').then((response) => {
+			var responseText = JSON.stringify(response.body);
+			expect(responseText).to.not.have.string(projectName);
+  		});
+	});
 });
