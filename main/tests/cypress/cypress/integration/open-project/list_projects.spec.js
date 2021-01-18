fa27cdc9daaa410a9b8f7704ae6f32a76c54608f
Kush Trivedi <kushthedude@gmail.com>
2021-01-18 16:18:08 +0530

    fix: add hack to check for sorting
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- main/tests/cypress/cypress/integration/open-project/list_projects.spec.js
+++ main/tests/cypress/cypress/integration/open-project/list_projects.spec.js
@@ -18,14 +18,44 @@ describe(__filename, function () {
 	it('Ensure projects are sorted on basis of names', function () {
 		const projectName = "projectA";
 		const projectName2 = "projectZ";
+		var positionA, positionZ;
 		cy.loadProject('food.mini.csv', projectName);
 		cy.loadProject('food.mini.csv', projectName2);
 		cy.visitOpenRefine();
 		cy.navigateTo('Open Project');
 		cy.get('#projects-list table').contains("Name").click();
-		cy.get('#projects-list tbody>tr').eq(0).should('contain','projectA');
+		// cy.get('#projects-list tbody>tr').eq(0).should('contain','projectA');
+		// cy.get('#projects-list table').contains("Name").click();
+		// cy.get('#projects-list tbody>tr').eq(0).should('contain', 'projectZ'); 
+		// hack since all projects are not being deleted
+		cy.get("#projects-list tbody")
+		.find("tr")
+		.then((rows) => {
+		  rows.toArray().forEach((element) => {
+			if (element.innerHTML.includes("projectZ")) {
+				positionZ = (rows.index(element));
+			}
+			if (element.innerHTML.includes("projectA")) {
+					positionA = (rows.index(element));
+				}
+		  });
+		  expect(positionA).to.be.lessThan(positionZ);
+		});
 		cy.get('#projects-list table').contains("Name").click();
-		cy.get('#projects-list tbody>tr').eq(0).should('contain', 'projectZ'); 
+		cy.get("#projects-list tbody")
+		.find("tr")
+		.then((rows) => {
+		  rows.toArray().forEach((element) => {
+			if (element.innerHTML.includes("projectZ")) {
+				positionZ = (rows.index(element));
+			}
+			if (element.innerHTML.includes("projectA")) {
+					positionA = (rows.index(element));
+				}
+		  });
+		  expect(positionA).to.be.greaterThan(positionZ);
+		});
+		
 	});
 	it('Ensure project is deleted from database as well as UI', function () {
 		const projectName = Date.now();
