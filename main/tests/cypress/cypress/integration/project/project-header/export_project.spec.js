8ed6cbd9072cd790e001a4b014ca83a21086f453
Kush Trivedi <kushthedude@gmail.com>
2021-01-18 16:05:07 +0530

    feat: introduct prettier style formatting for cypress test suite
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- main/tests/cypress/cypress/integration/project/project-header/export_project.spec.js
+++ main/tests/cypress/cypress/integration/project/project-header/export_project.spec.js
@@ -1,8 +1,10 @@
 describe(__filename, function () {
-	it('Export a project', function () {
-		const testProjectName = Date.now();
-		cy.loadAndVisitProject('food.mini.csv', testProjectName);
-		cy.get('#export-button').click();
-		cy.get('.menu-container a').contains('OpenRefine project archive to file').click();
-	});
-});
+    it('Export a project', function () {
+        const testProjectName = Date.now()
+        cy.loadAndVisitProject('food.mini.csv', testProjectName)
+        cy.get('#export-button').click()
+        cy.get('.menu-container a')
+            .contains('OpenRefine project archive to file')
+            .click()
+    })
+})
