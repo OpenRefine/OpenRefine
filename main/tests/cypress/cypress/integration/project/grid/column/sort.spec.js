20b92e8a2df70228f180e60d3047368c96be7006
kushthedude <kushthedude@gmail.com>
2021-02-03 18:01:55 +0530

    feat: Initialise ES-Lint for cypress
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- main/tests/cypress/cypress/integration/project/grid/column/sort.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/column/sort.spec.js
@@ -1,115 +1,105 @@
-describe(__filename, function () {
-    it('Perform a text sort + Reverse + Remove', function () {
-        cy.loadAndVisitProject('food.sort')
+describe(__filename, function() {
+  it('Perform a text sort + Reverse + Remove', function() {
+    cy.loadAndVisitProject('food.sort');
 
-        // sort and confirm
-        cy.columnActionClick('Shrt_Desc', ['Sort'])
-        cy.waitForDialogPanel()
-        cy.confirmDialogPanel()
+    // sort and confirm
+    cy.columnActionClick('Shrt_Desc', ['Sort']);
+    cy.waitForDialogPanel();
+    cy.confirmDialogPanel();
 
-        // ensure sorting is active
-        cy.getCell(0, 'Shrt_Desc').should(
-            'to.contain',
-            'BUTTER,WHIPPED,WITH SALT'
-        )
-        cy.getCell(1, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT')
-        
-        cy.columnActionClick('Shrt_Desc', ['Sort', 'Reverse'])
-        
-        cy.getCell(0, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT')
-        cy.getCell(1, 'Shrt_Desc').should(
-            'to.contain',
-            'BUTTER,WHIPPED,WITH SALT'
-        )
+    // ensure sorting is active
+    cy.getCell(0, 'Shrt_Desc').should('to.contain', 'BUTTER,WHIPPED,WITH SALT');
+    cy.getCell(1, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT');
 
-        cy.columnActionClick('Shrt_Desc', ['Sort', 'Remove sort'])
+    cy.columnActionClick('Shrt_Desc', ['Sort', 'Reverse']);
 
-        cy.getCell(0, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT')
-        cy.getCell(1, 'Shrt_Desc').should(
-            'to.contain',
-            'BUTTER,WHIPPED,WITH SALT'
-        )
-    })
-    it('Perform a number sort + Reverse + Remove', function () {
-        cy.loadAndVisitProject('food.sort')
+    cy.getCell(0, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT');
+    cy.getCell(1, 'Shrt_Desc').should('to.contain', 'BUTTER,WHIPPED,WITH SALT');
 
-        cy.castColumnTo('NDB_No','number')
-        cy.columnActionClick('NDB_No', ['Sort'])
+    cy.columnActionClick('Shrt_Desc', ['Sort', 'Remove sort']);
 
-        cy.waitForDialogPanel()
-        cy.get('[type="radio"]').check('number')
-        cy.get('[type="radio"]').check('reverse')
-        cy.confirmDialogPanel()
+    cy.getCell(0, 'Shrt_Desc').should('to.contain', 'BUTTER,WITH SALT');
+    cy.getCell(1, 'Shrt_Desc').should('to.contain', 'BUTTER,WHIPPED,WITH SALT');
+  });
+  it('Perform a number sort + Reverse + Remove', function() {
+    cy.loadAndVisitProject('food.sort');
 
-        // ensure sorting is active
-        cy.getCell(0, 'NDB_No').should('to.contain', 1002)
-        cy.getCell(1, 'NDB_No').should('to.contain', 1001)
-        cy.columnActionClick('NDB_No', ['Sort', 'Reverse'])
-        cy.getCell(0, 'NDB_No').should('to.contain', 1001)
-        cy.getCell(1, 'NDB_No').should('to.contain', 1002)
-        cy.columnActionClick('NDB_No', ['Sort', 'Remove sort'])
-        cy.getCell(0, 'NDB_No').should('to.contain', 1001)
-        cy.getCell(1, 'NDB_No').should('to.contain', 1002)
-    })
-    it('Perform a date sort + Reverse + Remove', function () {
-        cy.loadAndVisitProject('food.sort')
+    cy.castColumnTo('NDB_No', 'number');
+    cy.columnActionClick('NDB_No', ['Sort']);
 
-        cy.castColumnTo('Date','date')
-        cy.columnActionClick('Date', ['Sort'])
+    cy.waitForDialogPanel();
+    cy.get('[type="radio"]').check('number');
+    cy.get('[type="radio"]').check('reverse');
+    cy.confirmDialogPanel();
 
-        cy.waitForDialogPanel()
-        cy.get('[type="radio"]').check('date')
-        cy.get('[type="radio"]').check('reverse')
-        cy.confirmDialogPanel()
+    // ensure sorting is active
+    cy.getCell(0, 'NDB_No').should('to.contain', 1002);
+    cy.getCell(1, 'NDB_No').should('to.contain', 1001);
+    cy.columnActionClick('NDB_No', ['Sort', 'Reverse']);
+    cy.getCell(0, 'NDB_No').should('to.contain', 1001);
+    cy.getCell(1, 'NDB_No').should('to.contain', 1002);
+    cy.columnActionClick('NDB_No', ['Sort', 'Remove sort']);
+    cy.getCell(0, 'NDB_No').should('to.contain', 1001);
+    cy.getCell(1, 'NDB_No').should('to.contain', 1002);
+  });
+  it('Perform a date sort + Reverse + Remove', function() {
+    cy.loadAndVisitProject('food.sort');
 
-        // ensure sorting is active
-        cy.getCell(0, 'Date').should('to.contain', '2020-12-17T00:00:00Z')
-        cy.getCell(1, 'Date').should('to.contain', '2020-08-17T00:00:00Z')
-        
-        cy.columnActionClick('Date', ['Sort', 'Reverse'])
+    cy.castColumnTo('Date', 'date');
+    cy.columnActionClick('Date', ['Sort']);
 
-        cy.getCell(0, 'Date').should('to.contain', '2020-08-17T00:00:00Z')
-        cy.getCell(1, 'Date').should('to.contain', '2020-12-17T00:00:00Z')
+    cy.waitForDialogPanel();
+    cy.get('[type="radio"]').check('date');
+    cy.get('[type="radio"]').check('reverse');
+    cy.confirmDialogPanel();
 
-        cy.columnActionClick('Date', ['Sort', 'Remove sort'])
-        cy.getCell(0, 'Date').should('to.contain', '2020-12-17T00:00:00Z')
-        cy.getCell(1, 'Date').should('to.contain', '2020-08-17T00:00:00Z')
+    // ensure sorting is active
+    cy.getCell(0, 'Date').should('to.contain', '2020-12-17T00:00:00Z');
+    cy.getCell(1, 'Date').should('to.contain', '2020-08-17T00:00:00Z');
 
-    })
-    it('Perform a bool sort + Reverse + Remove', function () {
-        cy.loadAndVisitProject('food.sort')
+    cy.columnActionClick('Date', ['Sort', 'Reverse']);
 
-        cy.getCell(0, 'Fat')
-            .trigger('mouseover')
-            .within(() => {
-                cy.get('a.data-table-cell-edit').click()
-            })
-        cy.get('select').select('boolean')
-        cy.get('button').contains(new RegExp('Apply', 'g')).click()
+    cy.getCell(0, 'Date').should('to.contain', '2020-08-17T00:00:00Z');
+    cy.getCell(1, 'Date').should('to.contain', '2020-12-17T00:00:00Z');
 
-        cy.getCell(1, 'Fat')
-            .trigger('mouseover')
-            .within(() => {
-                cy.get('a.data-table-cell-edit').click()
-            })
-        cy.get('select').select('boolean')
-        cy.get('button').contains(new RegExp('Apply', 'g')).click()
+    cy.columnActionClick('Date', ['Sort', 'Remove sort']);
+    cy.getCell(0, 'Date').should('to.contain', '2020-12-17T00:00:00Z');
+    cy.getCell(1, 'Date').should('to.contain', '2020-08-17T00:00:00Z');
+  });
+  it('Perform a bool sort + Reverse + Remove', function() {
+    cy.loadAndVisitProject('food.sort');
 
-        cy.columnActionClick('Fat', ['Sort'])
+    cy.getCell(0, 'Fat')
+        .trigger('mouseover')
+        .within(() => {
+          cy.get('a.data-table-cell-edit').click();
+        });
+    cy.get('select').select('boolean');
+    cy.get('button').contains(new RegExp('Apply', 'g')).click();
 
-        cy.waitForDialogPanel()
-        cy.get('[type="radio"]').check('boolean')
-        cy.confirmDialogPanel()
+    cy.getCell(1, 'Fat')
+        .trigger('mouseover')
+        .within(() => {
+          cy.get('a.data-table-cell-edit').click();
+        });
+    cy.get('select').select('boolean');
+    cy.get('button').contains(new RegExp('Apply', 'g')).click();
 
-        cy.getCell(0, 'Fat').should('to.contain', 'false')
-        cy.getCell(1, 'Fat').should('to.contain', 'true')
+    cy.columnActionClick('Fat', ['Sort']);
 
-        cy.columnActionClick('Fat', ['Sort', 'Reverse'])
-        cy.getCell(0, 'Fat').should('to.contain', 'true')
-        cy.getCell(1, 'Fat').should('to.contain', 'false')
+    cy.waitForDialogPanel();
+    cy.get('[type="radio"]').check('boolean');
+    cy.confirmDialogPanel();
 
-        cy.columnActionClick('Fat', ['Sort', 'Remove sort'])
-        cy.getCell(0, 'Fat').should('to.contain', 'false')
-        cy.getCell(1, 'Fat').should('to.contain', 'true')
-    })
-})
+    cy.getCell(0, 'Fat').should('to.contain', 'false');
+    cy.getCell(1, 'Fat').should('to.contain', 'true');
+
+    cy.columnActionClick('Fat', ['Sort', 'Reverse']);
+    cy.getCell(0, 'Fat').should('to.contain', 'true');
+    cy.getCell(1, 'Fat').should('to.contain', 'false');
+
+    cy.columnActionClick('Fat', ['Sort', 'Remove sort']);
+    cy.getCell(0, 'Fat').should('to.contain', 'false');
+    cy.getCell(1, 'Fat').should('to.contain', 'true');
+  });
+});
--- main/tests/cypress/cypress/support/openrefine_api.js
+++ main/tests/cypress/cypress/support/openrefine_api.js
@@ -1,127 +1,136 @@
-const fixtures = require('../fixtures/fixtures.js')
+const fixtures = require('../fixtures/fixtures.js');
 
 Cypress.Commands.add('setPreference', (preferenceName, preferenceValue) => {
-    const openRefineUrl = Cypress.env('OPENREFINE_URL')
-    cy.request(openRefineUrl + '/command/core/get-csrf-token').then(
-        (response) => {
-            cy.request({
-                method: 'POST',
-                url: `${openRefineUrl}/command/core/set-preference`,
-                body: `name=${preferenceName}&value="${preferenceValue}"&csrf_token=${response.body.token}`,
-                form: false,
-                headers: {
-                    'content-type':
-                        'application/x-www-form-urlencoded; charset=UTF-8',
-                },
-            }).then((resp) => {
-                cy.log(
-                    'Set preference ' +
-                        preferenceName +
-                        ' with value ' +
-                        preferenceValue
-                )
-            })
-        }
-    )
-})
+  const openRefineUrl = Cypress.env('OPENREFINE_URL');
+  cy.request(openRefineUrl + '/command/core/get-csrf-token').then(
+      (response) => {
+        cy.request({
+          method: 'POST',
+          url: `${openRefineUrl}/command/core/set-preference`,
+          body: `name=${preferenceName}&value="${preferenceValue}"&csrf_token=${response.body.token}`,
+          form: false,
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          },
+        }).then((resp) => {
+          cy.log(
+              'Set preference ' + preferenceName + ' with value ' + preferenceValue,
+          );
+        });
+      },
+  );
+});
 
 Cypress.Commands.add('cleanupProjects', () => {
-    const openRefineUrl = Cypress.env('OPENREFINE_URL')
-    cy.get('@deletetoken', { log: false }).then((token) => {
-        cy.get('@loadedProjectIds', { log: false }).then((loadedProjectIds) => {
-            for (const projectId of loadedProjectIds) {
-                cy.request({
-                    method: 'POST',
-                    url:
-                        `${openRefineUrl}/command/core/delete-project?csrf_token=` +
-                        token,
-                    body: { project: projectId },
-                    form: true,
-                }).then((resp) => {
-                    cy.log('Deleted OR project' + projectId)
-                })
-            }
-        })
-    })
-})
+  const openRefineUrl = Cypress.env('OPENREFINE_URL');
+  cy.get('@deletetoken', {log: false}).then((token) => {
+    cy.get('@loadedProjectIds', {log: false}).then((loadedProjectIds) => {
+      for (const projectId of loadedProjectIds) {
+        cy.request({
+          method: 'POST',
+          url:
+            `${openRefineUrl}/command/core/delete-project?csrf_token=` + token,
+          body: {project: projectId},
+          form: true,
+        }).then((resp) => {
+          cy.log('Deleted OR project' + projectId);
+        });
+      }
+    });
+  });
+});
 
 Cypress.Commands.add('loadProject', (fixture, projectName, tagName) => {
-    const openRefineUrl = Cypress.env('OPENREFINE_URL')
-    const openRefineProjectName = projectName ? projectName : 'cypress-test'
+  const openRefineUrl = Cypress.env('OPENREFINE_URL');
+  const openRefineProjectName = projectName ? projectName : 'cypress-test';
 
-    let jsonFixture
-    if (typeof fixture == 'string') {
-        jsonFixture = fixtures[fixture]
+  let jsonFixture;
+  if (typeof fixture == 'string') {
+    jsonFixture = fixtures[fixture];
+  } else {
+    jsonFixture = fixture;
+  }
+
+  const csv = [];
+  jsonFixture.forEach((item) => {
+    csv.push('"' + item.join('","') + '"');
+  });
+  const content = csv.join('\n');
+
+  cy.get('@token', {log: false}).then((token) => {
+    // cy.request(Cypress.env('OPENREFINE_URL')+'/command/core/get-csrf-token').then((response) => {
+    const openRefineFormat = 'text/line-based/*sv';
+
+    // the following code can be used to inject tags in created projects
+    // It's conflicting though, breaking up the CSV files
+    // It is a hack to parse out CSV files in the openrefine while creating a project with tags
+    const options = {
+      encoding: 'US-ASCII',
+      separator: ',',
+      ignoreLines: -1,
+      headerLines: 1,
+      skipDataLines: 0,
+      limit: -1,
+      storeBlankRows: true,
+      guessCellValueTypes: false,
+      processQuotes: true,
+      quoteCharacter: '"',
+      storeBlankCellsAsNulls: true,
+      includeFileSources: false,
+      includeArchiveFileName: false,
+      trimStrings: false,
+      projectName: openRefineProjectName,
+      projectTags: [tagName],
+    };
+    let postData;
+    if (tagName == undefined) {
+      postData =
+        '------BOUNDARY\r\nContent-Disposition: form-data; name="project-file"; filename="' +
+        fixture +
+        '"\r\nContent-Type: "text/csv"\r\n\r\n' +
+        content +
+        '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="project-name"\r\n\r\n' +
+        openRefineProjectName +
+        '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="format"\r\n\r\n' +
+        openRefineFormat +
+        '\r\n------BOUNDARY--';
     } else {
-        jsonFixture = fixture
+      postData =
+        '------BOUNDARY\r\nContent-Disposition: form-data; name="project-file"; filename="' +
+        fixture +
+        '"\r\nContent-Type: "text/csv"\r\n\r\n' +
+        content +
+        '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="project-name"\r\n\r\n' +
+        openRefineProjectName +
+        '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="options"\r\n\r\n' +
+        JSON.stringify(options) +
+        '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="format"\r\n\r\n' +
+        openRefineFormat +
+        '\r\n------BOUNDARY--';
     }
 
-    const csv = []
-    jsonFixture.forEach((item) => {
-        csv.push('"' + item.join('","') + '"')
-    })
-    const content = csv.join('\n')
+    cy.request({
+      method: 'POST',
+      url:
+        `${openRefineUrl}/command/core/create-project-from-upload?csrf_token=` +
+        token,
+      body: postData,
+      headers: {
+        'content-type': 'multipart/form-data; boundary=----BOUNDARY',
+      },
+    }).then((resp) => {
+      const location = resp.allRequestResponses[0]['Response Headers'].location;
+      const projectId = location.split('=').slice(-1)[0];
+      cy.log('Created OR project', projectId);
 
-    cy.get('@token', { log: false }).then((token) => {
-        // cy.request(Cypress.env('OPENREFINE_URL')+'/command/core/get-csrf-token').then((response) => {
-        const openRefineFormat = 'text/line-based/*sv'
-
-        // the following code can be used to inject tags in created projects
-        // It's conflicting though, breaking up the CSV files
-        // It is a hack to parse out CSV files in the openrefine while creating a project with tags
-        const options = {"encoding":"US-ASCII","separator":",","ignoreLines":-1,"headerLines":1,"skipDataLines":0,"limit":-1,"storeBlankRows":true,"guessCellValueTypes":false,"processQuotes":true,"quoteCharacter":"\"","storeBlankCellsAsNulls":true,"includeFileSources":false,"includeArchiveFileName":false,"trimStrings":false,"projectName":openRefineProjectName,"projectTags": [tagName]}
-        var postData
-        if (tagName == undefined) {
-            postData =
-                '------BOUNDARY\r\nContent-Disposition: form-data; name="project-file"; filename="' +
-                fixture +
-                '"\r\nContent-Type: "text/csv"\r\n\r\n' +
-                content +
-                '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="project-name"\r\n\r\n' +
-                openRefineProjectName +
-                '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="format"\r\n\r\n' +
-                openRefineFormat +
-                '\r\n------BOUNDARY--'
-        } else {
-            postData =
-                '------BOUNDARY\r\nContent-Disposition: form-data; name="project-file"; filename="' +
-                fixture +
-                '"\r\nContent-Type: "text/csv"\r\n\r\n' +
-                content +
-                '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="project-name"\r\n\r\n' +
-                openRefineProjectName +
-                '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="options"\r\n\r\n' +
-                JSON.stringify(options) +
-                '\r\n------BOUNDARY\r\nContent-Disposition: form-data; name="format"\r\n\r\n' +
-                openRefineFormat +
-                '\r\n------BOUNDARY--'
-        }
-
-        cy.request({
-            method: 'POST',
-            url:
-                `${openRefineUrl}/command/core/create-project-from-upload?csrf_token=` +
-                token,
-            body: postData,
-            headers: {
-                'content-type': 'multipart/form-data; boundary=----BOUNDARY',
-            },
-        }).then((resp) => {
-            const location =
-                resp.allRequestResponses[0]['Response Headers'].location
-            const projectId = location.split('=').slice(-1)[0]
-            cy.log('Created OR project', projectId)
-
-            cy.get('@loadedProjectIds', { log: false }).then(
-                (loadedProjectIds) => {
-                    loadedProjectIds.push(projectId)
-                    cy.wrap(loadedProjectIds, { log: false })
-                        .as('loadedProjectIds')
-                        .then(() => {
-                            return projectId
-                        })
-                }
-            )
-        })
-    })
-})
+      cy.get('@loadedProjectIds', {log: false}).then((loadedProjectIds) => {
+        loadedProjectIds.push(projectId);
+        cy.wrap(loadedProjectIds, {log: false})
+            .as('loadedProjectIds')
+            .then(() => {
+              return projectId;
+            });
+      });
+    });
+  });
+});
