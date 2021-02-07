8f6d0f49fb2d04dd6e098ba64bc8f836285d128e
akshitasingh <singhakshita1210@gmail.com>
2021-02-07 21:28:24 +0530

    fix for #3089 i18n

--- main/webapp/modules/core/about.js
+++ main/webapp/modules/core/about.js
@@ -1,14 +1,19 @@
 $("#about-openrefine").text($.i18n('core-index/about')+" OpenRefine");
 $("#contributor").text($.i18n('core-index/contributor'));
 $("#definition").text($.i18n('core-index/definition'));
-// $("#history-1").text($.i18n('core-index/history-1'));
-// $("#history-2").text($.i18n('core-index/history-2'));
-// $("#history-3").text($.i18n('core-index/history-3'));
-// $("#thanks").text($.i18n('core-index/thanks'));
- let history_1 = $.i18n('core-index/history-1');
- let history_2 = $.i18n('core-index/history-2');
- let history_3 = $.i18n('core-index/history-3');
 
- let testContent =  `${history_1} + ${history_2} +${history_3}`;
-$("#history").text(textContent);
+const history_1 = document.createTextNode($.i18n('core-index/history-1'));
+let link_1 = document.createElement('a');
+link_1.setAttribute('href' ,"http://www.metaweb.com/")
+link_1.textContent ="http://www.metaweb.com/";
+const history_2 = document.createTextNode($.i18n('core-index/history-2'));
+let link_2 = document.createElement('a');
+link_2.setAttribute('href' ,"http://www.google.com/")
+link_2.textContent ="http://www.google.com/";
+const history_3 = document.createTextNode($.i18n('core-index/history-3'));
+ 
+$("#history-openrefine").append(history_1 ," " ,link_1, " " ,"Metaweb Technologies, Inc."," ",history_2," ",link_2," ","Google"," " ,history_3);
+
+$("#thanks").text($.i18n('core-index/thanks'));
+
 
