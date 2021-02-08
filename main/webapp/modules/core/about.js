9eff9ccf68bc7dbff829282b55750932de4d06e5
akshitasingh <singhakshita1210@gmail.com>
2021-02-08 21:41:05 +0530

    i18n fixed

--- main/webapp/modules/core/about.js
+++ main/webapp/modules/core/about.js
@@ -2,18 +2,7 @@ $("#about-openrefine").text($.i18n('core-index/about')+" OpenRefine");
 $("#contributor").text($.i18n('core-index/contributor'));
 $("#definition").text($.i18n('core-index/definition'));
 
-const history_1 = document.createTextNode($.i18n('core-index/history-1'));
-let link_1 = document.createElement('a');
-link_1.setAttribute('href' ,"http://www.metaweb.com/")
-link_1.textContent ="http://www.metaweb.com/";
-const history_2 = document.createTextNode($.i18n('core-index/history-2'));
-let link_2 = document.createElement('a');
-link_2.setAttribute('href' ,"http://www.google.com/")
-link_2.textContent ="http://www.google.com/";
-const history_3 = document.createTextNode($.i18n('core-index/history-3'));
- 
-$("#history-openrefine").append(history_1 ," " ,link_1, " " ,"Metaweb Technologies, Inc."," ",history_2," ",link_2," ","Google"," " ,history_3);
-
+$("#history-openrefine").html($.i18n('core-index/history' ,'http://www.metaweb.com/' , 'http://www.google.com/' ));
 $("#thanks").text($.i18n('core-index/thanks'));
 
 
