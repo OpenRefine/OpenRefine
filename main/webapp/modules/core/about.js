4c5ba90bf7b6a1de70e33fd781441a9a27fea3ff
akshitasingh <singhakshita1210@gmail.com>
2021-02-08 22:43:56 +0530

    fixes missing (i18n) internationalisation on about page #3089

---
+++ main/webapp/modules/core/about.js
@@ -0,0 +1,9 @@
+
+$("#about-openrefine").text($.i18n('core-index/about') + " OpenRefine");
+$("#contributor").text($.i18n('core-index/contributor'));
+$("#definition").text($.i18n('core-index/definition'));
+$("#history-openrefine").html($.i18n('core-index/history' ,'http://www.metaweb.com/' , 'http://www.google.com/' ));
+$("#thanks").text($.i18n('core-index/thanks'));
+
+
+ 
