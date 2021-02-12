9e30f8147852c0b4193c04f90aa6722c74699c35
S-Harshit <harshit19csu411@ncuindia.edu>
2021-02-12 18:27:43 +0530

    Made Changes As Suggested-m

--- main/tests/cypress/cypress/integration/project/grid/all-column/flag.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/all-column/flag.spec.js
@@ -1,34 +1,24 @@
 describe(__filename, function () {
   it('Ensure flag is visible and toggle on/off', function () {
     cy.loadAndVisitProject('food.mini');
-    cy.get('.odd > :nth-child(2) > .data-table-flag-off')
-      .should('exist')
+    cy.get('.data-table tr:nth-child(1) td:nth-child(2) a')
+      .should('have.class', 'data-table-flag-off')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Flag row 1'
-    );
-    cy.get('.even > :nth-child(2) > .data-table-flag-off')
-      .should('exist')
+    cy.assertNotificationContainingText('Flag row 1');
+    
+    cy.get('.data-table tr:nth-child(2) td:nth-child(2) a')
+      .should('have.class', 'data-table-flag-off')
+      .click();    
+    cy.assertNotificationContainingText('Flag row 2');
+    
+    cy.get('.data-table tr:nth-child(1) td:nth-child(2) a')
+      .should('have.class', 'data-table-flag-on')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Flag row 2'
-    );
-
-    cy.get('.odd > :nth-child(2) > .data-table-flag-on')
-      .should('exist')
-      .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Unflag row 1'
-    );
-    cy.get('.even > :nth-child(2) > .data-table-flag-on')
-      .should('exist')
-      .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Unflag row 2'
-    );
+    cy.assertNotificationContainingText('Unflag row 1');
+    
+    cy.get('.data-table tr:nth-child(2) td:nth-child(2) a')
+      .should('have.class', 'data-table-flag-on')
+      .click();  
+    cy.assertNotificationContainingText('Unflag row 2');
   });
 });
--- main/tests/cypress/cypress/integration/project/grid/all-column/star.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/all-column/star.spec.js
@@ -1,34 +1,24 @@
 describe(__filename, function () {
   it('Ensure flag is visible and toggle on/off', function () {
     cy.loadAndVisitProject('food.mini');
-    cy.get('.odd > :nth-child(1) > .data-table-star-off')
-      .should('exist')
+    cy.get('.data-table tr:nth-child(1) td:nth-child(1) a')
+      .should('have.class', 'data-table-star-off')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Star row 1'
-    );
-    cy.get('.even > :nth-child(1) > .data-table-star-off')
-      .should('exist')
+    cy.assertNotificationContainingText('Star row 1');
+    
+    cy.get('.data-table tr:nth-child(2) td:nth-child(1) a')
+      .should('have.class', 'data-table-star-off')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Star row 2'
-    );
+    cy.assertNotificationContainingText('Star row 2');
 
-    cy.get('.odd > :nth-child(1) > .data-table-star-on')
-      .should('exist')
+    cy.get('.data-table tr:nth-child(1) td:nth-child(1) a')
+      .should('have.class', 'data-table-star-on')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Unstar row 1'
-    );
-    cy.get('.even > :nth-child(1) > .data-table-star-on')
-      .should('exist')
+    cy.assertNotificationContainingText('Unstar row 1');
+    
+    cy.get('.data-table tr:nth-child(2) td:nth-child(1) a')
+      .should('have.class', 'data-table-star-on')
       .click();
-    cy.get('#notification > div:nth-child(2) > span:nth-child(1)').should(
-      'have.text',
-      'Unstar row 2'
-    );
+    cy.assertNotificationContainingText('Unstar row 2');
   });
 });
