19fb2306c7b59519342bad46f6634cfb6299c80e
Florian Giroud <florian.giroud@gmail.com>
2021-03-10 10:12:28 +0100

    wip split&join

--- main/tests/cypress/cypress/integration/project/grid/column/edit-cells/join-multi-valued-cells.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/column/edit-cells/join-multi-valued-cells.spec.js
@@ -1,8 +1,8 @@
 describe(__filename, function () {
-  it('Test a split by regex', function () {
+  it('Join cells', function () {
     // Load a splitted dataset, one number per row,
     // expect that the joined dataset would be 1234
-    const fixture = [['A column'], ['1'], ['2'], ['3'], ['4']];
+    const fixture = [['A column'], ['a,b,c'], ['b'], ['c'], ['d']];
     cy.loadAndVisitProject(fixture);
 
     cy.window().then(($win) => {
@@ -14,6 +14,6 @@ describe(__filename, function () {
       'Join multi-valued cells...',
     ]);
 
-    cy.assertCellEquals(0, 'A column', '1234');
+    cy.assertCellEquals(0, 'A column', 'abcdef');
   });
 });
--- main/tests/cypress/cypress/integration/project/grid/column/edit-cells/split-multi-valued-cells.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/column/edit-cells/split-multi-valued-cells.spec.js
@@ -13,13 +13,17 @@ describe(__filename, function () {
     ]);
     cy.get('.dialog-container label').contains('by separator').click();
     cy.get('.dialog-container input[bind="separatorInput"]').type('***');
-    cy.get('.dialog-container button[bind="okButton"]').click();
-    cy.assertCellEquals(0, 'A column', 'b_0_0');
-    cy.assertCellEquals(1, 'A column', 'b_0_1');
-    cy.assertCellEquals(2, 'A column', 'b_0_2');
-    cy.assertCellEquals(3, 'A column', 'b_1_0');
-    cy.assertCellEquals(4, 'A column', 'b_1_1');
-    cy.assertCellEquals(5, 'A column', 'b_1_2');
+    cy.confirmDialogPanel();
+
+    cy.assertGridEquals([
+      ['A column'],
+      ['b_0_0'],
+      ['b_0_1'],
+      ['b_0_2'],
+      ['b_1_0'],
+      ['b_1_1'],
+      ['b_1_2'],
+    ]);
   });
 
   it('Test a split by regex', function () {
@@ -33,13 +37,17 @@ describe(__filename, function () {
     cy.get('.dialog-container label').contains('by separator').click();
     cy.get('.dialog-container input[bind="separatorInput"]').type('\\d');
     cy.get('.dialog-container label').contains('regular expression').click();
-    cy.get('.dialog-container button[bind="okButton"]').click();
-    cy.assertCellEquals(0, 'A column', 'a');
-    cy.assertCellEquals(1, 'A column', 'b');
-    cy.assertCellEquals(2, 'A column', 'c');
-    cy.assertCellEquals(3, 'A column', 'd');
-    cy.assertCellEquals(4, 'A column', 'e');
-    cy.assertCellEquals(5, 'A column', 'f');
+    cy.confirmDialogPanel();
+
+    cy.assertGridEquals([
+      ['A column'],
+      ['a'],
+      ['b'],
+      ['c'],
+      ['d'],
+      ['e'],
+      ['f'],
+    ]);
   });
 
   it('Test a split by field length', function () {
@@ -52,61 +60,30 @@ describe(__filename, function () {
     cy.get('.dialog-container').should('to.contain', 'Split multi-valued');
     cy.get('.dialog-container textarea[bind="lengthsTextarea"]').type('2,4,8');
     cy.get('.dialog-container label').contains('by field lengths').click();
-    cy.get('.dialog-container button[bind="okButton"]').click();
+    cy.confirmDialogPanel();
     cy.assertCellEquals(0, 'A column', 'BU');
     cy.assertCellEquals(1, 'A column', 'TTER');
     cy.assertCellEquals(2, 'A column', ',WITH SA');
-  });
-  it('Test a split by field length (single number)', function () {
-    // each cell contains a number between letters, we will split by \d which is the regex for any int
-    const fixture = [['A column'], ['BUTTER,WITH SALT']];
-    cy.loadAndVisitProject(fixture);
-    cy.columnActionClick('A column', [
-      'Edit cells',
-      'Split multi-valued cells...',
-    ]);
-    cy.get('.dialog-container').should('to.contain', 'Split multi-valued');
-    cy.get('.dialog-container textarea[bind="lengthsTextarea"]').type('2');
-    cy.get('.dialog-container label').contains('by field lengths').click();
-    cy.get('.dialog-container button[bind="okButton"]').click();
-    cy.assertCellEquals(0, 'A column', 'BU');
-    cy.assertCellEquals(1, 'A column', 'TT');
-    cy.assertCellEquals(2, 'A column', 'ER');
-  });
 
-  it('Test a split by transition from lowercase to uppercase', function () {
-    const fixture = [['A column'], ['BuTtEr']];
-
-    cy.loadAndVisitProject(fixture);
-    cy.columnActionClick('A column', [
-      'Edit cells',
-      'Split multi-valued cells...',
-    ]);
-
-    cy.get('.dialog-container input[value="cases"]').check();
-    cy.get('.dialog-container button[bind="okButton"]').click();
-    cy.assertCellEquals(0, 'A column', 'B');
-    cy.assertCellEquals(1, 'A column', 'u');
-    cy.assertCellEquals(2, 'A column', 'T');
-    cy.assertCellEquals(3, 'A column', 't');
-    cy.assertCellEquals(4, 'A column', 'E');
-    cy.assertCellEquals(5, 'A column', 'r');
+    cy.assertGridEquals([['A column'], ['BU'], ['TTER'], [',WITH SA']]);
   });
 
   it('Test a split by transition from lowercase to uppercase (with the example from the UX)', function () {
-    const fixture = [['A column'], ['11Abc', 'Def22']];
+    const fixture = [['A column'], ['11AbcDef22']];
 
     cy.loadAndVisitProject(fixture);
     cy.columnActionClick('A column', [
       'Edit cells',
       'Split multi-valued cells...',
     ]);
+    cy.get('.dialog-container input[value="cases"]').check();
+    cy.confirmDialogPanel();
 
-    // what's the expected grid here ?
+    cy.assertGridEquals([['A column'], ['11Abc'], ['Def22']]);
   });
 
   it('Test a split by transition from numbers to letters', function () {
-    const fixture = [['A column'], ['11', 'AbcDef22']];
+    const fixture = [['A column'], ['Abcdef1Abcdef2Abcdef3']];
 
     cy.loadAndVisitProject(fixture);
     cy.columnActionClick('A column', [
@@ -114,7 +91,9 @@ describe(__filename, function () {
       'Split multi-valued cells...',
     ]);
 
-    // what's the expected grid here ?
+    cy.get('.dialog-container input[value="number"]').check();
+    cy.confirmDialogPanel();
+
+    cy.assertGridEquals([['A column'], ['Abcdef1'], ['Abcdef2'], ['Abcdef3']]);
   });
-  it('Test a split by transition from numbers to letters (adding "Reverse splitting order" option)', function () {});
 });
