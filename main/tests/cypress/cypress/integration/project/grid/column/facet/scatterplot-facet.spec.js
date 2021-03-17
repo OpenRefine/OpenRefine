f5120124e7dc47561d87a4f8dae20d8c51701c74
Florian Giroud <florian.giroud@gmail.com>
2021-03-17 14:19:41 +0100

    linting

--- main/tests/cypress/cypress/integration/project/grid/column/facet/scatterplot-facet.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/column/facet/scatterplot-facet.spec.js
@@ -3,34 +3,34 @@
  * It's using "text facet" as it is the most simple facet
  */
 describe(__filename, function () {
-	// it('Test and verify the matrix against a snapshot (3 integer columns)', function () {
-	// 	cy.loadAndVisitProject('food.small', 'food-small');
+  // it('Test and verify the matrix against a snapshot (3 integer columns)', function () {
+  // 	cy.loadAndVisitProject('food.small', 'food-small');
 
-	// 	cy.castColumnTo('Water', 'number');
-	// 	cy.castColumnTo('Energ_Kcal', 'number');
-	// 	cy.castColumnTo('Protein', 'number');
+  // 	cy.castColumnTo('Water', 'number');
+  // 	cy.castColumnTo('Energ_Kcal', 'number');
+  // 	cy.castColumnTo('Protein', 'number');
 
-	// 	cy.columnActionClick('Protein', ['Facet', 'Scatterplot facet']);
-	// 	cy.get('.scatterplot-matrix-table').matchImageSnapshot(
-	// 		'scatterplot-default'
-	// 	);
-	// });
+  // 	cy.columnActionClick('Protein', ['Facet', 'Scatterplot facet']);
+  // 	cy.get('.scatterplot-matrix-table').matchImageSnapshot(
+  // 		'scatterplot-default'
+  // 	);
+  // });
 
-	it('Visual testing for the lin/log options', function () {
-		cy.loadAndVisitProject('food.small', 'food-small');
+  it('Visual testing for the lin/log options', function () {
+    cy.loadAndVisitProject('food.small', 'food-small');
 
-		cy.castColumnTo('Water', 'number');
-		cy.castColumnTo('Energ_Kcal', 'number');
-		cy.castColumnTo('Protein', 'number');
+    cy.castColumnTo('Water', 'number');
+    cy.castColumnTo('Energ_Kcal', 'number');
+    cy.castColumnTo('Protein', 'number');
 
-		cy.columnActionClick('Protein', ['Facet', 'Scatterplot facet']);
+    cy.columnActionClick('Protein', ['Facet', 'Scatterplot facet']);
 
-		// test against the linear screenshot
-		cy.get('.scatterplot-selectors label').contains('lin').click();
-		cy.get('.scatterplot-matrix-table').matchImageSnapshot('scatterplot-lin');
+    // test against the linear screenshot
+    cy.get('.scatterplot-selectors label').contains('lin').click();
+    cy.get('.scatterplot-matrix-table').matchImageSnapshot('scatterplot-lin');
 
-		// test against the linear screenshot
-		cy.get('.scatterplot-selectors label').contains('log').click();
-		cy.get('.scatterplot-matrix-table').matchImageSnapshot('scatterplot-log');
-	});
+    // test against the linear screenshot
+    cy.get('.scatterplot-selectors label').contains('log').click();
+    cy.get('.scatterplot-matrix-table').matchImageSnapshot('scatterplot-log');
+  });
 });
--- main/tests/cypress/cypress/plugins/index.js
+++ main/tests/cypress/cypress/plugins/index.js
@@ -16,12 +16,12 @@
  */
 
 const {
-	addMatchImageSnapshotPlugin,
+  addMatchImageSnapshotPlugin,
 } = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
-	// `on` is used to hook into various events Cypress emits
-	// `config` is the resolved Cypress config
-	addMatchImageSnapshotPlugin(on, config);
-	return config;
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  addMatchImageSnapshotPlugin(on, config);
+  return config;
 };
