9f87f9dc90268b508fc44be0fbfeb14ea8d4bdb5
kushthedude <kushthedude@gmail.com>
2021-04-06 18:52:57 +0530

    fix
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- main/tests/cypress/cypress/integration/project/grid/all-column/view.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/all-column/view.spec.js
@@ -1,24 +1,24 @@
 describe(__filename, function () {
-    it('it collapses all columns', function () {
-      cy.loadAndVisitProject('food.mini');
-      cy.columnActionClick('All', ['View', 'Collapse all columns']);
+  it('it collapses all columns', function () {
+    cy.loadAndVisitProject('food.mini');
+    cy.columnActionClick('All', ['View', 'Collapse all columns']);
 
-      cy.get('.odd td:nth-child(4)').should('to.contain', "");
-      cy.get('.odd td:nth-child(5)').should('to.contain', "");
-      cy.get('.odd td:nth-child(6)').should('to.contain', "");
-      cy.get('.odd td:nth-child(7)').should('to.contain', "");
+    cy.get('.odd td:nth-child(4)').should('to.contain', '');
+    cy.get('.odd td:nth-child(5)').should('to.contain', '');
+    cy.get('.odd td:nth-child(6)').should('to.contain', '');
+    cy.get('.odd td:nth-child(7)').should('to.contain', '');
 
-      cy.get('.even td:nth-child(4)').should('to.contain', "");
-      cy.get('.even td:nth-child(5)').should('to.contain', "");
-      cy.get('.even td:nth-child(6)').should('to.contain', "");
-      cy.get('.even td:nth-child(7)').should('to.contain', "");
-    });
+    cy.get('.even td:nth-child(4)').should('to.contain', '');
+    cy.get('.even td:nth-child(5)').should('to.contain', '');
+    cy.get('.even td:nth-child(6)').should('to.contain', '');
+    cy.get('.even td:nth-child(7)').should('to.contain', '');
+  });
   it('it expands all columns', function () {
     cy.loadAndVisitProject('food.mini');
     cy.columnActionClick('All', ['View', 'Collapse all columns']);
 
-    cy.get('.odd td:nth-child(4)').should('to.contain', "");
-    cy.get('.even td:nth-child(7)').should('to.contain', "");
+    cy.get('.odd td:nth-child(4)').should('to.contain', '');
+    cy.get('.even td:nth-child(7)').should('to.contain', '');
 
     cy.columnActionClick('All', ['View', 'Expand all columns']);
 
