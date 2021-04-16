3bb58de5ce950461af0c0991696fb438cb13ce3c
Antonin Delpeuch <antonin@delpeuch.eu>
2021-04-16 14:15:49 +0200

    Preserve hash when removing trailing slash in URL

--- docs/static/js/fix-location.js
+++ docs/static/js/fix-location.js
@@ -4,5 +4,5 @@
  * https://github.com/facebook/docusaurus/issues/2394#issuecomment-630638096
  */
 if (window && window.location && window.location.pathname.endsWith('/') && window.location.pathname !== '/') {
-  window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1))
+  window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1) + window.location.hash)
 }
