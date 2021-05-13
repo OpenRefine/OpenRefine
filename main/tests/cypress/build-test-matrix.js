08f7582c48622cc14b209769270a4bb4d2a89028
Florian Giroud <florian.giroud@gmail.com>
2021-05-13 10:32:15 +0200

    updated matrix

--- main/tests/cypress/build-test-matrix.js
+++ main/tests/cypress/build-test-matrix.js
@@ -12,25 +12,39 @@ const glob = require('glob');
 const groups = [
 	{
 		specs: [
-			'cypress/integration/create-project/*.spec.js',
-			'cypress/integration/extensions/*.spec.js',
-			'cypress/integration/import-project/*.spec.js',
-			'cypress/integration/language/*.spec.js',
-			'cypress/integration/open-project/*.spec.js',
-			'cypress/integration/preferences/*.spec.js',
-			'cypress/integration/project-management/*.spec.js',
+			'cypress/integration/create-project/**/*.spec.js',
+			'cypress/integration/extensions/**/*.spec.js',
+			'cypress/integration/import-project/**/*.spec.js',
+			'cypress/integration/language/**/*.spec.js',
+			'cypress/integration/open-project/**/*.spec.js',
+			'cypress/integration/preferences/**/*.spec.js',
+			'cypress/integration/project-management/**/*.spec.js',
 		],
 	},
-	,
 	{
 		specs: [
-			'project/grid/all-column/*.spec.js',
-			'project/grid/column/edit-cells/*.spec.js',
+			'cypress/integration/project/grid/all-column/**/*.spec.js',
+			'cypress/integration/project/grid/column/**/*.spec.js',
+			'cypress/integration/project/grid/column/edit-cells/**/*.spec.js',
 		],
 	},
+	{
+		specs: [
+			'cypress/integration/project/grid/column/edit-column/**/*.spec.js',
+			'cypress/integration/project/grid/column/facet/**/*.spec.js',
+			'cypress/integration/project/grid/column/facet/reconcile/**/*.spec.js',
+		],
+	},
+	// {
+	// 	specs: [
+	// 		'cypress/integration/project/grid/column/transpose/*.spec.js',
+	// 		'cypress/integration/project/grid/column/facet/*.spec.js',
+	// 		'cypress/integration/project/grid/column/facet/reconcile.spec.js',
+	// 	],
+	// },
 ];
 
-const merged_groups = groups.map((group) => group.specs.join(','));
+const mergedGroups = groups.map((group) => group.specs.join(','));
 
 // step1 ,find files matched by existing groups
 const matchedFiles = [];
@@ -40,6 +54,7 @@ groups.forEach((group) => {
 		matchedFiles.push(...files);
 	});
 });
+// console.log('matched', matchedFiles);
 
 // step2 , add a last group that contains missed files
 const allSpecFiles = glob.sync(
@@ -54,13 +69,13 @@ for (file of allSpecFiles) {
 }
 
 // if (missedFiles.length) {
-// 	merged_groups.push(missedFiles.join(','));
+// 	mergedGroups.push(missedFiles.join(','));
 // }
 
-const browsers = process.env.browsers.split(',');
-
+// const browsers = process.env.browsers.split(',');
+const browsers = ['chrome'];
 console.log(
 	`::set-output name=matrix::{"browser":${JSON.stringify(
 		browsers
-	)}, "specs":${JSON.stringify(merged_groups)}}`
+	)}, "specs":${JSON.stringify(mergedGroups)}}`
 );
