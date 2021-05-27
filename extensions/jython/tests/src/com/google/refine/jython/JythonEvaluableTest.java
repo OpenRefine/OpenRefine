f8e88c45672bf5c36bd14268f643d2612cb299f1
LaiWang2020 <laiw13@uci.edu>
2021-05-28 07:18:15 +0800

    Cover PyObject branch in evaluate method

--- extensions/jython/tests/src/com/google/refine/jython/JythonEvaluableTest.java
+++ extensions/jython/tests/src/com/google/refine/jython/JythonEvaluableTest.java
@@ -22,57 +22,6 @@ import com.google.refine.model.Row;
 public class JythonEvaluableTest {
 
   // Reproduces the situation when result is a PyObject
-  public Object evaluatePyobjectTest(Properties bindings,JythonEvaluable eval1, String funcExpression) {
-    // Declear the functionName and engine
-    String s_functionName;
-    PythonInterpreter _engine;
-    File libPath = new File("webapp/WEB-INF/lib/jython");
-    if (!libPath.exists() && !libPath.canRead()) {
-      libPath = new File("main/webapp/WEB-INF/lib/jython");
-      if (!libPath.exists() && !libPath.canRead()) {
-        libPath = null;
-      }
-    }
-    if (libPath != null) {
-      Properties props = new Properties();
-      props.setProperty("python.path", libPath.getAbsolutePath());
-      PythonInterpreter.initialize(System.getProperties(), props, new String[] { "" });
-    }
-    _engine = new PythonInterpreter();
-
-    //get function and engine ready for __call__
-    String s = funcExpression;
-    s_functionName = String.format("__temp_%d__", Math.abs(s.hashCode()));
-
-    // indent and create a function out of the code
-    String[] lines = s.split("\r\n|\r|\n");
-    StringBuffer sb = new StringBuffer(1024);
-    sb.append("def ");
-    sb.append(s_functionName);
-    sb.append("(value, cell, cells, row, rowIndex):");
-    for (String line : lines) {
-      sb.append("\n  ");
-      sb.append(line);
-    }
-    _engine.exec(sb.toString());
-
-    try {
-      // call the temporary PyFunction directly
-      PyObject result = (_engine.get(s_functionName).__call__(
-              new PyObject[] {
-                      Py.java2py( bindings.get("value") ),
-                      new JythonHasFieldsWrapper((HasFields) bindings.get("cell"), bindings),
-                      new JythonHasFieldsWrapper((HasFields) bindings.get("cells"), bindings),
-                      new JythonHasFieldsWrapper((HasFields) bindings.get("row"), bindings),
-                      Py.java2py( bindings.get("rowIndex") )
-              }
-      ));
-      return eval1.unwrap(result);
-    } catch (PyException e) {
-      return new EvalError(e.toString());
-    }
-  }
-
   // Version with a test case which only calls the existing evaluate method
   @Test
   public void unwrapPyObjectTest(){
@@ -92,12 +41,9 @@ public class JythonEvaluableTest {
     props.put("call", "number");
     props.put("PI", "3.141592654");
     props.put("cells", new CellTuple(project, row));
-    //String funcExpression ="a = value\nreturn a * 2";
-    String funcExpression = "return [true, 13, {\"a\":3}]";
-
+    String funcExpression = "class Foo(object):\n      pass\n\nreturn Foo()";
     JythonEvaluable eval1 = new JythonEvaluable(funcExpression);
     eval1.evaluate(props);
-    //evaluatePyobjectTest(props,eval1,funcExpression);
 
   }
 
