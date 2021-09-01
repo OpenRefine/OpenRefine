aa2612e04bc9a9487a7076603b3aee0e12f3362b
Florian Giroud <florian.giroud@gmail.com>
2021-09-01 14:54:09 +0200

    Added Cypress tests for Clustering

--- main/tests/cypress/cypress/integration/project/grid/column/edit-cells/cluster-and-edit.spec.js
+++ main/tests/cypress/cypress/integration/project/grid/column/edit-cells/cluster-and-edit.spec.js
@@ -1,36 +1,214 @@
+const fixture = [
+  ['location'],
+  ['SWABYS HOME'],
+  ['SWABYS   HOME'],
+  ['BALLARDS river'],
+  ['BALLARDS River'],
+  ['MOUNT ZION'],
+  ['GRAVEL HILL'],
+];
+
 describe(__filename, function () {
-  it('Ensure cells are filled down', function () {
-    const fixture = [
-      ['location'],
-
-      ['SWABYS HOME'],
-      ['SWABYS   HOME'],
-      ['BALLARDS river'],
-      ['BALLARDS River'],
-      ['MOUNT ZION'],
-      ['GRAVEL HILL']
-      
-    ];
-
+  it('Test Layout, Popup & Close', function () {
     cy.loadAndVisitProject(fixture);
-    // // click
-    // cy.columnActionClick('b', ['Edit cells', 'Fill down']);
+    cy.columnActionClick('location', ['Edit cells', 'Cluster and edit']);
 
-    // // ensure notification and cell content
-    // cy.assertNotificationContainingText('Fill down 3 cells in column b');
-    // cy.assertCellEquals(0, 'b', '0b'); // untouched
-    // cy.assertCellEquals(1, 'b', '0b'); // filled
-    // cy.assertCellEquals(2, 'b', '2b'); // untouched
-    // cy.assertCellEquals(3, 'b', '2b'); // filled
-    // cy.assertCellEquals(4, 'b', '2b'); // filled
-    // cy.assertCellEquals(5, 'b', '5b'); // untouched
+    cy.get('.dialog-container').within(() => {
+      cy.get('.dialog-header').contains('Cluster & Edit column "location"');
+      cy.get('.dialog-body').contains(
+        'This feature helps you find groups of different cell values'
+      );
+      cy.get('.dialog-footer').contains('Export Clusters');
+    });
+
+    cy.get('.dialog-container .dialog-footer button[bind="closeButton"]')
+      .contains('Close')
+      .click();
+    cy.get('.dialog-container').should('not.exist');
   });
 
+  it('Test the footer buttons, Select/Unselect All', function () {
+    cy.loadAndVisitProject(fixture);
+    cy.columnActionClick('location', ['Edit cells', 'Cluster and edit']);
 
-  // Layout
-  // select all / unselect
+    cy.get('.dialog-container').within(() => {
+      cy.get('.dialog-footer button[bind="selectAllButton"]').click();
+      cy.get('.clustering-dialog-entry-table input[type="checkbox"]').should(
+        'be.checked'
+      );
+
+      cy.get('.dialog-footer button[bind="deselectAllButton"]').click();
+      cy.get('.clustering-dialog-entry-table input[type="checkbox"]').should(
+        'not.be.checked'
+      );
+    });
+  });
+
+  it('Test the different clustering options for rendering and expected inputs', function () {
+    cy.loadAndVisitProject(fixture);
+    cy.columnActionClick('location', ['Edit cells', 'Cluster and edit']);
+
+    // test the rendering of the main content when selecting different clustering methods
+    // the following code simply iterate over every clustering method (dropdow) and functions (ngram, levenshtein ...) to ensure the rendering is not broken
+    const methods = [
+      {
+        methodName: 'key collision',
+        selector: 'keyingFunctionSelector',
+        functions: [
+          'fingerprint',
+          'ngram-fingerprint',
+          'metaphone3',
+          'cologne-phonetic',
+          'Daitch-Mokotoff',
+          'Beider-Morse',
+        ],
+      },
+      {
+        methodName: 'nearest neighbor',
+        selector: 'distanceFunctionSelector',
+        functions: ['levenshtein', 'ppm'],
+      },
+    ];
+
+    cy.get('.dialog-container').within(() => {
+      cy.get('.clustering-dialog-entry-table')
+        .should('to.exist')
+        .should('be.visible');
+
+      // select a method
+      for (const method of methods) {
+        console.log(method);
+        cy.get('.clustering-dialog-entry-table')
+          .should('to.exist')
+          .should('be.visible');
+        cy.get(`.dialog-body select[bind="methodSelector"]`).select(
+          method['methodName']
+        );
+
+        // select each function
+        for (const functionName of method['functions']) {
+          cy.get(`select[bind="${method['selector']}"]`).select(functionName);
+          cy.get('.clustering-dialog-entry-table')
+            .should('to.exist')
+            .should('be.visible');
+          cy.get('.clustering-dialog-entry-table').should('be.visible');
+
+          // OR is adding &nbsp; for spaces, so we can't test BALLARDS River
+          // This assertion on text BALLARDS simply ensure the content has been rendered\
+          cy.get('.clustering-dialog-entry-table').should(
+            'to.contain',
+            'BALLARDS'
+          );
+        }
+      }
+    });
+  });
+
+  it('Merge Select & Close', function () {
+    cy.loadAndVisitProject(fixture);
+    cy.columnActionClick('location', ['Edit cells', 'Cluster and edit']);
+
+    cy.get('.dialog-container').within(() => {
+      // check lines to be merged
+      cy.get(
+        '.clustering-dialog-entry-table tr.odd input[type="checkbox"]'
+      ).check();
+
+      // enter a new cell value
+      cy.get('.clustering-dialog-entry-table tr.odd input[type="text"]').type(
+        'testing'
+      );
+
+      cy.get('.dialog-footer button[bind="applyCloseButton"]').click();
+    });
+
+    cy.assertGridEquals([
+      ['location'],
+      ['SWABYS HOME'],
+      ['SWABYS   HOME'],
+      ['BALLARDS Rivertesting'],
+      ['BALLARDS Rivertesting'],
+      ['MOUNT ZION'],
+      ['GRAVEL HILL'],
+    ]);
+  });
+
+  it('Merge Select & Re Cluster', function () {
+    cy.loadAndVisitProject(fixture);
+    cy.columnActionClick('location', ['Edit cells', 'Cluster and edit']);
+
+    cy.get('.dialog-container').within(() => {
+      // Merge BALLARDS RIVER
+      cy.get(
+        '.clustering-dialog-entry-table tr.odd input[type="checkbox"]'
+      ).check();
+
+      // enter a new cell value
+      cy.get('.clustering-dialog-entry-table tr.odd input[type="text"]').type(
+        ' TESTING A'
+      );
+
+      cy.get('.dialog-footer button[bind="applyReClusterButton"]').click();
+
+      // Re Cluster
+
+      // Merge Swabbys
+      cy.get(
+        '.clustering-dialog-entry-table tr.odd input[type="checkbox"]'
+      ).check();
+
+      // enter a new cell value
+      cy.get('.clustering-dialog-entry-table tr.odd input[type="text"]').type(
+        ' TESTING B'
+      );
+
+      cy.get('.dialog-footer button[bind="applyReClusterButton"]').click();
+
+      // Close
+
+      cy.get('.dialog-footer button[bind="closeButton"]')
+        .contains('Close')
+        .click();
+    });
+
+    cy.assertGridEquals([
+      ['location'],
+      ['SWABYS   HOME TESTING B'],
+      ['SWABYS   HOME TESTING B'],
+      ['BALLARDS River TESTING A'],
+      ['BALLARDS River TESTING A'],
+      ['MOUNT ZION'],
+      ['GRAVEL HILL'],
+    ]);
+  });
+
+  // https://github.com/OpenRefine/OpenRefine/issues/4004
+  it('Ensure duplicate whitespace are preserved, #4004', function () {
+    const fixture = [['test'], ['foo'], ['b ar'], ['b  ar'], ['b   ar']];
+
+    cy.loadAndVisitProject(fixture);
+    cy.columnActionClick('test', ['Edit cells', 'Cluster and edit']);
+
+    cy.get('.dialog-container').within(() => {
+      cy.get('.clustering-dialog-entry-table tr td:nth-child(3)').should(
+        'to.contain',
+        'b ar'
+      );
+      cy.get('.clustering-dialog-entry-table tr td:nth-child(3)').should(
+        'to.contain',
+        'b  ar'
+      );
+      cy.get('.clustering-dialog-entry-table tr td:nth-child(3)').should(
+        'to.contain',
+        'b   ar'
+      );
+    });
+  });
+
+  // ✓ Layout
+  // ✓ select all / unselect
   // Issue-4004
-  // Change method -> expected inputs
-  // Merge & Close (multiple merge)
-  // Merge & Re-cluster
+  // ✓ Change method -> expected inputs
+  // ✓ Merge & Close (multiple merge)
+  // ✓ Merge & Re-cluster
 });
