8a2f082a194db0f9ba7f5ad707c11de13c833b00
Florian Giroud <florian.giroud@gmail.com>
2021-09-17 14:50:10 +0200

    Attempt for flakiness on undo/redo -> apply

--- main/tests/cypress/cypress/integration/project/undo_redo/apply.spec.js
+++ main/tests/cypress/cypress/integration/project/undo_redo/apply.spec.js
@@ -2,12 +2,17 @@ describe(__filename, function () {
   it('Apply a JSON', function () {
     cy.loadAndVisitProject('food.mini');
 
-    cy.get('#or-proj-undoRedo').should('to.exist').should('be.visible').click();
-    cy.get('#refine-tabs-history').should('be.visible');
-    cy.get('#refine-tabs-history a[bind="applyLink"]').should('be.visible').click();
-
+    // Check some columns before the test
     cy.get('table.data-table thead th[title="Shrt_Desc"]').should('exist');
     cy.get('table.data-table thead th[title="Water"]').should('exist');
+
+
+    // find the "apply" button
+    cy.get('#or-proj-undoRedo').should('to.exist').should('be.visible').click();
+    cy.get('#refine-tabs-history').should('be.visible');
+    cy.get('#history-panel-controls').should('be.visible');
+    
+    cy.get('#refine-tabs-history a[bind="applyLink"]').should('be.visible').click();
 
     // JSON for operations that will be applied
     const operations = [
