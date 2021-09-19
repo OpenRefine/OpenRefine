bc80b4aeab9f12da3319f3b017eb64af111dec3d
mayralgr <ml17ft@gmail.com>
2021-09-18 21:42:30 -0500

    Add message no result when search does not return results

--- main/webapp/modules/core/scripts/index/open-project-ui.js
+++ main/webapp/modules/core/scripts/index/open-project-ui.js
@@ -285,7 +285,7 @@ Refine.OpenProjectUI.prototype._renderProjects = function(data) {
           
           return htmlDisplay;
       })() +     
-      '</tr></thead><tbody id="tableBody"></tbody></table>'
+      '</tr></thead><tbody id="tableBody"> </tbody></table>  <div id="no-results-message"> No Results </div>'
     ).appendTo(projectsUl)[0];
 
     var renderProject = function(project) {
@@ -365,7 +365,7 @@ Refine.OpenProjectUI.prototype._renderProjects = function(data) {
     for(var i in data)
     {
          if (data[i].display === true) {
-             appendMetaField(data[i].value); 
+             appendMetaField(data[i].value);
          }
     }
         
