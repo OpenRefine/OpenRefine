b3148cf4910142c2f0800cccd7d15a7787032888
jinouy2 <68868840+jinouy2@users.noreply.github.com>
2021-11-01 22:15:09 -0400

    Revert "Rudimentary Fix"
    This reverts commit b08ca22ee1952ad48f7e89856ab1945bc24cdd7b.

--- main/tests/server/src/com/google/refine/exporters/TsvExporterTests.java
+++ main/tests/server/src/com/google/refine/exporters/TsvExporterTests.java
@@ -23,8 +23,8 @@ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,           
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY           
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -33,13 +33,15 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package com.google.refine.exporters;
 
-import com.google.refine.RefineTest;
-import com.google.refine.browsing.Engine;
-import com.google.refine.model.Cell;
-import com.google.refine.model.Column;
-import com.google.refine.model.ModelException;
-import com.google.refine.model.Project;
-import com.google.refine.model.Row;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
+
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -47,14 +49,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Properties;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.google.refine.RefineTest;
+import com.google.refine.browsing.Engine;
+import com.google.refine.model.Cell;
+import com.google.refine.model.Column;
+import com.google.refine.model.ModelException;
+import com.google.refine.model.Project;
+import com.google.refine.model.Row;
 
 public class TsvExporterTests extends RefineTest {
 
@@ -74,7 +75,7 @@ public class TsvExporterTests extends RefineTest {
     CsvExporter SUT;
 
     @BeforeMethod
-    public void SetUp() {
+    public void SetUp(){
         SUT = new CsvExporter('\t');//new TsvExporter();
         writer = new StringWriter();
         project = new Project();
@@ -83,7 +84,7 @@ public class TsvExporterTests extends RefineTest {
     }
 
     @AfterMethod
-    public void TearDown() {
+    public void TearDown(){
         SUT = null;
         writer = null;
         project = null;
@@ -92,7 +93,7 @@ public class TsvExporterTests extends RefineTest {
     }
 
     @Test
-    public void exportSimpleTsv() {
+    public void exportSimpleTsv(){
         CreateGrid(2, 2);
 
         try {
@@ -102,13 +103,13 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "column0\tcolumn1\n" +
-                "row0cell0\trow0cell1\n" +
-                "row1cell0\trow1cell1\n");
+                                               "row0cell0\trow0cell1\n" +
+                                               "row1cell0\trow1cell1\n");
 
     }
 
     @Test
-    public void exportSimpleTsvNoHeader() {
+    public void exportSimpleTsvNoHeader(){
         CreateGrid(2, 2);
         when(options.getProperty("printColumnHeader")).thenReturn("false");
         try {
@@ -118,14 +119,14 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "row0cell0\trow0cell1\n" +
-                "row1cell0\trow1cell1\n");
+                                               "row1cell0\trow1cell1\n");
 
-        verify(options, times(2)).getProperty("printColumnHeader");
+        verify(options,times(2)).getProperty("printColumnHeader");
     }
 
     @Test
-    public void exportTsvWithLineBreaks() {
-        CreateGrid(3, 3);
+    public void exportTsvWithLineBreaks(){
+        CreateGrid(3,3);
 
         project.rows.get(1).cells.set(1, new Cell("line\n\n\nbreak", null));
         try {
@@ -135,14 +136,14 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "column0\tcolumn1\tcolumn2\n" +
-                "row0cell0\trow0cell1\trow0cell2\n" +
-                "row1cell0\t\"line\n\n\nbreak\"\trow1cell2\n" +
-                "row2cell0\trow2cell1\trow2cell2\n");
+                                               "row0cell0\trow0cell1\trow0cell2\n" +
+                                               "row1cell0\t\"line\n\n\nbreak\"\trow1cell2\n" +
+                                               "row2cell0\trow2cell1\trow2cell2\n");
     }
 
     @Test
-    public void exportTsvWithComma() {
-        CreateGrid(3, 3);
+    public void exportTsvWithComma(){
+        CreateGrid(3,3);
 
         project.rows.get(1).cells.set(1, new Cell("with\t tab", null));
         try {
@@ -152,14 +153,14 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "column0\tcolumn1\tcolumn2\n" +
-                "row0cell0\trow0cell1\trow0cell2\n" +
-                "row1cell0\t\"with\t tab\"\trow1cell2\n" +
-                "row2cell0\trow2cell1\trow2cell2\n");
+                                               "row0cell0\trow0cell1\trow0cell2\n" +
+                                               "row1cell0\t\"with\t tab\"\trow1cell2\n" +
+                                               "row2cell0\trow2cell1\trow2cell2\n");
     }
 
     @Test
-    public void exportTsvWithQuote() {
-        CreateGrid(3, 3);
+    public void exportTsvWithQuote(){
+        CreateGrid(3,3);
 
         project.rows.get(1).cells.set(1, new Cell("line has \"quote\"", null));
         try {
@@ -169,14 +170,14 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "column0\tcolumn1\tcolumn2\n" +
-                "row0cell0\trow0cell1\trow0cell2\n" +
-                "row1cell0\t\"line has \"\"quote\"\"\"\trow1cell2\n" +
-                "row2cell0\trow2cell1\trow2cell2\n");
+                                               "row0cell0\trow0cell1\trow0cell2\n" +
+                                               "row1cell0\t\"line has \"\"quote\"\"\"\trow1cell2\n" +
+                                               "row2cell0\trow2cell1\trow2cell2\n");
     }
 
     @Test
-    public void exportTsvWithEmptyCells() {
-        CreateGrid(3, 3);
+    public void exportTsvWithEmptyCells(){
+        CreateGrid(3,3);
 
         project.rows.get(1).cells.set(1, null);
         project.rows.get(2).cells.set(0, null);
@@ -187,15 +188,15 @@ public class TsvExporterTests extends RefineTest {
         }
 
         Assert.assertEquals(writer.toString(), "column0\tcolumn1\tcolumn2\n" +
-                "row0cell0\trow0cell1\trow0cell2\n" +
-                "row1cell0\t\trow1cell2\n" +
-                "\trow2cell1\trow2cell2\n");
+                                               "row0cell0\trow0cell1\trow0cell2\n" +
+                                               "row1cell0\t\trow1cell2\n" +
+                                               "\trow2cell1\trow2cell2\n");
     }
 
     //helper methods
 
-    protected void CreateColumns(int noOfColumns) {
-        for (int i = 0; i < noOfColumns; i++) {
+    protected void CreateColumns(int noOfColumns){
+        for(int i = 0; i < noOfColumns; i++){
             try {
                 project.columnModel.addColumn(i, new Column(i, "column" + i), true);
                 project.columnModel.columns.get(i).getCellIndex();
@@ -205,12 +206,12 @@ public class TsvExporterTests extends RefineTest {
         }
     }
 
-    protected void CreateGrid(int noOfRows, int noOfColumns) {
+    protected void CreateGrid(int noOfRows, int noOfColumns){
         CreateColumns(noOfColumns);
 
-        for (int i = 0; i < noOfRows; i++) {
+        for(int i = 0; i < noOfRows; i++){
             Row row = new Row(noOfColumns);
-            for (int j = 0; j < noOfColumns; j++) {
+            for(int j = 0; j < noOfColumns; j++){
                 row.cells.add(new Cell("row" + i + "cell" + j, null));
             }
             project.rows.add(row);
