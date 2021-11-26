047b990145cc75d4971f1e463f0627c347702edb
johnvl2 <90931628+johnvl2@users.noreply.github.com>
2021-11-26 15:36:21 -0500

    Add ability to disable the refreshing of import previews (#4309)
    * [#4009] Added checkbox to disable automatic preview updates for separator based files
    
    * [#4009] Checkbox now added for most parser types
    
    * [#4009] Google data and database additions for disabling auto preview
    
    * [#4009] Excel and pc axis fixes
    
    * [#4009] Test case addition
    
    * [#4009] Added comments
    
    * [#4009] Review comment fixes

--- main/webapp/modules/core/scripts/index/parser-interfaces/xml-parser-ui.js
+++ main/webapp/modules/core/scripts/index/parser-interfaces/xml-parser-ui.js
@@ -95,6 +95,8 @@ Refine.XmlParserUI.prototype.getOptions = function() {
   options.includeFileSources = this._optionContainerElmts.includeFileSourcesCheckbox[0].checked;
   options.includeArchiveFileName = this._optionContainerElmts.includeArchiveFileCheckbox[0].checked;
 
+  options.disableAutoPreview = this._optionContainerElmts.disableAutoPreviewCheckbox[0].checked;
+
   return options;
 };
 
@@ -108,6 +110,7 @@ Refine.XmlParserUI.prototype._initialize = function() {
 
   this._optionContainerElmts.pickRecordElementsButton.html($.i18n('core-buttons/pick-record'));
   this._optionContainerElmts.previewButton.html($.i18n('core-buttons/update-preview'));
+  $('#or-disable-auto-preview').text($.i18n('core-index-parser/disable-auto-preview'));
   $('#or-import-rows').text($.i18n('core-index-parser/rows-data'));
   $('#or-import-load').text($.i18n('core-index-parser/load-at-most'));
   $('#or-import-preserve').text($.i18n('core-index-parser/preserve-empty'));
@@ -140,8 +143,16 @@ Refine.XmlParserUI.prototype._initialize = function() {
     self._showPickRecordElementsUI();
   });
 
+  if (this._config.disableAutoPreview) {
+    this._optionContainerElmts.disableAutoPreviewCheckbox.prop('checked', true);
+  }
+
+  // If disableAutoPreviewCheckbox is not checked, we will schedule an automatic update
   var onChange = function() {
-    self._scheduleUpdatePreview();
+    if (!self._optionContainerElmts.disableAutoPreviewCheckbox[0].checked)
+    {
+        self._scheduleUpdatePreview();
+    }
   };
   this._optionContainer.find("input").bind("change", onChange);
   this._optionContainer.find("select").bind("change", onChange);
