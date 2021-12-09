e7e9c3ba34e87cfbfd38d62ddd800cbf57f426dc
Antonin Delpeuch <antonin@delpeuch.eu>
2021-12-09 09:39:34 +0200

    Fix spurious test failure in local runner test suite

--- or-localrunner/src/test/java/org/openrefine/model/LocalDatamodelRunnerTests.java
+++ or-localrunner/src/test/java/org/openrefine/model/LocalDatamodelRunnerTests.java
@@ -2,6 +2,8 @@
 package org.openrefine.model;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -18,7 +20,13 @@ public class LocalDatamodelRunnerTests extends DatamodelRunnerTestBase {
 
     @Override
     public DatamodelRunner getDatamodelRunner() throws IOException {
-        return new LocalDatamodelRunner();
+        Map<String, String> map = new HashMap<>();
+        // these values are purposely very low for testing purposes,
+        // so that we can check the partitioning strategy without using large files
+        map.put("minSplitSize", "128");
+        map.put("maxSplitSize", "1024");
+        RunnerConfiguration runnerConf = new RunnerConfigurationImpl(map);
+        return new LocalDatamodelRunner(runnerConf);
     }
 
     @Test
--- or-localrunner/src/test/java/org/openrefine/model/local/PLLTestsBase.java
+++ or-localrunner/src/test/java/org/openrefine/model/local/PLLTestsBase.java
@@ -7,9 +7,12 @@ import java.util.concurrent.Executors;
 
 import com.google.common.util.concurrent.MoreExecutors;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import org.openrefine.io.OrderedLocalFileSystem;
 
 public class PLLTestsBase {
 
@@ -21,12 +24,15 @@ public class PLLTestsBase {
             Configuration conf = new Configuration();
             // these values are purposely very low for testing purposes,
             // so that we can check the partitioning strategy without using large files
+            conf.set("fs.file.impl", OrderedLocalFileSystem.class.getName());
             conf.setLong("mapreduce.input.fileinputformat.split.minsize", 128L);
             conf.setLong("mapreduce.input.fileinputformat.split.maxsize", 1024L);
+            FileSystem fileSystem = LocalFileSystem.get(conf);
+
             context = new PLLContext(
                     MoreExecutors.listeningDecorator(
                             Executors.newCachedThreadPool()),
-                    LocalFileSystem.get(conf),
+                    fileSystem,
                     4);
         }
     }
