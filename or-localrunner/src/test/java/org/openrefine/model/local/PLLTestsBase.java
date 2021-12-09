a0df5494b541fc3ae71c0cefd49c1a821c45261d
Antonin Delpeuch <antonin@delpeuch.eu>
2021-12-09 09:39:34 +0200

    Fix spurious test failure in local runner test suite

--- or-localrunner/src/test/java/org/openrefine/model/local/PLLTestsBase.java
+++ or-localrunner/src/test/java/org/openrefine/model/local/PLLTestsBase.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.concurrent.Executors;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
+import org.openrefine.io.OrderedLocalFileSystem;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -21,12 +23,15 @@ public class PLLTestsBase {
             Configuration conf = new Configuration();
             // these values are purposely very low for testing purposes,
             // so that we can check the partitioning strategy without using large files
+            conf.set("fs.file.impl", OrderedLocalFileSystem.class.getName());
             conf.setLong("mapreduce.input.fileinputformat.split.minsize", 128L);
             conf.setLong("mapreduce.input.fileinputformat.split.maxsize", 1024L);
-            context = new PLLContext(
+            FileSystem fileSystem = LocalFileSystem.get(conf);
+
+			context = new PLLContext(
                     MoreExecutors.listeningDecorator(
                     Executors.newCachedThreadPool()),
-                    LocalFileSystem.get(conf),
+                    fileSystem,
                     4);
         }
     }
