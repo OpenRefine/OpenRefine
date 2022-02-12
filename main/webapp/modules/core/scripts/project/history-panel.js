879da1f2f0b30b4c507758e52807245568f96b7e
Nick Tzavidas <nikolaostzavidas@hotmail.gr>
2022-02-12 22:20:02 +0200

    Extraction of the operation history list as a downloadable JSON file (#4523)
    Co-authored-by: nicktz1408 <nikolaostzavidas@outlook.com>

--- main/webapp/modules/core/scripts/project/history-panel.js
+++ main/webapp/modules/core/scripts/project/history-panel.js
@@ -199,6 +199,7 @@ HistoryPanel.prototype._showExtractOperationsDialog = function(json) {
   elmts.or_proj_extractSave.html($.i18n('core-project/extract-save'));
   elmts.selectAllButton.html($.i18n('core-buttons/select-all'));
   elmts.unselectAllButton.html($.i18n('core-buttons/unselect-all'));
+  elmts.saveJsonAsFileButton.html($.i18n('core-buttons/export'))
   elmts.closeButton.html($.i18n('core-buttons/close'));
 
   var entryTable = elmts.entryTable[0];
@@ -254,6 +255,25 @@ HistoryPanel.prototype._showExtractOperationsDialog = function(json) {
     frame.find('input[type="checkbox"]').prop('checked', false);
     updateJson();
   });
+  elmts.saveJsonAsFileButton.click(function() {
+    var historyJson = elmts.textarea[0].value;
+
+    downloadFile('history.json', historyJson);
+  });
+
+  // Function originally created by Matěj Pokorný at StackOverflow:
+  // https://stackoverflow.com/a/18197341/5564816
+  var downloadFile = function(filename, content) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+    document.body.removeChild(element);
+  }
 
   var level = DialogSystem.showDialog(frame);
 
