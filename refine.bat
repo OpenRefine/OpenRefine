aeb0bb619aec17333f9182bc531f4d642294c6ec
elebitzero <42903164+elebitzero@users.noreply.github.com>
2022-03-24 04:58:14 -0700

    Frontend dependency management part II (#4621)
    Part II for #2423.
    Partial fix for #3958.

--- refine.bat
+++ refine.bat
@@ -272,7 +272,15 @@ echo   http://bit.ly/1c2gkR
 echo.
 :gotMvnHome
 set MVN_ACTION=""%ACTION%""
-if ""%ACTION%"" == ""build"" set MVN_ACTION=compile test-compile dependency:build-classpath
+if ""%ACTION%"" == ""build"" goto :build-setup
+goto :endif
+:build-setup
+pushd main\webapp 
+call npm install
+popd
+set MVN_ACTION=compile test-compile dependency:build-classpath
+:endif
+
 if ""%ACTION%"" == ""test"" set MVN_ACTION=test dependency:build-classpath
 if ""%ACTION%"" == ""server_test"" set MVN_ACTION=test -f main
 if ""%ACTION%"" == ""extensions_test"" set MVN_ACTION=test -f extensions
