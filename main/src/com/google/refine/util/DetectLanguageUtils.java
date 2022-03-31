c66934cda17bf96e66f6df705ce5af7427847a19
Elroy Kanye <elroykanye@gmail.com>
2022-03-31 22:50:40 +0100

    set the languageProfiles to load just once

--- main/src/com/google/refine/util/DetectLanguageUtils.java
+++ main/src/com/google/refine/util/DetectLanguageUtils.java
@@ -1,6 +1,5 @@
 package com.google.refine.util;
 
-
 import com.google.common.base.Optional;
 import com.optimaize.langdetect.LanguageDetector;
 import com.optimaize.langdetect.LanguageDetectorBuilder;
@@ -16,10 +15,17 @@ import java.io.IOException;
 import java.util.List;
 
 public class DetectLanguageUtils {
+    private static List<LanguageProfile> languageProfiles;
+
+    static {
+        try {
+            languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Optional<LdLocale> detect(String text) throws IOException {
-        List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
-
         // build language detector
         LanguageDetector languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
                 .withProfiles(languageProfiles)
@@ -27,7 +33,6 @@ public class DetectLanguageUtils {
 
         // create a text object factory
         TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText();
-
 
         // query the text for detection
         TextObject textObject = textObjectFactory.forText(text);
