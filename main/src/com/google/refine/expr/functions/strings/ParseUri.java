5a5d4df52470dd62e94a804f362a298b8ea02dc7
Elroy Kanye <elroykanye@gmail.com>
2022-04-06 14:11:15 +0100

    mod ParseUri to return JSON object
    mod ParseUri to return JSON object

--- main/src/com/google/refine/expr/functions/strings/ParseUri.java
+++ main/src/com/google/refine/expr/functions/strings/ParseUri.java
@@ -1,7 +1,10 @@
 package com.google.refine.expr.functions.strings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.refine.expr.EvalError;
 import com.google.refine.grel.Function;
+import com.google.refine.util.ParsingUtilities;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -10,6 +13,10 @@ import java.net.URL;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * Parse a URI string into its components and returns a JSON object with the following keys:
+ * scheme, authority, path, query, fragment, host, port.
+ */
 public class ParseUri implements Function {
     @Override
     public Object call(Properties bindings, Object[] args) {
@@ -19,7 +26,8 @@ public class ParseUri implements Function {
                 URL url = new URL(s);
                 URI uri = url.toURI();
 
-                return Map.of(
+                ObjectMapper objectMapper = new ObjectMapper();
+                return ParsingUtilities.mapper.readTree(objectMapper.writeValueAsString(Map.of(
                         "scheme", uri.getScheme() == null ? "" : uri.getScheme(),
                         "host", uri.getHost() == null ? "" : uri.getHost(),
                         "port", String.valueOf(uri.getPort()),
@@ -27,9 +35,11 @@ public class ParseUri implements Function {
                         "query", uri.getQuery() == null ? "" : uri.getQuery(),
                         "fragment", uri.getFragment() == null ? "" : uri.getFragment(),
                         "authority", uri.getAuthority() == null ? "" : uri.getAuthority()
-                );
+                )));
             } catch (URISyntaxException | MalformedURLException e) {
                 return new EvalError("Invalid URI: " + s);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
             }
         }
         return new EvalError("ParseUri takes a single string argument");
@@ -41,7 +51,12 @@ public class ParseUri implements Function {
     }
 
     @Override
+    public String getParams() {
+        return "String url";
+    }
+
+    @Override
     public String getReturns() {
-        return "Map m";
+        return "JSON object";
     }
 }
