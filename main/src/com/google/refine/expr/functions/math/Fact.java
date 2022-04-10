88ee8acd89065c797c1a0bfe1a7bec448d8c86a7
elebitzero <42903164+elebitzero@users.noreply.github.com>
2022-04-10 01:09:44 -0700

    Improved error handling for pow and exp (#4701)
    * Removed unneeded null checks from math functions, added test for math functions, and added error messages for pow and exp.
    
    * Cleanup.
    
    * Cleanup/reformatting.
    
    * Cleanup/reformatting.

--- main/src/com/google/refine/expr/functions/math/Fact.java
+++ main/src/com/google/refine/expr/functions/math/Fact.java
@@ -43,7 +43,7 @@ public class Fact implements Function {
 
     @Override
     public Object call(Properties bindings, Object[] args) {
-        if (args.length == 1 && args[0] != null && args[0] instanceof Number) {
+        if (args.length == 1 && args[0] instanceof Number) {
             return FactN.factorial(((Number) args[0]).intValue(), 1);
         }
         return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expects a number");
