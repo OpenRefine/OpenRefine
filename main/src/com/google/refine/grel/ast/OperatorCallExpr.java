2ef33d5bd24c0403c6ad29855819d9078b1ce2c1
Elroy Kanye <elroykanye@gmail.com>
2022-04-11 11:51:44 +0100

    adjusted formatting - OperatorCallExpr

--- main/src/com/google/refine/grel/ast/OperatorCallExpr.java
+++ main/src/com/google/refine/grel/ast/OperatorCallExpr.java
@@ -74,7 +74,9 @@ public class OperatorCallExpr implements Evaluable {
                     } else if ("*".equals(_op)) {
                         return n1 * n2;
                     } else if ("/".equals(_op)) {
-                        if(n2 == 0 && n1 == 0) { return Double.NaN; }
+                        if (n2 == 0 && n1 == 0) {
+                            return Double.NaN;
+                        }
                         return n1 / n2;
                     } else if ("%".equals(_op)) {
                         return n1 % n2;
@@ -102,7 +104,9 @@ public class OperatorCallExpr implements Evaluable {
                     } else if ("*".equals(_op)) {
                         return n1 * n2;
                     } else if ("/".equals(_op)) {
-                        if(n2 == 0&& n1 == 0) { return Double.NaN; }
+                        if (n2 == 0 && n1 == 0) {
+                            return Double.NaN;
+                        }
                         return n1 / n2;
                     } else if ("%".equals(_op)) {
                         return n1 % n2;
