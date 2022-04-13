d91c7def90808c9d813102a4a8a70967d80d9ef3
WaltonG <waltongoga@gmail.com>
2022-04-13 14:27:51 +0300

    Removed try catch

--- main/src/com/google/refine/importers/tree/XmlImportUtilities.java
+++ main/src/com/google/refine/importers/tree/XmlImportUtilities.java
@@ -1,12 +1,9 @@
 /*
-
 Copyright 2010, Google Inc.
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
-
     * Redistributions of source code must retain the above copyright
 notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above
@@ -16,7 +13,6 @@ distribution.
     * Neither the name of Google Inc. nor the names of its
 contributors may be used to endorse or promote products derived from
 this software without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -28,7 +24,6 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 */
 
 package com.google.refine.importers.tree;
@@ -374,11 +369,7 @@ public class XmlImportUtilities extends TreeImportUtilities {
                             }
                         }
                     }
-<<<<<<< HEAD
-=======
-                } catch (TreeReaderException e) {
-                    throw new TreeReaderException("Exception from parsing: " + e.getLocalizedMessage());
->>>>>>> b26142025c5f67f01a71644003bc88ce2512ecb5
+
                 }
 
             } else {
