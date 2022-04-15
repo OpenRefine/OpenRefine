dda594203f59b502edda3bbba78307044072b0da
Babi B <babibyoolah@gmail.com>
2022-04-15 01:06:46 +0100

    changed localization key

--- main/webapp/modules/core/scripts/project/browsing-engine.js
+++ main/webapp/modules/core/scripts/project/browsing-engine.js
@@ -99,7 +99,7 @@ BrowsingEngine.prototype._initializeUI = function() {
     '<h1>'+$.i18n('core-project/use-facets')+'</h1>' +
     '<p>'+$.i18n('core-project/use-to-select')+'</p>' +
     '<p>'+$.i18n('core-project/not-sure')+'<br /><a href="https://github.com/OpenRefine/OpenRefine/wiki/Screencasts" target="_blank"><b>'+$.i18n('core-project/watch-cast')+'</b></a></p>' +
-    '<button class="close-btn" bind="close">'+$.i18n('core-project/discard')+'</button>'+
+    '<button class="close-btn" bind="close">'+$.i18n('core-buttons/close')+'</button>'+
     '</div>' +
     '<div class="browsing-panel-header" bind="header">' +
     '<div class="browsing-panel-errors" bind="errors"></div>' +
--- main/webapp/modules/core/scripts/project/history-panel.js
+++ main/webapp/modules/core/scripts/project/history-panel.js
@@ -82,7 +82,7 @@ HistoryPanel.prototype._render = function() {
   elmts.extractLink.html($.i18n('core-project/extract'));
   elmts.or_proj_mistakes.html($.i18n('core-project/mistakes'));
   elmts.or_proj_filter.html($.i18n('core-project/filter'));
-  $("#close-history-panel").text($.i18n('core-project/discard'));
+  $("#close-history-panel").text($.i18n('core-buttons/close'));
   $("#close-history-panel").click(function(){ $('.history-panel-help').hide()});
 
   var renderEntry = function(container, index, entry, lastDoneID, past) {
