7a3aa966fd3091320f68dabe65f414b198ece700
Antoine Beaubien <antoine@beaubien.qc.ca>
2022-04-16 15:18:55 -0400

    #2 Refactor: Promises & API

--- main/webapp/modules/core/scripts/preferences.js
+++ main/webapp/modules/core/scripts/preferences.js
@@ -35,9 +35,97 @@ var Refine = {};
 var Core = {};
 var Languages = {};
 var Preferences = {};
+var API = { CORE: {} }; 
 
-// Core.Debugging = false;
+Core.Debugging = false;
 
+
+API.EncodeQueryData = function(queryData) {
+  return Object.entries(queryData).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+}
+
+API.GET = function(url, queryData) {
+  return new Promise((resolve, reject) => {
+    $.get(url, queryData, function( response, textStatus, jqXHR ) { resolve( jqXHR ) }, "json" )
+      .fail(function( jqXHR, textStatus, errorThrown ) {
+        if(typeof errorThrown != "object") { errorThrown = new Error(errorThrown); }
+        
+        errorThrown.jqXHR = jqXHR; 
+        errorThrown.textStatus = textStatus;          
+        reject(errorThrown);
+      });
+  });
+}
+
+API.CORE.GetCommand = function(command, queryData) {
+  return API.GET("command/core/"+ command, queryData);
+}
+
+API.CORE.GetCsrfToken = function() {
+  return new Promise((resolve, reject) => {
+    API.CORE.GetCommand( "get-csrf-token", {} )
+      .then( (jqXHR) => { resolve(jqXHR.response['token']); } )
+      .catch( (err) => { reject(err); } );
+  })
+}
+
+API.POST = function(url, queryData, postData) {
+  return new Promise((resolve, reject) => {
+    fullUrl = queryData ? url +"?"+ API.EncodeQueryData(queryData) : url;
+    
+    $.post(url, postData, function( response, textStatus, jqXHR ) { resolve( jqXHR ) }, "json" )
+			.fail(function( jqXHR, textStatus, errorThrown ) {
+				if(typeof errorThrown != "object") { errorThrown = new Error(errorThrown); }
+		
+				errorThrown.jqXHR = jqXHR; 
+				errorThrown.textStatus = textStatus;          
+				reject(errorThrown);
+			});
+	});
+}
+
+API.CORE.PostCommand = function(command, queryData, postData) {
+  return API.POST("command/core/"+ command, queryData, postData);
+}
+
+API.CORE.PostCommandCsrf = function(command, queryData, postData) {
+  return new Promise((resolve, reject) => {
+    API.CORE.GetCsrfToken()
+      .then( (token) => {
+        if (typeof postData == 'string') {
+          postData += "&" + $.param({csrf_token: token});
+        } else {
+          postData['csrf_token'] = token;
+        }
+
+        API.PostCommand(command, queryData, postData)
+          .then( (resultPostData) => { resolve(resultPostData); } )
+          .catch( (err) => { reject(err); } ); 
+      }) 
+      .catch(  (err) => { reject(err); } );
+  });
+}
+
+API.CORE.GetAllPreferences = function() {
+  return new Promise((resolve, reject) => {
+    API.CORE.PostCommand( "get-all-preferences", {} )
+      .then( (jqXHR) => { resolve(jqXHR.response); } )
+      .catch( (err) => { reject(err); } );
+  })
+}
+
+API.CORE.SetPreferences = function(key, newValue) {
+  return new Promise((resolve, reject) => {
+    API.CORE.PostCommandCsrf( "set-preference", $.param({ name: key }), { value: JSON.stringify(newValue) } )
+      .then( (jqXHR) => { resolve(); } )
+      .catch( (err) => { reject(err); } );
+  });
+}
+
+
+
+
+/*
 // Requests a CSRF token and calls the supplied callback
 // with the token
 Core.wrapCSRF = function(onCSRF) {
@@ -68,11 +156,17 @@ Core.postCSRF = function(url, data, success, dataType, failCallback) {
       if (failCallback !== undefined) { req.fail(failCallback); }
    });
 };
+*/
 
 Core.i18n = function(key, defaultValue) {
+  // const errNoKey = $.i18n("key");
+  // const errNoKey = $.i18n("key");
+
+  if(!key && Core.Debugging) { console.log("Error: Core.i18n() failed. No key."); }
+
   var errorMessage = $.i18n(key);
   
-  if(!errorMessage && errorMessage != key) {
+  if(!errorMessage || errorMessage == key) {
     if(Core.Debugging) { console.log("Error: $.i18n() failed. No key: "+ key); }
     
     errorMessage = defaultValue ? defaultValue : key;
@@ -83,15 +177,23 @@ Core.i18n = function(key, defaultValue) {
 
 Core.alertDialog = function(alertText) {
   window.alert(alertText);
+  debugger;
 }
 
-Preferences.loadAll = function() { 
-  $.post(
-      "command/core/get-all-preferences",
-      null,
-      function(data) { Preferences.values = data; populatePreferences(); },
-      "json"
-  );
+Preferences.Load = function() {
+  return new Promise((resolve, reject) => {
+    API.CORE.GetAllPreferences()
+      .then( (data) => { 
+        Preferences.values = data; 
+        resolve(data); 
+      })
+			.catch( (err) => {
+			  console.log(err);
+				var errorMessage = Core.i18n('core-index/prefs-loading-failed', err.textStatus +':'+ err.errorThrown);
+				Core.alertDialog(errorMessage);
+				reject(err);
+			});
+  });
 }
 
 Preferences.getValue = function(key, defaultValue) { 
@@ -101,46 +203,41 @@ Preferences.getValue = function(key, defaultValue) {
 }
 
 Preferences.setValue = function(key, newValue) { 
-  Preferences.values[key] = newValue;
+	return new Promise((resolve, reject) => {
+		API.CORE.SetPreferences(key, newValue)
+			.then( () => { Preferences.values[key] = newValue; resolve(); } )
+			.catch( (err) => { Core.alertDialog("Can save value."); reject(err); } );
+	});
+}
 
-  Core.wrapCSRF(function(token) {
-    $.ajax({
-      async: false,
-      type: "POST",
-      url: "command/core/set-preference?" + $.param({ name: key }),
-      data: {
-        "value" : JSON.stringify(newValue), 
-        csrf_token: token
-      },
-      success: function(data) { },
-      dataType: "json"
+API.CORE.LoadLanguage = function(lang) {
+  return new Promise((resolve, reject) => {
+    API.CORE.PostCommand( "load-language", {}, { module : "core", lang } )
+      .then( (jqXHR) => { resolve(jqXHR.response); } )
+      .catch( (err) => { reject(err); } );
+  })
+}
+
+Languages.Load = function() {
+  return new Promise((resolve, reject) => {
+    API.CORE.LoadLanguage()
+    .then( (data) => { 
+			Languages.dictionary = data['dictionary'];
+			Languages.lang = data['lang'];
+			resolve();
+		})
+    .catch( (err) => {
+      var errorMessage = Core.i18n('core-index/langs-loading-failed', err.textStatus +':'+ err.errorThrown);
+      Core.alertDialog(errorMessage);
+      reject(err);
     });
   });
 }
 
-Languages.loadAll = function() {
-  $.ajax({
-    url : "command/core/load-language?",
-    type : "POST",
-    async : false,
-    data : {
-      module : "core",
-      //lang : Languages.lang
-    },
-    success : function(data) {
-      Languages.dictionary = data['dictionary'];
-      Languages.lang = data['lang'];
-    }
-  }).fail(function( jqXhr, textStatus, errorThrown ) {
-    var errorMessage = Core.i18n('core-index/langs-loading-failed', textStatus +':'+ errorThrown);
-
-    Core.alertDialog(errorMessage); 
-  });
-}
 
 Languages.setDefaultLanguage = function() {
   Languages.lang = (navigator.language || navigator.userLanguage).split("-")[0];
-  Languages.loadAll();
+  Languages.Load();
   
 	$.i18n().load(Languages.dictionary, Languages.lang);
 	$.i18n().locale = Languages.lang;
@@ -164,7 +261,7 @@ function PreferenceUI(tr, key, initialValue) {
   $(td0).text(key);
 
   var td1 = tr.insertCell(1);
-  $(td1).text((initialValue !== null) ? initialValue : "");
+  $(td1).text((initialValue !== null)? initialValue : "");
 
   var td2 = tr.insertCell(2);
 
@@ -172,7 +269,7 @@ function PreferenceUI(tr, key, initialValue) {
     var newValue = window.prompt(Core.i18n('core-index/change-value')+" " + key, $(td1).text());
     if (newValue == null) { return; } // @todo old behavior kept, but should be handled.
     
-		newValue = key === "userMetadata" ? deDupUserMetaData(newValue) : newValue;        
+		newValue = (key === "userMetadata")? deDupUserMetaData(newValue) : newValue;        
 
 		Preferences.setValue(key, newValue);
 
@@ -231,6 +328,10 @@ function populatePreferences() {
   });
 }
 
-function onLoad() { Preferences.loadAll(); }
+function onLoad() { 
+  Preferences.Load().then( (data) => { 
+    populatePreferences(); 
+  });
+}
 
 $(onLoad);
