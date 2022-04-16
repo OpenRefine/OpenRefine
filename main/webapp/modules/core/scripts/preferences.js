7aedf8e366f38c3b5b5fa1e11b603b0b90628e25
Antoine Beaubien <antoine@beaubien.qc.ca>
2022-04-16 08:41:08 -0400

    #1 Refactor: Refine,Core,Preferences,Languages

--- main/webapp/modules/core/scripts/preferences.js
+++ main/webapp/modules/core/scripts/preferences.js
@@ -31,11 +31,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
 
-var preferenceUIs = [];
-var thePreferences;
-
-var Refine = {
-};
+var Refine = {};
+var Core = {};
+var Languages = {};
+var Preferences = {};
 
 // Requests a CSRF token and calls the supplied callback
 // with the token
@@ -56,72 +55,41 @@ Core.wrapCSRF = function(onCSRF) {
 Core.postCSRF = function(url, data, success, dataType, failCallback) {
    return Core.wrapCSRF(function(token) {
       var fullData = data || {};
+      
       if (typeof fullData == 'string') {
          fullData = fullData + "&" + $.param({csrf_token: token});
       } else {
          fullData['csrf_token'] = token;
       }
       var req = $.post(url, fullData, success, dataType);
-      if (failCallback !== undefined) {
-         req.fail(failCallback);
-      }
+      
+      if (failCallback !== undefined) { req.fail(failCallback); }
    });
 };
-
-
-var lang = (navigator.language|| navigator.userLanguage).split("-")[0];
-var dictionary = getAllLanguages();
-
-$.i18n().load(dictionary, lang);
-$.i18n().locale = lang;
-//End internationalization
 
 Core.alertDialog = function(alertText) {
   window.alert(alertText);
 }
 
-Core.getAllLanguages = function() { 
-  $.ajax({
-    url : "command/core/load-language?",
-    type : "POST",
-    async : false,
-    data : {
-      module : "core",
-      //lang : queryLang
-    },
-    success : function(data) {
-      dictionary = data['dictionary'];
-      lang = data['lang'];
-    }
-  }).fail(function( jqXhr, textStatus, errorThrown ) {
-    var errorMessage = $.i18n('core-index/prefs-loading-failed');   // @todo This is a failure of languages loading, not Prefs!
-    if(errorMessage != "" && errorMessage != 'core-index/prefs-loading-failed') {
-      Core.alertDialog(errorMessage); 
-    } else {
-      Core.alertDialog( textStatus + ':' + errorThrown );
-    }
-  });
-}
-
-Core.getAllPreferences = function(key, defaultValue) { 
+Preferences.loadAll = function() { 
   $.post(
       "command/core/get-all-preferences",
       null,
-      function(data) { thePreferences = data; populatePreferences(); },
+      function(data) { Preferences.values = data; populatePreferences(); },
       "json"
   );
 }
 
-Core.getPreference = function(key, defaultValue) { 
-  if(!thePreferences.hasOwnProperty(key)) { return defaultValue; }
+Preferences.getValue = function(key, defaultValue) { 
+  if(!Preferences.values.hasOwnProperty(key)) { return defaultValue; }
 
-  return thePreferences[key];
+  return Preferences.values[key];
 }
 
-Core.setPreference = function(key, newValue) { 
-  thePreferences[key] = newValue;
+Preferences.setValue = function(key, newValue) { 
+  Preferences.values[key] = newValue;
 
-  Refine.wrapCSRF(function(token) {
+  Core.wrapCSRF(function(token) {
     $.ajax({
       async: false,
       type: "POST",
@@ -135,6 +103,49 @@ Core.setPreference = function(key, newValue) {
     });
   });
 }
+
+Languages.loadAll = function() {
+  $.ajax({
+    url : "command/core/load-language?",
+    type : "POST",
+    async : false,
+    data : {
+      module : "core",
+      //lang : Languages.lang
+    },
+    success : function(data) {
+      Languages.dictionary = data['dictionary'];
+      Languages.lang = data['lang'];
+      // debugger;
+    }
+  }).fail(function( jqXhr, textStatus, errorThrown ) {
+    var errorMessage = $.i18n('core-index/prefs-loading-failed');   // @todo This is a failure of languages loading, not Prefs!
+    if(errorMessage != "" && errorMessage != 'core-index/prefs-loading-failed') {
+      Core.alertDialog(errorMessage); 
+    } else {
+      Core.alertDialog( textStatus + ':' + errorThrown );
+    }
+  });
+}
+
+Languages.setDefaultLanguage = function() {
+  Languages.lang = (navigator.language || navigator.userLanguage).split("-")[0];
+  Languages.loadAll();
+  
+	$.i18n().load(Languages.dictionary, Languages.lang);
+	$.i18n().locale = Languages.lang;
+}
+
+// Languages.setLanguage = function() {}
+
+var preferenceUIs = [];
+
+Languages.setDefaultLanguage();
+
+/*
+var lang = Languages.lang
+var dictionary = Languages.dictionary
+*/
 
 function deDupUserMetaData(arrObj)  {
     var result = _.uniq(JSON.parse(arrObj), function(x){
@@ -157,31 +168,44 @@ function PreferenceUI(tr, key, initialValue) {
 
   $('<button class="button">').text($.i18n('core-index/edit')).appendTo(td2).click(function() {
     var newValue = window.prompt($.i18n('core-index/change-value')+" " + key, $(td1).text());
-    if (newValue !== null) {
-      if (key === "userMetadata")  {
-          newValue = deDupUserMetaData(newValue);
-      }
-      $(td1).text(newValue);
-      thePreferences[key] = newValue;
-      
-      Refine.postCSRF(
-        "command/core/set-preference",
-        {
-          name : key,
-          value : JSON.stringify(newValue)
-        },
-        function(o) {
-          if (o.code == "error") {
-            alert(o.message);
-          }
-        },
-        "json"
-      );
-    }
+    if (newValue == null) { return; } // @todo old behavior kept, but should be handled.
+    
+		newValue = key === "userMetadata" ? deDupUserMetaData(newValue) : newValue;        
+
+		Preferences.setValue(key, newValue);
+
+		$(td1).text(newValue);
+		
+/*		
+		Refine.postCSRF(
+			"command/core/set-preference",
+			{
+				name : key,
+				value : JSON.stringify(newValue)
+			},
+			function(o) {
+				if (o.code == "error") {
+					Core.alertDialog(o.message);
+				}
+			},
+			"json"
+		);
+*/
   });
 
   $('<button class="button">').text($.i18n('core-index/delete')).appendTo(td2).click(function() {
     if (window.confirm($.i18n('core-index/delete-key')+" " + key + "?")) {
+      Preferences.setValue(key);
+      
+			$(tr).remove();
+			for (var i = 0; i < preferenceUIs.length; i++) {
+				if (preferenceUIs[i] === self) {
+					preferenceUIs.splice(i, 1);
+					break;
+				}
+			}
+
+/*
       Refine.postCSRF(
         "command/core/set-preference",
         {
@@ -198,19 +222,18 @@ function PreferenceUI(tr, key, initialValue) {
               }
             }
           } else if (o.code == "error") {
-            alert(o.message);
+            Core.alertDialog(o.message);
           }
         },
         "json"
       );
+*/
     }
   });
 }
 
-function populatePreferences(prefs) {
+function populatePreferences() {
   var body = $("#body-info").empty();
-
-  if (prefs != null) { thePreferences = prefs; }
 
   $("#or-proj-starting").text($.i18n('core-project/starting')+"...");
   $('<h1>').text($.i18n('core-index/preferences')).appendTo(body);
@@ -221,9 +244,9 @@ function populatePreferences(prefs) {
   .html('<tr><th>'+$.i18n('core-index/key')+'</th><th>'+$.i18n('core-index/value')+'</th><th></th></tr>')
   .appendTo(body)[0];
 
-  for (var k in prefs) {
+  for (var k in Preferences.values) {
     var tr = table.insertRow(table.rows.length);
-    preferenceUIs.push(new PreferenceUI(tr, k, prefs[k]));
+    preferenceUIs.push(new PreferenceUI(tr, k, Preferences.values[k]));
   }
 
   var trLast = table.insertRow(table.rows.length);
@@ -232,41 +255,35 @@ function populatePreferences(prefs) {
   trLast.insertCell(2);
   $('<button class="button">').text($.i18n('core-index/add-pref')).appendTo(tdLast0).click(function() {
     var key = window.prompt($.i18n('core-index/add-pref'));
-    if (key) {
-      var value = window.prompt($.i18n('core-index/pref-key'));
-      if (value !== null) {
-        var tr = table.insertRow(table.rows.length - 1);
-        preferenceUIs.push(new PreferenceUI(tr, key, value));
-        
-        if (key === "userMetadata")  {
-            value = deDupUserMetaData(value);
-        }
-        
-        Refine.postCSRF(
-          "command/core/set-preference",
-          {
-            name : key,
-            value : JSON.stringify(value)
-          },
-          function(o) {
-            if (o.code == "error") {
-              alert(o.message);
-            }
-          },
-          "json"
-        );
-      }
-    }
+    if (!key) { return; }  // @todo old behavior kept, but should be handled.
+    
+		var value = window.prompt($.i18n('core-index/pref-key'));
+		if (!value === null) { return; }  // @todo old behavior kept, but should be handled.
+		
+		var tr = table.insertRow(table.rows.length - 1);
+		preferenceUIs.push(new PreferenceUI(tr, key, value));
+		
+		value = key === "userMetadata" ? deDupUserMetaData(value) : value;        
+		
+		Preferences.setValue(key, value);
+/*
+		Refine.postCSRF(
+			"command/core/set-preference",
+			{
+				name : key,
+				value : JSON.stringify(value)
+			},
+			function(o) {
+				if (o.code == "error") {
+					Core.alertDialog(o.message);
+				}
+			},
+			"json"
+		);
+*/
   });
 }
 
-function onLoad() {
-  $.post(
-      "command/core/get-all-preferences",
-      null,
-      populatePreferences,
-      "json"
-  );
-}
+function onLoad() { Preferences.loadAll(); }
 
 $(onLoad);
