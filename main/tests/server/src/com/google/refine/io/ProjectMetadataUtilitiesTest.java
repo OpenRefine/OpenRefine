b8dadd9c359b23bab6fbeb287b94e2d50322976d
Elroy Kanye <elroykanye@gmail.com>
2022-04-20 23:04:23 +0100

    add tests for saveAndLoad with default and utf8

---
+++ main/tests/server/src/com/google/refine/io/ProjectMetadataUtilitiesTest.java
@@ -0,0 +1,56 @@
+
+package com.google.refine.io;
+
+import com.google.refine.ProjectMetadata;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ProjectMetadataUtilitiesTest {
+
+    ProjectMetadata actualMetadata = new ProjectMetadata();
+    ProjectMetadata expectedMetadata = new ProjectMetadata();
+    File file = null;
+
+    @Before
+    public void setUp() {
+        actualMetadata.setName("Velmi dobře, použijeme vícejazyčný neurální stroj, abychom to vyřešili. ÜÜÖÖÄÄ");
+
+        file = new File("tests/server/src/com/google/refine/io/example_project_metadata_charset_test.json");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testSaveAndLoadFileDefault() {
+        try {
+            ProjectMetadataUtilities.saveToFile(actualMetadata, file);
+            expectedMetadata = ProjectMetadataUtilities.loadFromFile(file);
+
+            Assert.assertNotEquals(expectedMetadata.getName(), actualMetadata.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSaveAndLoadFileWithCharset() {
+        try {
+            ProjectMetadataUtilities.saveToFile(actualMetadata, file);
+            expectedMetadata = ProjectMetadataUtilities.loadFromFile(file, StandardCharsets.UTF_8);
+
+            Assert.assertEquals(expectedMetadata.getName(), actualMetadata.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
