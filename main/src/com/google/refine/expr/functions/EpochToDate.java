20a04ec362b9f6d3cf4c71f4ccd9829465cb6ec1
Keerthy Reddy <keerthyreddy@Keerthys-Air.attlocal.net>
2022-04-20 16:10:39 -0700

    fixes #608 Conversion of Unix time to Refine datetime

---
+++ main/src/com/google/refine/expr/functions/EpochToDate.java
@@ -0,0 +1,83 @@
+
+package com.google.refine.expr.functions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZoneId;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.IllformedLocaleException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.refine.expr.EvalError;
+import com.google.refine.expr.util.CalendarParser;
+import com.google.refine.expr.util.CalendarParserException;
+import com.google.refine.grel.ControlFunctionRegistry;
+import com.google.refine.grel.Function;
+import com.google.refine.util.ParsingUtilities;
+
+public class EpochToDate implements Function{
+
+    @Override
+    public Object call(Properties bindings, Object[] args) {
+        if(args.length >= 1 && args[0] instanceof Number) {
+            long epoch = (long) args[0];
+            ZoneId zoneId = ZoneId.of("UTC");
+            //ZoneId zoneId = ZoneId.systemDefault();
+            Instant instant = null;
+            OffsetDateTime date = null;
+            if(args.length == 1){
+                instant = Instant.ofEpochSecond ( epoch );
+                date = OffsetDateTime.ofInstant(instant, zoneId);
+                return date;
+            } else if(args.length == 2 && args[1] instanceof String){
+                Object o2 = args[1];
+                String type = ((String ) o2).toLowerCase();
+                if(type.equals("second")){
+                    instant = Instant.ofEpochSecond ( epoch );
+                    date = OffsetDateTime.ofInstant(instant, zoneId);
+                    return date;
+                } else if (type.equals("millisecond")){
+                    instant = Instant.ofEpochSecond ( epoch/1000 );
+                    date = OffsetDateTime.ofInstant(instant, zoneId);
+                    return date;
+                } else if (type.equals("microsecond")){
+                    instant = Instant.ofEpochSecond ( epoch/1000000 );
+                    date = OffsetDateTime.ofInstant(instant, zoneId);
+                    return date;
+                }
+            }
+        }
+        return new EvalError(ControlFunctionRegistry.getFunctionName(this) + " expect one argument, " +
+                "which is a number of epoch time\n" +
+                "expect two argument, the first is a epoch time(second, millisecond, microsecond), the second " +
+                "is the type");
+    }
+
+   @Override
+    public String getDescription() {
+        return "Returns a number converted to a date. Can parse one parameter or two parameters. When parsing one parameter, the number is the epoch second." + 
+           "When parsing two parameters, the first is the number, the second is the numbers type, such as second, millisecond, microsecond.";
+    }
+
+    @Override
+    public String getParams() {
+        return "A number of epoch second, millisecond, microsecond. The second parameter is not necessary, is the input number's type";
+    }
+
+    @Override
+    public String getReturns() {
+        return "date(OffsetDateTime)";
+    }
+
+}
---
+++ main/tests/server/src/com/google/refine/expr/functions/EpochToDateTest.java
@@ -0,0 +1,41 @@
+
+package com.google.refine.expr.functions;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Properties;
+import java.text.ParseException;
+
+import org.testng.annotations.Test;
+
+import com.google.refine.RefineTest;
+import com.google.refine.expr.EvalError;
+import com.google.refine.grel.Function;
+import com.google.refine.util.TestUtils;
+import com.google.refine.expr.functions.*;
+
+public class EpochToDateTest extends RefineTest {
+
+    long epoch = 1485105822000L; // 2017-01-22T11:23:42-06:00
+    static Properties bindings = new Properties();
+
+    @Test
+    public void testEpoch2DateOneParam() {
+        long epoch1 = epoch / 1000;
+        EpochToDate etd = new EpochToDate();
+        assertEquals(etd.call(bindings, new Object[] { epoch1 }).toString(), "2017-01-22T11:23:42-06:00");
+    }
+
+    @Test
+    public void testEpoch2DateTwoParam() {
+        long epochSecond = epoch / 1000;
+        long epochMilliSecond = epoch;
+        long epochMicroSecond = epoch * 1000;
+        EpochToDate etd = new EpochToDate();
+        assertEquals(etd.call(bindings, new Object[] { epochSecond, "second" }).toString(), "2017-01-22T11:23:42-06:00");
+        assertEquals(etd.call(bindings, new Object[] { epochMilliSecond, "millisecond" }).toString(), "2017-01-22T11:23:42-06:00");
+        assertEquals(etd.call(bindings, new Object[] { epochMicroSecond, "microsecond" }).toString(), "2017-01-22T11:23:42-06:00");
+    }
+
+}
