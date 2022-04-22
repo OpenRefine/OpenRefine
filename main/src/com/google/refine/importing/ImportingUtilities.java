a5d2f6f9034f80ce77f92bd88c481ea90779f887
zkl-ai <907668776@qq.com>
2022-04-23 00:02:03 +0800

    change and test the separator

--- main/src/com/google/refine/importing/ImportingUtilities.java
+++ main/src/com/google/refine/importing/ImportingUtilities.java
@@ -77,6 +77,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.util.Streams;
 import org.apache.commons.io.FileSystem;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
@@ -456,18 +457,27 @@ public class ImportingUtilities {
         return (location.startsWith(File.separator)) ? location.substring(1) : location;
     }
 
-    private static String normalizeFileName(String fileName){
+    /**
+     * Replace the illegal character with '-' in the path in Windows
+     * @param path: file path
+     * @return the replaced path or original path if the OS is not Windows
+     */
+    public static String normalizePath(String path){
         FileSystem currentFileSystem = FileSystem.getCurrent();
-        if (currentFileSystem != FileSystem.WINDOWS) return fileName;
-        String normalizedLocalName = new String();
+        if (currentFileSystem != FileSystem.WINDOWS) {
+            return path;
+        }
         // normalize the file name if the current system is windows
-        String[] path = fileName.split("/");
-        for(String f : path){
-            if (f.equals("")){
+        String normalizedLocalName = "";
+        String pathWithWSeparator = FilenameUtils.separatorsToWindows(path);
+        String separator = String.format("\\%c",File.separatorChar);
+        String[] paths = pathWithWSeparator.split(separator);
+        for(String p : paths){
+            if (p.equals("")){
                 continue;
             }
-            f = currentFileSystem.toLegalFileName(f,'-');
-            normalizedLocalName += String.format("/%s",f);
+            p = currentFileSystem.toLegalFileName(p,'-');
+            normalizedLocalName += String.format("%c%s",File.separatorChar, p);
         }
         return normalizedLocalName;
     }
@@ -477,8 +487,7 @@ public class ImportingUtilities {
         if (q > 0) {
             name = name.substring(0, q);
         }
-
-        name = normalizeFileName(name);
+        name = normalizePath(name);
         File file = new File(dir, name);
         Path normalizedFile = file.toPath().normalize();
         // For CVE-2018-19859, issue #1840
--- main/tests/server/src/com/google/refine/importing/ImportingUtilitiesTests.java
+++ main/tests/server/src/com/google/refine/importing/ImportingUtilitiesTests.java
@@ -118,14 +118,52 @@ public class ImportingUtilitiesTests extends ImporterTest {
     }
 
     @Test
+    public void testNormalizePath() {
+        // Test for issue Unable to create a project from a URL on Windows if the URL path contains ":" character #4625
+        // https://github.com/OpenRefine/OpenRefine/issues/4625
+        String urlPath = "/a/b:c/dummy:test";
+        String urlPathFixed = "\\a\\b-c\\dummy-test";
+        String result = ImportingUtilities.normalizePath(urlPath);
+        Assert.assertEquals(urlPathFixed, result);
+    }
+
+    @Test
+    public void testNormalizePathWithDifferentSeparator() {
+        // Test for issue Unable to create a project from a URL on Windows if the URL path contains ":" character #4625
+        // https://github.com/OpenRefine/OpenRefine/issues/4625
+        String urlPath = "\\a\\b:c\\dummy:test";
+        String urlPathFixed = "\\a\\b-c\\dummy-test";
+        String result = ImportingUtilities.normalizePath(urlPath);
+        Assert.assertEquals(urlPathFixed, result);
+    }
+
+    @Test
     public void testAllocateFileWithIllegalCharInWindows() throws IOException {
         // Test for issue Unable to create a project from a URL on Windows if the URL path contains ":" character #4625
         // https://github.com/OpenRefine/OpenRefine/issues/4625
         File tempDir = TestUtils.createTempDirectory("openrefine-allocate-file-test");
         File dirA = new File(tempDir, "a");
-        dirA.mkdir();
+        Assert.assertTrue(dirA.mkdir());
         String urlPath = ".././a/b:c/dummy:test";
         String urlPathFixed = ".././a/b-c/dummy-test";
+        File allocated = ImportingUtilities.allocateFile(dirA, urlPath);
+        FileSystem fileSystem = FileSystem.getCurrent();
+        if (fileSystem == FileSystem.WINDOWS) {
+            Assert.assertEquals(allocated, new File(dirA, urlPathFixed));
+        } else {
+            Assert.assertEquals(allocated, new File(dirA, urlPath));
+        }
+    }
+
+    @Test
+    public void testAllocateFileWithIllegalCharInWindowsDifferentSeparator() throws IOException {
+        // Test for issue Unable to create a project from a URL on Windows if the URL path contains ":" character #4625
+        // https://github.com/OpenRefine/OpenRefine/issues/4625
+        File tempDir = TestUtils.createTempDirectory("openrefine-allocate-file-test");
+        File dirA = new File(tempDir, "a");
+        Assert.assertTrue(dirA.mkdir());
+        String urlPath = "..\\.\\a\\b:c\\dummy:test";
+        String urlPathFixed = "..\\.\\a\\b-c\\dummy-test";
         File allocated = ImportingUtilities.allocateFile(dirA, urlPath);
         FileSystem fileSystem = FileSystem.getCurrent();
         if (fileSystem == FileSystem.WINDOWS) {
