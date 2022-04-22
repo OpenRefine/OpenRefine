f74b03d4de7e1f292378be570a36d0ee0e069d65
keerthybreddy <keerthybreddy@gmail.com>
2022-04-21 21:51:44 -0700

    fixes #608 added more test cases for complete code coverage

--- main/tests/server/src/com/google/refine/expr/functions/EpochToDateTest.java
+++ main/tests/server/src/com/google/refine/expr/functions/EpochToDateTest.java
@@ -2,9 +2,13 @@
 package com.google.refine.expr.functions;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import java.util.Properties;
 import org.testng.annotations.Test;
 import com.google.refine.RefineTest;
+import com.google.refine.expr.EvalError;
+import com.google.refine.grel.ControlFunctionRegistry;
 
 public class EpochToDateTest extends RefineTest {
 
@@ -27,6 +31,29 @@ public class EpochToDateTest extends RefineTest {
         assertEquals(etd.call(bindings, new Object[] { epochSecond, "second" }).toString(), "2022-04-21T13:19:44Z");
         assertEquals(etd.call(bindings, new Object[] { epochMilliSecond, "millisecond" }).toString(), "2022-04-21T13:19:44Z");
         assertEquals(etd.call(bindings, new Object[] { epochMicroSecond, "microsecond" }).toString(), "2022-04-21T13:19:44Z");
+    }
+
+    @Test
+    public void testDescriptionParamsReturns() {
+        long epochMilliSecond = epoch;
+        EpochToDate etd = new EpochToDate();
+        assertEquals(etd.getDescription(),
+                "Returns a number converted to a date. Can parse one parameter or two parameters. When parsing one parameter, the number is the epoch second."
+                        + "When parsing two parameters, the first is the number, the second is the numbers type, such as second, millisecond, microsecond.");
+        assertEquals(etd.getParams(),
+                "A number of epoch second, millisecond, microsecond. The second parameter is not necessary, is the input number's type");
+        assertEquals(etd.getReturns(), "date(OffsetDateTime)");
+        assertTrue(etd.call(bindings, new Object[] { "millisecond", epochMilliSecond }) instanceof EvalError); // wrong
+                                                                                                               // // in
+    }
+
+    @Test
+    public void testEpoch2DateEvalError() {
+        long epochMilliSecond = epoch;
+        EpochToDate etd = new EpochToDate();
+
+        assertTrue(etd.call(bindings, new Object[] { "millisecond", epochMilliSecond }) instanceof EvalError); // wrong
+                                                                                                               // // in
     }
 
 }
