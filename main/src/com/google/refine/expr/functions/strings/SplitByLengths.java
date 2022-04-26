63d2b8c867cdc403b658c836fd968114a42b4de9
Vani Gupta <59418361+TheTrailblazer09@users.noreply.github.com>
2022-04-26 14:24:16 +0530

    (I#3934) Update user manual and in-app documentation for splitByLength() (#4769)
    * fixes #3934
    
    made the requested changes:
      - removed optional arguments
      - changed return statement of getDescription()
    
    * fixes #3934
    
    "Shortened In App Description from SplitByLengths.java"
    
    * Update SplitByLengths.java
    
    * Update docs/versioned_docs/version-3.5/manual/grelfunctions.md
    
    put back the dash
    
    Co-authored-by: Antoine Beaubien <antoine@beaubien.qc.ca>

--- main/src/com/google/refine/expr/functions/strings/SplitByLengths.java
+++ main/src/com/google/refine/expr/functions/strings/SplitByLengths.java
@@ -69,7 +69,7 @@ public class SplitByLengths implements Function {
     
     @Override
     public String getDescription() {
-        return "Returns the array of strings obtained by splitting s into substrings with the given lengths. For example, \"internationalization\".splitByLengths(5, 6, 3) returns an array of 3 strings: [ \"inter\", \"nation\", \"ali\" ]. Excess characters are discarded.";
+        return "Returns the array of strings obtained by splitting s into substrings with the given lengths. For example, \"internationalization\".splitByLengths(5, 6, 3) returns an array of 3 strings: [ \"inter\", \"nation\", \"ali\" ]. Excess characters are discarded from the output array.";
     }
     
     @Override
