c3ffbacc27b39a888c9894e2289f41cd0885cd56
elebitzero <42903164+elebitzero@users.noreply.github.com>
2022-05-05 08:36:19 -0700

    Cleaned up preferences set in Cypress tests (#4831)

--- main/tests/cypress/cypress/integration/preferences/change_preference.spec.js
+++ main/tests/cypress/cypress/integration/preferences/change_preference.spec.js
@@ -1,8 +1,27 @@
 describe(__filename, function () {
+  const testPreferenceName = 'PreferenceName_Test';
+  const testPreferenceValue = '"PreferenceValue_Test"';
+  
+  afterEach(function () {
+    cy.deletePreference(testPreferenceName);
+  });
+
+  it('Add a new preference', function () {
+    cy.visitOpenRefine();
+    cy.get('#project-links a').contains('Preferences').click();
+
+    cy.window().then(($win) => {
+      cy.stub($win, 'prompt').returns(testPreferenceName);
+      cy.get('table.preferences tr:last-child button.button').click();
+    });
+
+    cy.get('table.preferences tr:nth-last-child(2)').contains(
+        testPreferenceName
+    );
+  });
+  
   it('Edit a preference', function () {
     cy.visitOpenRefine();
-    const testPreferenceName = `PreferenceName_${Date.now()}`;
-    const testPreferenceValue = `"PreferenceValue_${Date.now()}"`;
 
     cy.setPreference(testPreferenceName, testPreferenceValue);
 
@@ -19,21 +38,5 @@ describe(__filename, function () {
     });
 
     cy.get('table.preferences tr').contains(testPreferenceValue + '_Edited');
-  });
-
-  it('Add a new preference', function () {
-    cy.visitOpenRefine();
-    cy.get('#project-links a').contains('Preferences').click();
-
-    const testPreferenceName = Date.now();
-
-    cy.window().then(($win) => {
-      cy.stub($win, 'prompt').returns(testPreferenceName);
-      cy.get('table.preferences tr:last-child button.button').click();
-    });
-
-    cy.get('table.preferences tr:nth-last-child(2)').contains(
-      testPreferenceName
-    );
   });
 });
