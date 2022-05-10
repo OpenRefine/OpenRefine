b81c197d9ceff98fa0b7543d9775341d94e1934a
WaltonG <waltongoga@gmail.com>
2022-05-10 20:00:16 +0300

    Loading projects from workspace.json only

--- main/src/com/google/refine/io/FileProjectManager.java
+++ main/src/com/google/refine/io/FileProjectManager.java
@@ -362,13 +362,8 @@ public class FileProjectManager extends ProjectManager  {
         if (loadFromFile(new File(_workspaceDir, "workspace.json"))) {
             return;
         }
-        if (loadFromFile(new File(_workspaceDir, "workspace.temp.json"))) {
-            return;
-        }
-        if (loadFromFile(new File(_workspaceDir, "workspace.old.json"))) {
-            return;
-        }
-        logger.error("Failed to load workspace from any attempted alternatives.");
+
+        logger.error("Failed to load workspace");
     }
     
     protected boolean loadFromFile(File file) {
