75dc6b49db313a70674a5b1da79b197669062270
Elroy Kanye <elroykdev@gmail.com>
2022-06-02 17:58:59 +0100

    mod ReplaceEach to use replaceEach in StringUtils by commons.lang

--- main/src/com/google/refine/expr/functions/strings/ReplaceEach.java
+++ main/src/com/google/refine/expr/functions/strings/ReplaceEach.java
@@ -3,6 +3,7 @@ package com.google.refine.expr.functions.strings;
 import com.google.refine.expr.EvalError;
 import com.google.refine.grel.ControlFunctionRegistry;
 import com.google.refine.grel.Function;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -19,8 +20,7 @@ public class ReplaceEach implements Function {
         if(args.length == 3) {
             if(args[0] instanceof String && args[1] instanceof String[] && (args[2] instanceof String[] || args[2] instanceof String)) {
                 String str = (String) args[0];
-                String[] search = (String[]) args[1];
-                String[] replace;
+                String[] search = (String[]) args[1], replace;
                 if(args[2] instanceof String) {
                     replace = new String[]{(String) args[2]};
                 }else {
@@ -29,23 +29,16 @@ public class ReplaceEach implements Function {
 
                 // Check that the search array is greater than or equal to the replace array in length
                 if(search.length >= replace.length) {
-                    // Create a new array to hold the new replace strings
-                    String[] newReplace = new String[search.length];
-
-                    // check that the replace array and search array are the same length
-                    if(search.length == replace.length) {
-                        // assign the replace strings to the new replace array
-                        newReplace = replace;
-                    } else {
+                   // check that the replace array and search array are the same length
+                    if(search.length != replace.length) {
                         // make multiple replacements of the last element in replace
+                        String[] newReplace = new String[search.length];
                         Arrays.fill(newReplace, replace[replace.length - 1]);
+                        replace = newReplace;
                     }
 
-                    // replace each occurrence of search with corresponding element in newReplace
-                    for(int i = 0; i < search.length; i++) {
-                        str = str.replace(search[i], newReplace[i]);
-                    }
-                    return str;
+                    // replace each occurrence of search with corresponding element in replace
+                    return StringUtils.replaceEachRepeatedly(str, search, replace);
                 } else {
                     return new EvalError(ControlFunctionRegistry.getFunctionName(this) + ": the number of replacements must be equal to or less than the number of search strings.");
                 }
