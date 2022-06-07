d7b39fbcc0c53de75eb5dd46551b6cbe9c930d6f
elebitzero <dave.lefaive@gmail.com>
2022-06-06 17:22:35 -0700

    Added fix for Wikibase login buttons.

--- main/tests/cypress/cypress/integration/open-project/open_project.spec.js
+++
@@ -1,9 +0,0 @@
-describe(__filename, function () {
-  it('Open an existing project by visiting the URL directly', function () {
-    const projectName = Date.now();
-    cy.loadProject('food.mini', projectName).then((projectId) => {
-      cy.visitProject(projectId);
-      cy.get('#project-name-button').contains(projectName);
-    });
-  });
-});
