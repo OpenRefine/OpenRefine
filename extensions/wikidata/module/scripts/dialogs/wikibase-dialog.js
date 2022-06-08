142ce311b0bdde927e1fb460e79b83b10e32547e
elebitzero <dave.lefaive@gmail.com>
2022-06-07 20:25:15 -0700

    Improved error message for invalid manifest version.

--- extensions/wikidata/module/scripts/dialogs/wikibase-dialog.js
+++ extensions/wikidata/module/scripts/dialogs/wikibase-dialog.js
@@ -153,6 +153,12 @@ WikibaseDialog.validateManifest = function (manifest) {
     WikibaseDialog.validateWikibaseManifestV2 = WikibaseDialog.ajv.compile(WikibaseManifestSchemaV2);
   }
 
+  let majorVersion = manifest.version.split('.')[0];
+  if (majorVersion < 1 || majorVersion > 2) {
+    alert($.i18n('wikibase-addition/version-error', manifest.version));
+    return false;
+  }
+
   if (manifest.version !== undefined && manifest.version.startsWith('1.')) {
     if (WikibaseDialog.validateWikibaseManifestV1(manifest)) {
        return true;
