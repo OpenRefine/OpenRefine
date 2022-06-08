7c19f8a0ee5fe27befc6a6d5f7619aadb5ddfd7d
WaltonG <waltongoga@gmail.com>
2022-06-08 15:58:22 +0300

    Added onClick function

--- extensions/sparql/module/scripts/index/sparql-source-ui.js
+++ extensions/sparql/module/scripts/index/sparql-source-ui.js
@@ -10,6 +10,16 @@ Refine.SPARQLSourceUI.prototype.attachUI = function(body) {
   
   $('#or-import-sparql').text($.i18n('sparql-import/importer-label'));
   this._elmts.queryButton.html($.i18n('sparql-buttons/query'));
+  
+   //To Do: Get complete url info
+  this._elmts.queryButton.on('click',function(evt){
+	var queryInfo = {};
+	queryInfo.query = jQueryTrim($( "#sparql-import-textarea" ).val());
+	if(!query.length === 0){
+		self._controller.startImportingData(queryInfo);
+	}
+});
+  
 };
 
 Refine.SPARQLSourceUI.prototype.focus = function() {
