e9b4a2294c4565b68e37fbb5a5e32c2c7138a6af
elebitzero <dave.lefaive@gmail.com>
2022-06-28 23:54:10 -0700

    Cleaning up and removing unused and old freebase methods and references.

--- main/src/com/google/refine/importers/OdsImporter.java
+++ main/src/com/google/refine/importers/OdsImporter.java
@@ -190,9 +190,6 @@ public class OdsImporter extends TabularImportingParserBase {
                             Cell cell = null;
 
                             OdfTableCell sourceCell = row.getCellByIndex(cellIndex);
-                            if (sourceCell != null) {
-                                cell = extractCell(sourceCell, reconMap);
-                            }
                             cells.add(cell);
                             if (cell != null && cellIndex > maxCol) {
                                 maxCol = cellIndex;
@@ -248,57 +245,5 @@ public class OdsImporter extends TabularImportingParserBase {
             value = cell.getDisplayText();
         }
         return value;
-    }
-
-    static protected Cell extractCell(OdfTableCell cell, Map<String, Recon> reconMap) {
-        Serializable value = extractCell(cell);
-
-        if (value != null) {
-            Recon recon = null;
-
-            String hyperlink = ""; // TODO: cell.getHyperlink();
-            if (hyperlink != null) {
-                String url = hyperlink; // TODO: hyperlink.getAddress();
-
-                if (url.startsWith("http://") ||
-                        url.startsWith("https://")) {
-
-                    final String sig = "freebase.com/view";
-
-                    int i = url.indexOf(sig);
-                    if (i > 0) {
-                        String id = url.substring(i + sig.length());
-
-                        int q = id.indexOf('?');
-                        if (q > 0) {
-                            id = id.substring(0, q);
-                        }
-                        int h = id.indexOf('#');
-                        if (h > 0) {
-                            id = id.substring(0, h);
-                        }
-
-                        if (reconMap.containsKey(id)) {
-                            recon = reconMap.get(id);
-                            recon.judgmentBatchSize++;
-                        } else {
-                            recon = new Recon(0, null, null);
-                            recon.service = "import";
-                            recon.match = new ReconCandidate(id, value.toString(), new String[0], 100);
-                            recon.matchRank = 0;
-                            recon.judgment = Judgment.Matched;
-                            recon.judgmentAction = "auto";
-                            recon.judgmentBatchSize = 1;
-                            recon.addCandidate(recon.match);
-
-                            reconMap.put(id, recon);
-                        }
-                    }
-                }
-            }
-            return new Cell(value, recon);
-        } else {
-            return null;
-        }
     }
 }
