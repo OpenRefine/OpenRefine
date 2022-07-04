5b5bcce629fbc48f608dad2b925c5c893ce20ba3
Elroy Kanye <elroykdev@gmail.com>
2022-07-04 20:03:52 +0100

    change returns to method refs for function descriptions in math

--- main/src/com/google/refine/expr/functions/math/ATan.java
+++ main/src/com/google/refine/expr/functions/math/ATan.java
@@ -38,6 +38,7 @@ import java.util.Properties;
 import com.google.refine.expr.EvalError;
 import com.google.refine.grel.ControlFunctionRegistry;
 import com.google.refine.grel.Function;
+import com.google.refine.grel.FunctionDescription;
 
 public class ATan implements Function {
 
@@ -51,7 +52,7 @@ public class ATan implements Function {
 
     @Override
     public String getDescription() {
-        return "Returns the arc tangent of an angle in the range of -PI/2 through PI/2.";
+        return FunctionDescription.math_atan();
     }
     
     @Override
