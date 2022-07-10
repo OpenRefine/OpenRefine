76359668e2b3b9f0ee709264e0b69424a0f4a414
Elroy Kanye <elroykdev@gmail.com>
2022-07-10 17:46:31 +0100

    add utils class for setting default locale given a locale code

---
+++ main/src/com/google/refine/util/LocaleUtils.java
@@ -0,0 +1,28 @@
+package com.google.refine.util;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class LocaleUtils {
+    public static void setLocale(String code) {
+        ResourceBundle langResource = ResourceBundle.getBundle("language_codes");
+        String localeCode;
+
+        try {
+            localeCode = (String) langResource.getObject(code);
+        } catch (NullPointerException | MissingResourceException e) {
+            return;
+        }
+
+        String[] localeParts = localeCode.split("_");
+        Locale.Builder localeBuilder = new Locale.Builder();
+        switch (localeParts.length) {
+            case 1: localeBuilder.setLanguage(localeParts[0]); break;
+            case 2: localeBuilder.setLanguage(localeParts[0]).setRegion(localeParts[1]);break;
+            default: return;
+        }
+        Locale.setDefault(localeBuilder.build());
+        System.out.println(Locale.getDefault());
+    }
+}
