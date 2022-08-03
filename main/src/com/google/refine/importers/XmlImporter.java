63913fc80ef72360a1d757fe5de4a11494cb4832
Elroy Kanye <elroykanye@gmail.com>
2022-08-03 16:23:51 +0100

    use readers

--- main/src/com/google/refine/importers/XmlImporter.java
+++ main/src/com/google/refine/importers/XmlImporter.java
@@ -37,6 +37,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PushbackInputStream;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
@@ -224,6 +225,8 @@ public class XmlImporter extends TreeImportingParserBase {
         }
 
         private InputStream removeInvalidCharacters(InputStream inputStream) throws IOException {
+            var reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            // TODO do replacement using the reader object
             String in = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
             Pattern p = Pattern.compile("[^\\u0009\\u000A\\u000D\\u0020-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFF]+");
             String result = p.matcher(in).replaceAll("");
