2827ba0131b7b06603966f387bc5dae41b74f737
elebitzero <42903164+elebitzero@users.noreply.github.com>
2022-08-23 23:34:14 -0700

    Re-enabled tests that create project from URL(s) (#5199)
    * Re-enabled tests that create project from URL(s) and removed iframes.
    
    * Cleanup.

--- main/tests/cypress/cypress/integration/create-project/create_project.spec.js
+++ main/tests/cypress/cypress/integration/create-project/create_project.spec.js
@@ -4,7 +4,6 @@ describe(__filename, function () {
   });
   
   it('Test the create project from this computer based on CSV', function () {
-    // navigate to the create page
     cy.visitOpenRefine();
     cy.navigateTo('Create project');
     cy.get('#create-project-ui-source-selection-tabs > div')
@@ -40,7 +39,6 @@ describe(__filename, function () {
     cy.waitForProjectTable();
   });
   it('Test the create project from this computer based on TSV', function () {
-    // navigate to the create page
     cy.visitOpenRefine();
     cy.navigateTo('Create project');
     cy.get('#create-project-ui-source-selection-tabs > div')
@@ -76,7 +74,6 @@ describe(__filename, function () {
     cy.waitForProjectTable();
   });
   it('Test the create project from clipboard based on CSV', function () {
-    // navigate to the create page
     cy.visitOpenRefine();
     cy.navigateTo('Create project');
     cy.get('#create-project-ui-source-selection-tabs > div')
@@ -113,7 +110,6 @@ describe(__filename, function () {
     cy.waitForProjectTable();
   });
   it('Test the create project from clipboard based on TSV', function () {
-    // navigate to the create page
     cy.visitOpenRefine();
     cy.navigateTo('Create project');
     cy.get('#create-project-ui-source-selection-tabs > div')
@@ -197,87 +193,71 @@ describe(__filename, function () {
         .parent()
         .contains(uniqueTagName2);
   });
-  // it('Test the create project from Web URL based on CSV', function () {
-  //   // navigate to the create page
-  //   cy.visitOpenRefine();
-  //   cy.navigateTo('Create project');
-  //   cy.get('#create-project-ui-source-selection-tabs > div')
-  //     .contains('Web Addresses (URLs)')
-  //     .click();
-  //   cy.get('#or-import-enterurl').should(
-  //     'to.contain',
-  //     'Enter one or more web addresses (URLs) pointing to data to download:'
-  //   );
-  //   // add file
-  //   const csvURL =
-  //     'https://raw.githubusercontent.com/OpenRefine/OpenRefine/master/main/tests/cypress/cypress/fixtures/food.mini.csv';
-  //   cy.get('input[bind="urlInput"]').filter(':visible').type(csvURL);
-  //   Cypress.config('pageLoadTimeout', 10000);
-  //   Cypress.config('commandTimeout', 10000);
-  //   Cypress.config('defaultCommandTimeout', 10000);
 
-  //   cy.get(
-  //     '.create-project-ui-source-selection-tab-body.selected button.button-primary'
-  //   )
-  //     .contains('Next »')
-  //     .click();
+  const csvURL =
+      'https://raw.githubusercontent.com/OpenRefine/OpenRefine/master/main/tests/cypress/cypress/fixtures/food.mini.csv';
 
-  //   cy.get('.default-importing-wizard-header input[bind="projectNameInput"]', {
-  //     timeout: 6000,
-  //   }).should('have.value', 'food mini csveuh');
+  it('Test the create project from Web URL based on CSV', function () {
+    cy.visitOpenRefine();
+    cy.navigateTo('Create project');
+    cy.get('#create-project-ui-source-selection-tabs > div')
+      .contains('Web Addresses (URLs)')
+      .click();
+    cy.get('#or-import-enterurl').should(
+      'to.contain',
+      'Enter one or more web addresses (URLs) pointing to data to download:'
+    );
+    cy.get('input[bind="urlInput"]').filter(':visible').type(csvURL);
 
-  //   // then ensure we are on the preview page
-  //   cy.get('.create-project-ui-panel').contains('Configure parsing options');
+    cy.get('.create-project-ui-source-selection-tab-body.selected button.button-primary')
+      .contains('Next »')
+      .click();
 
-  //   // preview and click next
-  //   cy.get('.default-importing-wizard-header button[bind="nextButton"]')
-  //     .contains('Create project »')
-  //     .click();
-  //   cy.get('#create-project-progress-message').contains('Done.');
+    // then ensure we are on the preview page
+    cy.get('.default-importing-wizard-header input[bind="projectNameInput"]')
+        .should('have.value', 'food mini csv');
 
-  //   // ensure the project is loading by checking presence of a project-title element
-  //   cy.get('#project-title').should('exist');
-  // });
+    cy.get('.create-project-ui-panel').contains('Configure parsing options');
 
-  // it('Test the create project from Multiple Web URLs based on CSV', function () {
-  //   // navigate to the create page
-  //   cy.visitOpenRefine();
-  //   cy.navigateTo('Create project');
-  //   cy.get('#create-project-ui-source-selection-tabs > div')
-  //     .contains('Web Addresses (URLs)')
-  //     .click();
-  //   cy.get('#or-import-enterurl').should(
-  //     'to.contain',
-  //     'Enter one or more web addresses (URLs) pointing to data to download:'
-  //   );
-  //   // add file
-  //   const csvURL =
-  //     'https://raw.githubusercontent.com/OpenRefine/OpenRefine/master/main/tests/cypress/cypress/fixtures/food.mini.csv';
-  //   cy.get('input[bind="urlInput"]').filter(':visible').type(csvURL);
-  //   cy.get('button[bind="addButton"]').contains('Add another URL').click();
+    // preview and click next
+    cy.get('.default-importing-wizard-header button[bind="nextButton"]')
+      .contains('Create project »')
+      .click();
+    cy.waitForProjectTable();
+  });
 
-  //   cy.get(
-  //     '.create-project-ui-source-selection-tab-body.selected button.button-primary'
-  //   )
-  //     .contains('Next »')
-  //     .click();
-  //   cy.get('.create-project-ui-panel', { timeout: 10000 })
-  //     .contains('Configure parsing options »')
-  //     .click();
-  //   cy.get(
-  //     '.default-importing-wizard-header input[bind="projectNameInput"]'
-  //   ).should('have.value', 'food mini csv');
+  it('Test the create project from Multiple Web URLs based on CSV', function () {
+    cy.visitOpenRefine();
+    cy.navigateTo('Create project');
+    cy.get('#create-project-ui-source-selection-tabs > div')
+      .contains('Web Addresses (URLs)')
+      .click();
+    cy.get('#or-import-enterurl').should(
+      'to.contain',
+      'Enter one or more web addresses (URLs) pointing to data to download:'
+    );
+    cy.get('input[bind="urlInput"]').filter(':visible').type(csvURL);
+    cy.get('button[bind="addButton"]').contains('Add another URL').click();
 
-  //   // then ensure we are on the preview page
-  //   // cy.get('.create-project-ui-panel').contains('Configure parsing options');
+    cy.get('.create-project-ui-source-selection-tab-body.selected button.button-primary')
+      .contains('Next »')
+      .click();
 
-  //   // preview and click next
-  //   cy.get('.default-importing-wizard-header button[bind="nextButton"]')
-  //     .contains('Create project »')
-  //     .click();
-  //   cy.get('#create-project-progress-message').contains('Donuue.');
+    cy.get('.create-project-ui-panel')
+      .contains('Configure parsing options »')
+      .click();
 
-  //   // // ensure the project is loading by checking presence of a project-title element
-  //   // cy.get('#project-title').should('exist');
-  // });
+    // then ensure we are on the preview page
+    cy.get(
+      '.default-importing-wizard-header input[bind="projectNameInput"]'
+    ).should('have.value', 'food mini csv');
+
+    cy.get('.create-project-ui-panel').contains('Configure parsing options');
+
+    // preview and click next
+    cy.get('.default-importing-wizard-header button[bind="nextButton"]')
+      .contains('Create project »')
+      .click();
+    cy.waitForProjectTable();
+  });
 });
