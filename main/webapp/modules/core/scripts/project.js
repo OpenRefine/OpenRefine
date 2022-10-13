431b8ced91d2fba6fd0014bbbf336866d537e40f
elebitzero <42903164+elebitzero@users.noreply.github.com>
2022-10-13 14:05:25 -0700

    Made it so the Wikibase schema panel gets resized when the window gets resized (#5334)
    * Made it so the Wikibase schema panel gets resized when the window gets resized.
    
    * Added a guard to check if SchemaAlignment exists when doing resizeAll().

--- main/webapp/modules/core/scripts/project.js
+++ main/webapp/modules/core/scripts/project.js
@@ -105,6 +105,9 @@ function resizeAll() {
   ui.processPanel.resize();
   ui.historyPanel.resize();
   ui.dataTableView.resize();
+  if (SchemaAlignment) {
+    SchemaAlignment.resize();
+  }
 }
 
 function initializeUI(uiState) {
