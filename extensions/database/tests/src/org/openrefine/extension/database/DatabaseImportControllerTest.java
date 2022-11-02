267b6fda3dc2942e80dbc0600c71c223ca8b8eb3
Antonin Delpeuch <antonin@delpeuch.eu>
2022-11-02 20:28:06 +0100

    Fix registration of testing datamodel runner in database extension tests

--- extensions/database/tests/src/org/openrefine/extension/database/DatabaseImportControllerTest.java
+++ extensions/database/tests/src/org/openrefine/extension/database/DatabaseImportControllerTest.java
@@ -24,6 +24,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import org.openrefine.ProjectMetadata;
+import org.openrefine.RefineModel;
 import org.openrefine.RefineServlet;
 import org.openrefine.extension.database.mysql.MySQLDatabaseService;
 import org.openrefine.extension.database.stub.RefineDbServletStub;
@@ -62,6 +63,7 @@ public class DatabaseImportControllerTest extends DBExtensionTests {
 
         File dir = DBExtensionTestUtils.createTempDirectory("OR_DBExtension_Test_WorkspaceDir");
         DatamodelRunner runner = new TestingDatamodelRunner();
+        RefineModel.setRunner(runner);
         FileProjectManager.initialize(runner, dir);
 
         servlet = new RefineDbServletStub();
