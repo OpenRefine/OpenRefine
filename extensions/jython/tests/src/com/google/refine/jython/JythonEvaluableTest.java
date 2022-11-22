30abc7b185041493e89985b676f8a19eff4c1ea1
Tom Morris <tfmorris@gmail.com>
2022-11-21 21:05:49 -0500

    Add date handling to Jython.  Fixes #692
    - includes tests for passing dates both directions
    - simplify sequence unwrapping
    - add tests for sequences, ints, floats, strings

--- extensions/jython/tests/src/com/google/refine/jython/JythonEvaluableTest.java
+++ extensions/jython/tests/src/com/google/refine/jython/JythonEvaluableTest.java
@@ -1,6 +1,8 @@
 
 package com.google.refine.jython;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Properties;
 
 import org.python.core.PyObject;
@@ -18,26 +20,29 @@ import com.google.refine.model.Row;
  */
 public class JythonEvaluableTest {
 
+    private Properties createBindings() {
+        Project project = new Project();
+        Row row = new Row(2);
+        row.setCell(0, new Cell("one", null));
+        row.setCell(0, new Cell("1", null));
+        Properties bindings = new Properties();
+        bindings.put("columnName", "number");
+        bindings.put("true", "true");
+        bindings.put("false", "false");
+        bindings.put("rowIndex", "0");
+        bindings.put("value", 1);
+        bindings.put("project", project);
+        bindings.put("call", "number");
+        bindings.put("PI", "3.141592654");
+        bindings.put("cells", new CellTuple(project, row));
+        return bindings;
+    }
+
     // Reproduces the situation when result is a PyObject
     // Version with a test case which only calls the existing evaluate method
     @Test
     public void unwrapPyObjectTest() {
-        Properties props = new Properties();
-        Project project = new Project();
-
-        Row row = new Row(2);
-        row.setCell(0, new Cell("one", null));
-        row.setCell(0, new Cell("1", null));
-
-        props.put("columnName", "number");
-        props.put("true", "true");
-        props.put("false", "false");
-        props.put("rowIndex", "0");
-        props.put("value", 1);
-        props.put("project", project);
-        props.put("call", "number");
-        props.put("PI", "3.141592654");
-        props.put("cells", new CellTuple(project, row));
+        Properties props = createBindings();
         String funcExpression = "class Foo(object):\n" +
                 "    bar = 1\n" +
                 "\n" +
@@ -49,22 +54,7 @@ public class JythonEvaluableTest {
 
     @Test
     public void testJythonConcurrent() {
-        Properties props = new Properties();
-        Project project = new Project();
-
-        Row row = new Row(2);
-        row.setCell(0, new Cell("one", null));
-        row.setCell(0, new Cell("1", null));
-
-        props.put("columnName", "number");
-        props.put("true", "true");
-        props.put("false", "false");
-        props.put("rowIndex", "0");
-        props.put("value", 1);
-        props.put("project", project);
-        props.put("call", "number");
-        props.put("PI", "3.141592654");
-        props.put("cells", new CellTuple(project, row));
+        Properties props = createBindings();
 
         Evaluable eval1 = new JythonEvaluable("a = value\nreturn a * 2");
         Long value1 = (Long) eval1.evaluate(props);
@@ -75,5 +65,72 @@ public class JythonEvaluableTest {
         // repeat same previous test
         Long value2 = (Long) eval1.evaluate(props);
         Assert.assertEquals(value1, value2);
+    }
+
+    @Test
+    public void testJythonDate() {
+        Properties bindings = createBindings();
+        OffsetDateTime expectedDate = OffsetDateTime.of(2024, 10, 2, 12, 30, 30, 0, ZoneOffset.UTC);
+
+        // Test passing a date out from Python (wow, Python 2.7 timezone handling is primitive!)
+        Evaluable evaluable = new JythonEvaluable("from datetime import datetime,timedelta,tzinfo\n" +
+                "\n" +
+                "ZERO = timedelta(0)\n" +
+                "\n" +
+                "class UTC(tzinfo):\n" +
+                "    \"\"\"UTC\"\"\"\n" +
+                "\n" +
+                "    def utcoffset(self, dt):\n" +
+                "        return ZERO\n" +
+                "\n" +
+                "    def tzname(self, dt):\n" +
+                "        return \"UTC\"\n" +
+                "\n" +
+                "    def dst(self, dt):\n" +
+                "        return ZERO\n" +
+                "\n" +
+                "utc = UTC()" +
+                "\n" +
+                "return datetime(2024, 10, 2, 12, 30, 30, tzinfo=utc)");
+        Object result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, expectedDate);
+
+        // Test passing a date in
+        bindings.put("value", expectedDate);
+        evaluable = new JythonEvaluable("import datetime\n" +
+                "return isinstance(value,datetime.datetime)");
+        result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, 1L); // Python's version of true
+
+        evaluable = new JythonEvaluable("import datetime\n" +
+                "return str(value)");
+        result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, "2024-10-02 12:30:30");
+    }
+
+    @Test
+    public void testJythonDataTypes() {
+        Properties bindings = new Properties(); // tests are self-contained, so no bindings needed
+
+        // Test sequences
+        Evaluable evaluable = new JythonEvaluable("return (1,2)");
+        Object result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, new Long[] { 1L, 2L });
+
+        // Test floats
+        evaluable = new JythonEvaluable("return 1.2");
+        result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, 1.2);
+
+        // Test integers
+        evaluable = new JythonEvaluable("return 1");
+        result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, 1L);
+
+        // Test strings
+        evaluable = new JythonEvaluable("return 'foo'");
+        result = evaluable.evaluate(bindings);
+        Assert.assertEquals(result, "foo");
+
     }
 }
