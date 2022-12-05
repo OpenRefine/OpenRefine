OpenRefine/OpenRefine

commit 0258dc1787019103d5180c0bddc6e53c0682951d

fix issue #1383. use the localDateTime for project info

main/src/com/google/refine/ProjectManager.java
@@ -35,10 +35,11 @@ package com.google.refine;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -179,7 +180,7 @@ public abstract class ProjectManager {
             }//FIXME what should be the behaviour if metadata is null? i.e. not found
 
             Project project = getProject(id);
-            if (project != null && metadata != null && metadata.getModified().after(project.getLastSave())) {
+            if (project != null && metadata != null && metadata.getModified().isAfter(project.getLastSave())) {
                 try {
                     saveProject(project);
                 } catch (Exception e) {
@@ -242,7 +243,7 @@ public abstract class ProjectManager {
      */
     protected void saveProjects(boolean allModified) {
         List<SaveRecord> records = new ArrayList<SaveRecord>();
-        Date startTimeOfSave = new Date();
+        LocalDateTime startTimeOfSave = LocalDateTime.now();
         
         synchronized (this) {
             for (long id : _projectsMetadata.keySet()) {
@@ -251,18 +252,18 @@ public abstract class ProjectManager {
 
                 if (project != null) {
                     boolean hasUnsavedChanges =
-                        metadata.getModified().getTime() >= project.getLastSave().getTime();
+                        metadata.getModified().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() >= project.getLastSave().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
                     // We use >= instead of just > to avoid the case where a newly created project
                     // has the same modified and last save times, resulting in the project not getting
                     // saved at all.
 
                     if (hasUnsavedChanges) {
-                        long msecsOverdue = startTimeOfSave.getTime() - project.getLastSave().getTime();
+                        long msecsOverdue = startTimeOfSave.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() - project.getLastSave().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
 
                         records.add(new SaveRecord(project, msecsOverdue));
 
                     } else if (!project.getProcessManager().hasPending()
-                              && startTimeOfSave.getTime() - project.getLastSave().getTime() > PROJECT_FLUSH_DELAY) {
+                              && startTimeOfSave.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() - project.getLastSave().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() > PROJECT_FLUSH_DELAY) {
                         
                         /*
                          *  It's been a while since the project was last saved and it hasn't been
@@ -295,7 +296,8 @@ public abstract class ProjectManager {
 
             for (int i = 0;
                  i < records.size() &&
-                    (allModified || (new Date().getTime() - startTimeOfSave.getTime() < QUICK_SAVE_MAX_TIME));
+                    (allModified || (LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() - 
+                            startTimeOfSave.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() < QUICK_SAVE_MAX_TIME));
                  i++) {
 
                 try {
@@ -318,7 +320,7 @@ public abstract class ProjectManager {
                 ProjectMetadata metadata = getProjectMetadata(id);
                 Project project = _projects.get(id);
                 if (project != null && !project.getProcessManager().hasPending() 
-                        && metadata.getModified().getTime() < project.getLastSave().getTime()) {
+                        && metadata.getModified().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() < project.getLastSave().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()) {
                         _projects.remove(id).dispose();
                 }
             }

main/tests/server/src/com/google/refine/tests/ProjectManagerTests.java
@@ -41,8 +41,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDateTime;
 
 import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
@@ -222,7 +221,7 @@ public class ProjectManagerTests extends RefineTest {
     }
 
     protected void whenProjectGetLastSave(Project proj){
-        Date projectLastSaveDate = new GregorianCalendar(1970,01,02,00,30,00).getTime();
+        LocalDateTime projectLastSaveDate = LocalDateTime.of(1970,01,02,00,30,00);
         when(proj.getLastSave()).thenReturn(projectLastSaveDate);
     }
 
@@ -230,7 +229,7 @@ public class ProjectManagerTests extends RefineTest {
         whenMetadataGetModified(meta, 5*60);
     }
     protected void whenMetadataGetModified(ProjectMetadata meta, int secondsDifference){
-        Date metadataModifiedDate = new GregorianCalendar(1970,01,02,00, 30, secondsDifference).getTime();
+        LocalDateTime metadataModifiedDate = LocalDateTime.of(1970,01,02,00, 30 + secondsDifference);
         when(meta.getModified()).thenReturn(metadataModifiedDate);
     }
 

main/src/com/google/refine/ProjectMetadata.java
@@ -35,7 +35,7 @@ package com.google.refine;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -55,9 +55,9 @@ import com.google.refine.util.JSONUtilities;
 import com.google.refine.util.ParsingUtilities;
 
 public class ProjectMetadata implements Jsonizable {
-    private final Date     _created;
-    private Date           _modified;
-    private Date written = null;
+    private final LocalDateTime     _created;
+    private LocalDateTime           _modified;
+    private LocalDateTime written = null;
     private String         _name = "";
     private String         _password = "";
 
@@ -81,17 +81,17 @@ public class ProjectMetadata implements Jsonizable {
 
     private final static Logger logger = LoggerFactory.getLogger("project_metadata");
 
-    protected ProjectMetadata(Date date) {
+    protected ProjectMetadata(LocalDateTime date) {
         _created = date;
         preparePreferenceStore(_preferenceStore);
     }
 
     public ProjectMetadata() {
-        this(new Date());
+        this(LocalDateTime.now());
         _modified = _created;
     }
 
-    public ProjectMetadata(Date created, Date modified, String name) {
+    public ProjectMetadata(LocalDateTime created, LocalDateTime modified, String name) {
         this(created);
         _modified = modified;
         _name = name;
@@ -103,8 +103,8 @@ public class ProjectMetadata implements Jsonizable {
 
         writer.object();
         writer.key("name"); writer.value(_name);
-        writer.key("created"); writer.value(ParsingUtilities.dateToString(_created));
-        writer.key("modified"); writer.value(ParsingUtilities.dateToString(_modified));
+        writer.key("created"); writer.value(ParsingUtilities.localDateToString(_created));
+        writer.key("modified"); writer.value(ParsingUtilities.localDateToString(_modified));
         writer.key("creator"); writer.value(_creator);
         writer.key("contributors"); writer.value(_contributors);
         writer.key("subject"); writer.value(_subject);
@@ -143,7 +143,7 @@ public class ProjectMetadata implements Jsonizable {
         writer.endObject();
         
         if (isSaveMode(options)) {
-            written = new Date();
+            written = LocalDateTime.now();
         }
     }
     
@@ -158,7 +158,7 @@ public class ProjectMetadata implements Jsonizable {
     }
 
     public boolean isDirty() {
-        return written == null || _modified.after(written);
+        return written == null || _modified.isAfter(written);
     }
     
     public void write(JSONWriter jsonWriter) throws JSONException  {
@@ -181,9 +181,9 @@ public class ProjectMetadata implements Jsonizable {
     
     static public ProjectMetadata loadFromJSON(JSONObject obj) {
         // TODO: Is this correct?  It's using modified date for creation date
-        ProjectMetadata pm = new ProjectMetadata(JSONUtilities.getDate(obj, "modified", new Date()));
+        ProjectMetadata pm = new ProjectMetadata(JSONUtilities.getLocalDate(obj, "modified", LocalDateTime.now()));
 
-        pm._modified = JSONUtilities.getDate(obj, "modified", new Date());
+        pm._modified = JSONUtilities.getLocalDate(obj, "modified", LocalDateTime.now());
         pm._name = JSONUtilities.getString(obj, "name", "<Error recovering project name>");
         pm._password = JSONUtilities.getString(obj, "password", "");
 
@@ -249,7 +249,7 @@ public class ProjectMetadata implements Jsonizable {
             }
         } 
         
-        pm.written = new Date(); // Mark it as not needing writing until modified
+        pm.written = LocalDateTime.now(); // Mark it as not needing writing until modified
         
         return pm;
     }
@@ -259,7 +259,7 @@ public class ProjectMetadata implements Jsonizable {
         // Any project specific preferences?
     }
 
-    public Date getCreated() {
+    public LocalDateTime getCreated() {
         return _created;
     }
 
@@ -305,12 +305,12 @@ public class ProjectMetadata implements Jsonizable {
         return _password;
     }
 
-    public Date getModified() {
+    public  LocalDateTime getModified() {
         return _modified;
     }
 
     public void updateModified() {
-        _modified = new Date();
+        _modified = LocalDateTime.now();
     }
 
     public PreferenceStore getPreferenceStore() {

main/src/com/google/refine/commands/expr/PreviewExpressionCommand.java
@@ -35,8 +35,10 @@ package com.google.refine.commands.expr;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -212,10 +214,10 @@ public class PreviewExpressionCommand extends Command {
                     Calendar c = (Calendar) v;
                     
                     sb.append("[date " + 
-                        ParsingUtilities.dateToString(c.getTime()) +"]");
-                } else if (v instanceof Date) {
+                        ParsingUtilities.dateToString(ZonedDateTime.ofInstant(c.toInstant(), ZoneId.systemDefault())) +"]");
+                } else if (v instanceof LocalDateTime) {
                     sb.append("[date " + 
-                            ParsingUtilities.dateToString((Date) v) +"]");
+                            ParsingUtilities.dateToString((ZonedDateTime) v) +"]");
                 } else if (v instanceof String) {
                     if (quote) {
                         sb.append(JSONObject.quote((String) v));

main/src/com/google/refine/expr/functions/ToDate.java
@@ -35,6 +35,7 @@ package com.google.refine.expr.functions;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -83,7 +84,7 @@ public class ToDate implements Function {
             try {
                 return CalendarParser.parse( o1, (month_first) ? CalendarParser.MM_DD_YY : CalendarParser.DD_MM_YY);
             } catch (CalendarParserException e) {
-                Date d = ParsingUtilities.stringToDate(o1);
+                ZonedDateTime d = ParsingUtilities.stringToDate(o1);
                 if (d != null) {
                     return d;
                 } else {

main/src/com/google/refine/history/HistoryEntry.java
@@ -34,7 +34,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package com.google.refine.history;
 
 import java.io.Writer;
-import java.util.Date;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Properties;
 
 import org.json.JSONException;
@@ -59,7 +60,7 @@ public class HistoryEntry implements Jsonizable {
     final public long   id;
     final public long   projectID;
     final public String description;
-    final public Date   time;
+    final public ZonedDateTime   time;
 
     // the manager (deals with IO systems or databases etc.)
     final public HistoryEntryManager _manager;
@@ -85,11 +86,11 @@ public class HistoryEntry implements Jsonizable {
     }
 
     public HistoryEntry(long id, Project project, String description, AbstractOperation operation, Change change) {
-        this(id,project.id,description,operation,new Date());
+        this(id,project.id,description,operation,ZonedDateTime.now(ZoneId.of("Z")));
         setChange(change);
     }
 
-    protected HistoryEntry(long id, long projectID, String description, AbstractOperation operation, Date time) {
+    protected HistoryEntry(long id, long projectID, String description, AbstractOperation operation, ZonedDateTime time) {
         this.id = id;
         this.projectID = projectID;
         this.description = description;

main/src/com/google/refine/io/ProjectMetadataUtilities.java
@@ -39,7 +39,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -137,7 +139,9 @@ public class ProjectMetadataUtilities {
                     mtime = Math.max(mtime, time);
                 }
             }
-            pm = new ProjectMetadata(new Date(ctime),new Date(mtime), tempName);
+            pm = new ProjectMetadata(LocalDateTime.ofInstant(Instant.ofEpochMilli(ctime), ZoneId.systemDefault()),
+                    LocalDateTime.ofInstant(Instant.ofEpochMilli(mtime), ZoneId.systemDefault()),
+                    tempName);
             logger.error("Partially recovered missing metadata project in directory " + projectDir + " - " + tempName);
         }
         return pm;

main/src/com/google/refine/model/Cell.java
@@ -35,15 +35,16 @@ package com.google.refine.model;
 
 import java.io.Serializable;
 import java.io.Writer;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Properties;
+
+import org.json.JSONException;
+import org.json.JSONWriter;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import org.json.JSONException;
-import org.json.JSONWriter;
 
 import com.google.refine.Jsonizable;
 import com.google.refine.expr.EvalError;
@@ -86,11 +87,11 @@ public class Cell implements HasFields, Jsonizable {
         } else {
             writer.key("v");
             if (value != null) {
-                if (value instanceof Calendar) {
-                    writer.value(ParsingUtilities.dateToString(((Calendar) value).getTime()));
+                if (value instanceof LocalDateTime) {
+                    writer.value(ParsingUtilities.localDateToString((LocalDateTime)value));
                     writer.key("t"); writer.value("date");
-                } else if (value instanceof Date) {
-                    writer.value(ParsingUtilities.dateToString((Date) value));
+                } else if (value instanceof ZonedDateTime) {
+                    writer.value(ParsingUtilities.dateToString((ZonedDateTime) value));
                     writer.key("t"); writer.value("date");
                 } else if (value instanceof Double 
                         && (((Double)value).isNaN() || ((Double)value).isInfinite())) {

commit 661ca8401965f900c7caa992bf6af2db81e6bc32

Clean up tests
- Fix bug introduced in 0258dc1787019103d5180c0bddc6e53c0682951d
  where minutes are being added instead of seconds
- Make test less nosey about internal implementation details of
  ProjectManager

main/src/com/google/refine/ProjectManager.java
@@ -276,14 +276,11 @@ public abstract class ProjectManager {
                 Project project = _projects.get(id); // don't call getProject() as that will load the project.
 
                 if (project != null) {
-                    // These variables are just to avoid calling methods twice and triggering a test failure
-                    // from a test which is peeking at our internals
-                    Instant modified = metadata.getModified();
-                    Instant lastSave = project.getLastSave();
                     // We use after or equals to avoid the case where a newly created project
                     // has the same modified and last save times, resulting in the project not getting
                     // saved at all.
-                    boolean hasUnsavedChanges = modified.isAfter(lastSave) || modified.equals(lastSave);
+                    boolean hasUnsavedChanges = metadata.getModified().isAfter(project.getLastSave())
+                            || metadata.getModified().equals(project.getLastSave());
 
                     if (hasUnsavedChanges) {
                         long msecsOverdue = Duration.between(startTimeOfSave, project.getLastSave()).toMillis();

main/tests/server/src/com/google/refine/ProjectManagerTests.java
@@ -33,15 +33,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package com.google.refine;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-
 import java.time.Instant;
+
+import static org.mockito.Mockito.*;
 
 import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
@@ -57,6 +51,7 @@ import com.google.refine.process.ProcessManager;
 
 public class ProjectManagerTests extends RefineTest {
 
+    private static final Instant BASE_DATE = Instant.parse("1970-01-02T00:30:00Z");
     ProjectManagerStub pm;
     ProjectManagerStub SUT;
     Project project;
@@ -130,7 +125,7 @@ public class ProjectManagerTests extends RefineTest {
 
     @Test
     public void canSaveAllModified() {
-        whenGetSaveTimes(project, metadata); // 5 minute difference
+        whenGetSaveTimes(project, metadata); // 5-minute difference
         registerProject(project, metadata);
 
         // add a second project to the cache
@@ -140,7 +135,7 @@ public class ProjectManagerTests extends RefineTest {
         registerProject(project2, metadata2);
 
         // check that the two projects are not the same
-        Assert.assertFalse(project.id == project2.id);
+        Assert.assertNotEquals(project2.id, project.id);
 
         SUT.save(true);
 
@@ -160,14 +155,13 @@ public class ProjectManagerTests extends RefineTest {
 
         SUT.save(true);
 
-        // FIXME: Why do we care about the internals of the implementation instead of the result?
-        verify(metadata, times(1)).getModified();
-        verify(metadata, times(1)).getTags();
-        verify(project, times(1)).getProcessManager();
-        verify(project, times(2)).getLastSave(); // FIXME: ditto
+        verify(metadata, atLeastOnce()).getModified();
+        verify(metadata, atLeastOnce()).getTags();
+        verify(project, atLeastOnce()).getProcessManager();
+        verify(project, atLeastOnce()).getLastSave();
         verify(project, times(1)).dispose();
         verify(SUT, never()).saveProject(project);
-        Assert.assertEquals(SUT.getProject(0), null);
+        Assert.assertNull(SUT.getProject(0));
         verifyNoMoreInteractions(project);
         verifyNoMoreInteractions(metadata);
 
@@ -228,18 +222,11 @@ public class ProjectManagerTests extends RefineTest {
     }
 
     protected void whenProjectGetLastSave(Project proj) {
-        Instant projectLastSaveDate = Instant.parse("1970-01-02T00:30:00Z");
-        when(proj.getLastSave()).thenReturn(projectLastSaveDate);
-    }
-
-    protected void whenMetadataGetModified(ProjectMetadata meta) {
-        whenMetadataGetModified(meta, 5 * 60);
+        when(proj.getLastSave()).thenReturn(BASE_DATE);
     }
 
     protected void whenMetadataGetModified(ProjectMetadata meta, int secondsDifference) {
-        // FIXME: This has been ported in a bug-compatible fashion, but it's subtracting 30 *minutes*, not seconds
-        Instant metadataModifiedDate = Instant.parse(String.format("1970-01-02T00:%02d:00Z", 30 + secondsDifference));
-        when(meta.getModified()).thenReturn(metadataModifiedDate);
+        when(meta.getModified()).thenReturn(BASE_DATE.plusSeconds(secondsDifference));
     }
 
     protected void verifySaveTimeCompared(int times) {