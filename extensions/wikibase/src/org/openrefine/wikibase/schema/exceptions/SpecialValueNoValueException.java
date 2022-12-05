1f66bdf09efc2beab412cce93001a2c131e26181
Albin Larsson <albin.post@gmail.com>
2022-12-05 07:16:21 +0100

    wikibase: initial somevalue novalue editing support (#5360)
    * wikibase: initial somevalue novalue editing support
    
    * Add test and fix small issues
    
    * Cleanup unused imports
    
    Co-authored-by: Antonin Delpeuch <antonin@delpeuch.eu>

---
+++ extensions/wikibase/src/org/openrefine/wikibase/schema/exceptions/SpecialValueNoValueException.java
@@ -0,0 +1,8 @@
+
+package org.openrefine.wikibase.schema.exceptions;
+
+public class SpecialValueNoValueException extends SkipSchemaExpressionException {
+
+    public SpecialValueNoValueException() {
+    }
+}
