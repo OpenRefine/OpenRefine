9703e4e6c7ed150f029c762c92ecd90d05cf5f4d
Albin Larsson <albin.post@gmail.com>
2023-01-17 10:45:16 +0100

    header: move "OpenRefine" header text from image (#5549)
    side effects:
    
     - minor style changes (acceptable in my opinion)
     - accessibility improvements
     - dropped some absolute positioning
     - fixed a bug causing the slogan to shift from the left during page load
    
    Part of #3017

--- main/webapp/modules/core/about.js
+++ main/webapp/modules/core/about.js
@@ -4,6 +4,5 @@ $("#contributors").text($.i18n('core-about/contributors'));
 $("#definition").text($.i18n('core-about/definition'));
 $("#history-openrefine").html($.i18n('core-about/history' ,'http://www.metaweb.com/' ,'Metaweb Technologies,Inc.', 'http://www.google.com/','Google' ));
 $("#thanks").text($.i18n('core-about/thanks'));
-
-
+$('#app-home-button').attr('title', $.i18n('core-index/navigate-home'));
  
