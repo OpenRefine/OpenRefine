5b99803f7e13cadf9713c847ce7beb4339d78a6f
Antonin Delpeuch <antonin@delpeuch.eu>
2023-01-27 09:17:56 +0100

    Set version to 3.7-beta5

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -65,7 +65,7 @@ import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
 
-    static private String ASSIGNED_VERSION = "3.7-beta4";
+    static private String ASSIGNED_VERSION = "3.7-beta5";
 
     static public String VERSION = "";
     static public String REVISION = "";
