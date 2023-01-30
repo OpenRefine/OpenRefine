c196f687cf2c4f80e29d29c06da55d094525e447
Antonin Delpeuch <antonin@delpeuch.eu>
2023-01-30 10:08:14 +0100

    Small improvements to javadocs

--- modules/refine-model/src/main/java/org/openrefine/process/ProgressReporter.java
+++ modules/refine-model/src/main/java/org/openrefine/process/ProgressReporter.java
@@ -2,10 +2,8 @@
 package org.openrefine.process;
 
 /**
- * Callback to report the progress of some long-running operation to the user. This is passed to the datamodel
- * implementation which periodically calls `reportProgress` with the current percentage of completion of the task.
- * 
- *
+ * Callback to report the progress of some long-running operation to the user. This is passed to the runner which
+ * periodically calls `reportProgress` with the current percentage of completion of the task.
  */
 public interface ProgressReporter {
 
