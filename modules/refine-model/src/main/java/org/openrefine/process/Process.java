b65229680311e07acc3e739bb5b97fddd1734596
Antonin Delpeuch <antonin@delpeuch.eu>
2023-03-22 10:57:35 +0100

    Ignore exceptions thrown by canceled processes

--- modules/refine-model/src/main/java/org/openrefine/process/Process.java
+++ modules/refine-model/src/main/java/org/openrefine/process/Process.java
@@ -72,6 +72,10 @@ public abstract class Process {
         return _thread != null && !_thread.isAlive();
     }
 
+    public boolean isCanceled() {
+        return _canceled;
+    }
+
     public void startPerforming(ProcessManager manager) {
         if (_thread == null) {
             _manager = manager;
