a1b167bb16f210fc701d8bc626999698c88f68c9
Antonin Delpeuch <antonin@delpeuch.eu>
2023-03-29 11:42:38 +0200

    Fix change data fetching bug after manual testing

--- modules/refine-local-runner/src/main/java/org/openrefine/runners/local/pll/TextFilePLL.java
+++ modules/refine-local-runner/src/main/java/org/openrefine/runners/local/pll/TextFilePLL.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.util.*;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipException;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.openrefine.importers.MultiFileReadingProgress;
@@ -192,7 +193,7 @@ public class TextFilePLL extends PLL<String> {
                         if (nextLine == null && lastOffsetSeen > lastOffsetReported) {
                             reportProgress();
                         }
-                    } catch (EOFException e) {
+                    } catch (EOFException | ZipException e) {
                         if (ignoreEarlyEOF) {
                             nextLine = null;
                         } else {
