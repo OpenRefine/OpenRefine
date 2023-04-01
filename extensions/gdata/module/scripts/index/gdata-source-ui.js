9ec508839bba02842a59b4f42f3c7909091a3d68
nish <snish1603@gmail.com>
2023-04-02 00:58:57 +0530

    UI logging out visible

--- extensions/gdata/module/scripts/index/gdata-source-ui.js
+++ extensions/gdata/module/scripts/index/gdata-source-ui.js
@@ -31,16 +31,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
 
-Refine.GDataSourceUI = function(controller) {
+Refine.GDataSourceUI = function (controller) {
   this._controller = controller;
 };
 
-Refine.GDataSourceUI.prototype.attachUI = function(body) {
+Refine.GDataSourceUI.prototype.attachUI = function (body) {
   this._body = body;
-  
+
   this._body.html(DOM.loadHTML("gdata", "scripts/index/import-from-gdata-form.html"));
   this._elmts = DOM.bind(this._body);
-  
+
   $('#gdata-title').text($.i18n('gdata-import/title'));
   $('#gdata-import').html($.i18n('gdata-import/import-by-url'));
   $('#gdata-next').html($.i18n('gdata-import/next->'));
@@ -51,26 +51,26 @@ Refine.GDataSourceUI.prototype.attachUI = function(body) {
   $('#gdata-signout').text($.i18n('gdata-import/sign-out'));
   $('#gdata-resignin').text($.i18n('gdata-import/sign-in'));
   $('#gdata-re-signin-another').text($.i18n('gdata-import/re-sign-in-another'));
-  
+
   var self = this;
-  this._body.find('.gdata-signin.button').on('click',function() {
+  this._body.find('.gdata-signin.button').on('click', function () {
     GdataExtension.showAuthorizationDialog(
-      function() {
+      function () {
         self._listDocuments();
       },
-      function() {
+      function () {
         self._body.find('.gdata-page').hide();
         self._elmts.signinPage.show();
       }
     );
   });
-  this._body.find('.gdata-signout.button').on('click',function() {
-      $.get("command/gdata/deauthorize" );
-      self._body.find('.gdata-page').hide();
-      self._elmts.signinPage.show();
+  this._body.find('.gdata-signout.button').on('click', function () {
+    $.get("command/gdata/deauthorize");
+    self._body.find('.gdata-page').hide();
+    self._elmts.signinPage.show();
   });
-  
-  this._elmts.urlNextButton.on('click',function(evt) {
+
+  this._elmts.urlNextButton.on('click', function (evt) {
     var url = jQueryTrim(self._elmts.urlInput[0].value);
     if (url.length === 0) {
       window.alert($.i18n('gdata-source/alert-url'));
@@ -85,55 +85,55 @@ Refine.GDataSourceUI.prototype.attachUI = function(body) {
       self._controller.startImportingDocument(doc);
     }
   });
-   
+
   this._body.find('.gdata-page').hide();
   this._elmts.signinPage.show();
-  
+
   if (GdataExtension.isAuthorized()) {
     this._listDocuments();
   }
 };
 
-Refine.GDataSourceUI.prototype.focus = function() {
+Refine.GDataSourceUI.prototype.focus = function () {
 };
 
-Refine.GDataSourceUI.prototype._listDocuments = function() {
+Refine.GDataSourceUI.prototype._listDocuments = function () {
   this._body.find('.gdata-page').hide();
   this._elmts.progressPage.show();
-  
+
   var self = this;
-  Refine.wrapCSRF(function(token) {
+  Refine.wrapCSRF(function (token) {
     $.post(
-        "command/core/importing-controller?" + $.param({
+      "command/core/importing-controller?" + $.param({
         "controller": "gdata/gdata-importing-controller",
         "subCommand": "list-documents",
         "csrf_token": token
-        }),
-        null,
-        function(o) {
-          self._renderDocuments(o);
-        },
-        "json"
+      }),
+      null,
+      function (o) {
+        self._renderDocuments(o);
+      },
+      "json"
     );
   });
 };
 
-Refine.GDataSourceUI.prototype._renderDocuments = function(o) {
+Refine.GDataSourceUI.prototype._renderDocuments = function (o) {
   var self = this;
-  
+
   this._elmts.listingContainer.empty();
-  
+
   var table = $(
     '<table><tr>' +
-      '<th></th>' + // starred
-      '<th>'+$.i18n('gdata-source/type')+'</th>' +
-      '<th>'+$.i18n('gdata-source/title')+'</th>' +
-      '<th>'+$.i18n('gdata-source/authors')+'</th>' +
-      '<th>'+$.i18n('gdata-source/updated')+'</th>' +
+    '<th></th>' + // starred
+    '<th>' + $.i18n('gdata-source/type') + '</th>' +
+    '<th>' + $.i18n('gdata-source/title') + '</th>' +
+    '<th>' + $.i18n('gdata-source/authors') + '</th>' +
+    '<th>' + $.i18n('gdata-source/updated') + '</th>' +
     '</tr></table>'
   ).appendTo(this._elmts.listingContainer)[0];
-  
-  var renderDocument = function(doc) {
+
+  var renderDocument = function (doc) {
     var tr = table.insertRow(table.rows.length);
 
     try {
@@ -147,51 +147,51 @@ Refine.GDataSourceUI.prototype._renderDocuments = function(o) {
 
       td = tr.insertCell(tr.cells.length);
       $('<a>')
-          .addClass('gdata-doc-title')
-          .attr('href', 'javascript:{}')
-          .text(doc.title)
-          .appendTo(td)
-          .on('click', function (evt) {
-            self._controller.startImportingDocument(doc);
-          });
+        .addClass('gdata-doc-title')
+        .attr('href', 'javascript:{}')
+        .text(doc.title)
+        .appendTo(td)
+        .on('click', function (evt) {
+          self._controller.startImportingDocument(doc);
+        });
 
       $('<a>')
-          .addClass('gdata-doc-preview')
-          .attr('href', doc.docLink)
-          .attr('target', '_blank')
-          .text('preview')
-          .appendTo(td);
+        .addClass('gdata-doc-preview')
+        .attr('href', doc.docLink)
+        .attr('target', '_blank')
+        .text('preview')
+        .appendTo(td);
 
       td = tr.insertCell(tr.cells.length);
       $('<span>')
-          .addClass('gdata-doc-authors')
-          .text((doc.authors) ? doc.authors.join(', ') : '<unknown>')
-          .appendTo(td);
+        .addClass('gdata-doc-authors')
+        .text((doc.authors) ? doc.authors.join(', ') : '<unknown>')
+        .appendTo(td);
 
       td = tr.insertCell(tr.cells.length);
       $('<span>')
-          .addClass('gdata-doc-date')
-          .text((doc.updated) ? formatRelativeDate(doc.updated) : '<unknown>')
-          .attr('title', (doc.updated) ? doc.updated : '<unknown>')
-          .appendTo(td);
+        .addClass('gdata-doc-date')
+        .text((doc.updated) ? formatRelativeDate(doc.updated) : '<unknown>')
+        .attr('title', (doc.updated) ? doc.updated : '<unknown>')
+        .appendTo(td);
     } catch (e) {
       console.log(e);
-      console.log('Error rendering Google Document "'+doc.title+'". Skipping...');
+      console.log('Error rendering Google Document "' + doc.title + '". Skipping...');
       tr.remove();
     }
   };
-  
+
   if (o.status === 'error') {
     // We're probably not logged in, even though we thought we were. Show signin page
     this._body.find('.gdata-page').hide();
     this._elmts.signinPage.show();
   } else {
     var docs = o.documents;
-    $.each(docs, function() {
+    $.each(docs, function () {
       this.updatedDate = (this.updated) ? new Date(this.updated) : null;
       this.updatedDateTime = (this.updatedDate) ? this.updatedDate.getTime() : 0;
     });
-    docs.sort(function(a, b) { return b.updatedDateTime -  a.updatedDateTime; });
+    docs.sort(function (a, b) { return b.updatedDateTime - a.updatedDateTime; });
 
     for (var i = 0; i < docs.length; i++) {
       renderDocument(docs[i]);
