8261a6c5d26f881e62df03e49f99f790e64c225a
nish <snish1603@gmail.com>
2023-04-02 01:02:37 +0530

    UI logging out visible

--- main/src/com/google/refine/grel/Function.java
+++ main/src/com/google/refine/grel/Function.java
@@ -38,58 +38,7 @@ import java.util.Properties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-package com.google.refine.expr.functions;
 
-import java.util.Properties;
-
-import org.codehaus.jackson.JsonNode;
-
-import com.google.refine.expr.EvalError;
-import com.google.refine.expr.ExpressionUtils;
-import com.google.refine.expr.HasFields;
-import com.google.refine.grel.ControlFunctionRegistry;
-import com.google.refine.grel.Function;
-
-public class GrelFunctions {
-    public static void register(ControlFunctionRegistry registry) {
-        registry.registerFunction(new ForEachItem());
-    }
-
-    private static class ForEachItem implements Function {
-
-        @Override
-        public Object call(Properties bindings, Object[] args) {
-            if (args.length != 3) {
-                return new EvalError("forEachItem: Wrong number of arguments");
-            }
-            Object obj = args[0];
-            String keyName = args[1].toString();
-            String valueName = args[2].toString();
-            if (!(obj instanceof JsonNode)) {
-                return new EvalError("forEachItem: Argument is not a JSON object");
-            }
-            JsonNode json = (JsonNode) obj;
-            if (!json.isObject()) {
-                return new EvalError("forEachItem: Argument is not a JSON object");
-            }
-            for (String fieldName : ExpressionUtils.getFieldsInJsonObject(json)) {
-                JsonNode fieldNode = json.get(fieldName);
-                bindings.put(keyName, fieldName);
-                bindings.put(valueName, fieldNode);
-                if (fieldNode.isObject() && !(fieldNode instanceof HasFields)) {
-                    return new EvalError("forEachItem: Cannot iterate over object that does not implement HasFields");
-                }
-                ExpressionUtils.evaluate(args[2], bindings);
-            }
-            return null;
-        }
-
-        @Override
-        public String getDescription() {
-            return "Iterates over each item in a JSON object, assigning the string value of the key to the key variable and the value for the item to the value variable and then executing the GREL expression.";
-        }
-    }
-}
 /**
  * Interface for functions. When a function is called, its arguments have already been evaluated down into non-error
  * values.
