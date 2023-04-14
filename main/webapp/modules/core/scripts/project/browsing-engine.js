9ec3b037eaf0845e207e2fe8dab0594dbf91b963
elebitzero <dave.lefaive@gmail.com>
2023-04-13 20:16:29 -0700

    Changed it to render facets in general when switching to the facets tab.

--- main/webapp/modules/core/scripts/project/browsing-engine.js
+++ main/webapp/modules/core/scripts/project/browsing-engine.js
@@ -195,11 +195,10 @@ BrowsingEngine.prototype.addFacet = function(type, config, options) {
      let activeTabId = ui.newTab.children('a').attr("href");
      if (activeTabId === '#refine-tabs-facets') {
        for (let facet of this._facets) {
-         if (facet.facet.checkInitialHeight) {
-           facet.facet.checkInitialHeight();
-         }
-         if (facet.facet instanceof RangeFacet) {
+         if (facet.facet.render) {
            facet.facet.render();
+         } else if (facet.facet instanceof ListFacet) {
+           facet.facet.checkInitialHeight();
          }
        }
      }
