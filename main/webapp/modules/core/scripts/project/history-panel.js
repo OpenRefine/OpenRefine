d08584acd700592ae47c13f1321cf46326527ecf
Ijay Abby <inyangasi4@gmail.com>
2023-04-26 17:04:37 +0300

    Fix fileInput select elements

--- main/webapp/modules/core/scripts/project/history-panel.js
+++ main/webapp/modules/core/scripts/project/history-panel.js
@@ -291,8 +291,8 @@ HistoryPanel.prototype._showApplyOperationsDialog = function() {
   elmts.or_proj_pasteJson.html($.i18n('core-project/paste-json'));
 
   elmts.operationJsonButton.on('click', async function() {
-    const fileInput = document.getElementById('file-input');
-    //const fileInput = elmts.file_input.upload_json;
+    //const fileInput = document.getElementById('file-input');
+    const fileInput = elmts.operationJsonButton[0];
     fileInput.accept = '.json';
     fileInput.onchange = async function() {
       const file = fileInput.files[0];
