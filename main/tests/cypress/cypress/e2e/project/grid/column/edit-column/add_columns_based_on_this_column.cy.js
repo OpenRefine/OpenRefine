27dcb37807d814d0c969e437b7bd47478667aa43
Antonin Delpeuch <antonin@delpeuch.eu>
2023-05-04 17:40:15 +0200

    Add cypress test for cancellation of long-running operations

--- main/tests/cypress/cypress/e2e/project/grid/column/edit-column/add_columns_based_on_this_column.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/column/edit-column/add_columns_based_on_this_column.cy.js
@@ -35,9 +35,39 @@ describe(__filename, function () {
     ).should('to.contain', 'butter,with salt');
     cy.confirmDialogPanel();
 
+    // make sure the computation has completed (because this is treated as a long-running operation)
+    cy.get('.column-header-name').contains('Test_Python_toLower').should('exist');
+    cy.get('.data-table td .waiting', { timeout: 10000 }).should('not.exist');
+
     cy.assertCellEquals(0, 'Test_Python_toLower', 'butter,with salt');
     cy.assertCellEquals(1, 'Test_Python_toLower', 'butter,whipped,with salt');
   });
+  it('Cancel a long computation of Python expressions', function () {
+    cy.loadAndVisitProject('food.mini');
+
+    cy.columnActionClick('Shrt_Desc', [
+      'Edit column',
+      'Add column based on this column…',
+    ]);
+    cy.waitForDialogPanel();
+
+    cy.get('input[bind="columnNameInput"]').type('long_computation');
+    cy.get('select[bind="expressionPreviewLanguageSelect"]').select('jython');
+    cy.get('textarea.expression-preview-code').type('from time import sleep\nsleep(10)\nreturn value');
+    cy.confirmDialogPanel();
+
+    // check that the computation is in progress in the grid
+    cy.get('.column-header-name').contains('long_computation').should('exist');
+    cy.get('.data-table td .waiting').should('exist');
+    
+    // cancel the process
+    cy.get('.process-buttons-container button').contains('Cancel').click();
+
+    // there are no pending cells anymore
+    cy.get('.column-header-name').contains('long_computation').should('not.exist');
+    cy.get('.data-table td .waiting').should('not.exist');
+  });
+
   it('Ensure new column is added based on previous column in Clojure', function () {
     cy.loadAndVisitProject('food.mini');
 
@@ -54,6 +84,10 @@ describe(__filename, function () {
       '.expression-preview-table-wrapper tr:nth-child(2) td:last-child'
     ).should('to.contain', 'butter,with salt');
     cy.confirmDialogPanel();
+
+    // make sure the computation has completed (because this is treated as a long-running operation)
+    cy.get('.column-header-name').contains('Test_Clojure_toLower').should('exist');
+    cy.get('.data-table td .waiting', { timeout: 10000 }).should('not.exist');
 
     cy.assertCellEquals(0, 'Test_Clojure_toLower', 'butter,with salt');
     cy.assertCellEquals(1, 'Test_Clojure_toLower', 'butter,whipped,with salt');
