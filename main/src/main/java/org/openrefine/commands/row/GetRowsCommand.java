7a09cbc6165f9227871da8934fa497115e823b1d
Antonin Delpeuch <antonin@delpeuch.eu>
2023-06-19 17:51:01 +0200

    Do not log the get-rows command

--- main/src/main/java/org/openrefine/commands/row/GetRowsCommand.java
+++ main/src/main/java/org/openrefine/commands/row/GetRowsCommand.java
@@ -312,4 +312,9 @@ public class GetRowsCommand extends Command {
 
         respondJSON(response, 200, result);
     }
+
+    @Override
+    public boolean logRequests() {
+        return false;
+    }
 }
