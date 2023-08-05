b0b4c4d493747fd6e4d9a30824bb598fedc9361d
Ayushi Rai <72227873+ayushrai206@users.noreply.github.com>
2023-08-05 19:03:37 +0530

    #1457 fixes the cypress tests

--- main/tests/cypress/cypress/e2e/project/grid/column/reconcile/actions/add_column_with_URLs_of_matched_entities.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/column/reconcile/actions/add_column_with_URLs_of_matched_entities.cy.js
@@ -7,13 +7,12 @@ describe('Add column with URLs of matched entities', () => {
     cy.navigateTo('Import project');
     cy.get('.grid-layout').should('to.contain', 'Locate an existing Refine project file');
     
-    // we're using here the reconciled and mateched project, so rows are reconciled and matched to their best candidate
+    // we're using here the reconciled and matched project, so rows are reconciled and matched to their best candidate
     cy.get('#project-tar-file-input').attachFile('openrefine-demo-sandbox-csv.openrefine.tar.gz.zip')
     cy.get('#import-project-button').click();
     cy.getCell(0, 'Item').should('to.contain', 'Choose new match');
     cy.columnActionClick('Item', [
       'Reconcile',
-      'Actions',
       'Add column with URLs of matched entities',
     ]);
     cy.assertNotificationContainingText(
