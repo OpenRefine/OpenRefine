c778fc5088248abfd8c8289c3185069869f99558
Antonin Delpeuch <antonin@delpeuch.eu>
2023-10-09 16:39:16 +0200

    Revert "Set version to 3.7.6"
    This reverts commit 182a17aac6273454ebe8a78a5878f259aae63e57.

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -65,7 +65,7 @@ import edu.mit.simile.butterfly.ButterflyModule;
 
 public class RefineServlet extends Butterfly {
 
-    static private String ASSIGNED_VERSION = "3.7.6";
+    static private String ASSIGNED_VERSION = "3.7-SNAPSHOT";
 
     static public String VERSION = "";
     static public String REVISION = "";
