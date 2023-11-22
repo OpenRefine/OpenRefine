3a6e187fa23b9ed1597d340e721f25656687266e
Tom Morris <tfmorris@gmail.com>
2023-11-22 14:13:17 -0500

    Address review feedback & save few msec on test

--- main/tests/server/src/com/google/refine/commands/CommandTestBase.java
+++ main/tests/server/src/com/google/refine/commands/CommandTestBase.java
@@ -3,8 +3,8 @@ package com.google.refine.commands;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,9 +13,12 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.testng.annotations.BeforeMethod;
 
 import com.google.refine.RefineTest;
+import com.google.refine.util.ParsingUtilities;
 import com.google.refine.util.TestUtils;
 
 public class CommandTestBase extends RefineTest {
@@ -47,9 +50,10 @@ public class CommandTestBase extends RefineTest {
     /**
      * Convenience method to check that CSRF protection was NOT triggered
      */
-    protected void assertErrorNotCSRF() {
+    protected void assertErrorNotCSRF() throws JsonProcessingException {
         String response = writer.toString();
-        assertTrue(response.contains("\"code\":\"error\""));
+        JsonNode node = ParsingUtilities.mapper.readValue(response, JsonNode.class);
+        assertEquals(node.get("code").toString(), "\"error\"");
         assertFalse(response.contains("Missing or invalid csrf_token parameter"));
     }
 }
