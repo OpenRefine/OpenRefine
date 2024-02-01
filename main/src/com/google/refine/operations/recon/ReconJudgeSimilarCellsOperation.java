3872247337fa0cefac16c8975f3186199092e0e7
Ayushi Rai <ayushirai206@gmail.com>
2024-02-01 22:35:19 +0530

    Update main/src/com/google/refine/operations/recon/ReconJudgeSimilarCellsOperation.java
    Co-authored-by: Antonin Delpeuch <antonin@delpeuch.eu>

--- main/src/com/google/refine/operations/recon/ReconJudgeSimilarCellsOperation.java
+++ main/src/com/google/refine/operations/recon/ReconJudgeSimilarCellsOperation.java
@@ -222,7 +222,7 @@ public class ReconJudgeSimilarCellsOperation extends EngineDependentMassCellOper
                                     recon.judgment = Recon.Judgment.New;
                                     recon.match = null;
                                 } else if (_judgment == Judgment.None) {
-                                    recon.judgment = Recon.Judgment.None;
+                                    recon.judgment = recon.error == null ? Recon.Judgment.None : Recon.Judgment.Error;
                                     recon.match = null;
                                 } else if (_judgment == Judgment.Error) {
                                     throw new IllegalArgumentException("Cannot manually set judgment to 'error'");
