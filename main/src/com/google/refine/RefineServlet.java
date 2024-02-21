7fd4077e0d761c855795c555898c6d4093566ec6
Antonin Delpeuch <antonin@delpeuch.eu>
2024-02-21 19:44:28 +0100

    Set version to 3.8-beta1

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -64,7 +64,7 @@ import com.google.refine.io.FileProjectManager;
 
 public class RefineServlet extends Butterfly {
 
-    static private String ASSIGNED_VERSION = "3.8-SNAPSHOT";
+    static private String ASSIGNED_VERSION = "3.8-beta1";
 
     static public String VERSION = "";
     static public String REVISION = "";
