5fafc6dc24cf8e6686d68968ada7601fd2d69d1e
Steve Kasica <kasica@cs.ubc.ca>
2024-03-18 11:05:50 -0500

    Refactor AddRowsCommand
    Changes AddRowsCommand to extend Command rather than
    EngineDependentCommand for consistency with existing
    core commands.

--- main/src/com/google/refine/commands/row/AddRowsCommand.java
+++ main/src/com/google/refine/commands/row/AddRowsCommand.java
@@ -1,50 +1,77 @@
 
 package com.google.refine.commands.row;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import com.google.refine.browsing.EngineConfig;
-import com.google.refine.commands.EngineDependentCommand;
+import com.google.refine.commands.Command;
 import com.google.refine.model.AbstractOperation;
 import com.google.refine.model.Project;
 import com.google.refine.model.Row;
 import com.google.refine.operations.row.RowAdditionOperation;
+import com.google.refine.process.Process;
 import com.google.refine.util.Pool;
 
-// It's not technically an engine dependent command, but it is easier to write
-// by extending EngineDependentCommand than Command
-public class AddRowsCommand extends EngineDependentCommand {
+public class AddRowsCommand extends Command {
 
     @Override
-    protected AbstractOperation createOperation(Project project,
-            HttpServletRequest request,
-            EngineConfig engineConfig) throws ServletException {
-
-        String rowDataParam = "rows";
-        if (!request.getParameterMap().containsKey(rowDataParam) || request.getParameter(rowDataParam) == null) {
-            throw new ServletException(String.format("Parameter \"%s\" is required", rowDataParam));
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (!hasValidCSRFToken(request)) {
+            respondCSRFError(response);
+            return;
         }
 
-        // TODO: How would I use an existing pool?
-        Pool pool = new Pool();
-        String[] rowData = request.getParameterValues(rowDataParam);
-        List<Row> rows = Arrays.stream(rowData)
-                .map(rowStr -> {
-                    try {
-                        return Row.load(rowStr, pool);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .collect(Collectors.toList());
+        try {
+            Project project = getProject(request);
 
-        return new RowAdditionOperation(rows);
+            String rowDataParam = "rows";
+            if (!request.getParameterMap().containsKey(rowDataParam) || request.getParameter(rowDataParam) == null) {
+                throw new ServletException(String.format("Parameter \"%s\" is required", rowDataParam));
+            }
+            String[] rowData = request.getParameterValues(rowDataParam);
 
+            // TODO: load Rows with existing pool. Current implementation requires users to re-reconcile values
+            //   in a previously reconciled column.
+            Pool pool = new Pool();
+
+            List<Row> rows = Arrays.stream(rowData)
+                    .map(rowStr -> {
+                        try {
+                            return Row.load(rowStr, pool);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toList());
+
+            AbstractOperation op = new RowAdditionOperation(rows);
+            Process process = op.createProcess(project, new Properties());
+
+            performProcessAndRespond(request, response, project, process);
+        } catch (Exception e) {
+            respondException(response, e);
+        }
     }
+
+//    @Override
+//    protected AbstractOperation createOperation(Project project,
+//            HttpServletRequest request,
+//            EngineConfig engineConfig) throws ServletException {
+//
+
+//
+
+//
+//        return
+//
+//    }
 
 }
