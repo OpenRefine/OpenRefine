703f28caebc24232580a696273eb0cd8873334ff
Steve <stephen.kasica@gmail.com>
2024-03-22 12:12:39 -0500

    Update copyright
    Co-authored-by: Tom Morris <tfmorris@gmail.com>

--- main/tests/server/src/com/google/refine/operations/row/RowAdditionOperationTests.java
+++ main/tests/server/src/com/google/refine/operations/row/RowAdditionOperationTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012,2023, OpenRefine contributors
+ * Copyright (C) 2024, OpenRefine contributors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
