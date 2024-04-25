846089a5d893a6e4cdfa67b59a7373e0314c4273
Steve Kasica <kasica@cs.ubc.ca>
2024-04-25 13:36:14 -0500

    Fix bug appending new row in records mode
    Modifies JavaScript to use `theProject.metadata.rowCount` to specify the last
    row index in a project, instead of `theProject.rowModel.total`. Also adds
    Cypress e2e test to catch bug when appending rows in records mode caused by
    this issue.

--- main/tests/cypress/cypress/e2e/project/grid/all-column/add-rows.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/all-column/add-rows.cy.js
@@ -1,20 +1,148 @@
+const jsonValue = `[
+  {
+     "id":"0001",
+     "type":"donut",
+     "name":"Cake",
+     "ppu":0.55,
+     "batters":{
+        "batter":[
+           {
+              "id":"1001",
+              "type":"Regular"
+           },
+           {
+              "id":"1002",
+              "type":"Chocolate"
+           },
+           {
+              "id":"1003",
+              "type":"Blueberry"
+           },
+           {
+              "id":"1004",
+              "type":"Devil's Food"
+           }
+        ]
+     },
+     "topping":[
+        {
+           "id":"5001",
+           "type":"None"
+        },
+        {
+           "id":"5002",
+           "type":"Glazed"
+        },
+        {
+           "id":"5005",
+           "type":"Sugar"
+        },
+        {
+           "id":"5007",
+           "type":"Powdered Sugar"
+        },
+        {
+           "id":"5006",
+           "type":"Chocolate with Sprinkles"
+        },
+        {
+           "id":"5003",
+           "type":"Chocolate"
+        },
+        {
+           "id":"5004",
+           "type":"Maple"
+        }
+     ]
+  },
+  {
+     "id":"0002",
+     "type":"donut",
+     "name":"Raised",
+     "ppu":0.55,
+     "batters":{
+        "batter":[
+           {
+              "id":"1001",
+              "type":"Regular"
+           }
+        ]
+     },
+     "topping":[
+        {
+           "id":"5001",
+           "type":"None"
+        },
+        {
+           "id":"5002",
+           "type":"Glazed"
+        },
+        {
+           "id":"5005",
+           "type":"Sugar"
+        },
+        {
+           "id":"5003",
+           "type":"Chocolate"
+        },
+        {
+           "id":"5004",
+           "type":"Maple"
+        }
+     ]
+  },
+  {
+     "id":"0003",
+     "type":"donut",
+     "name":"Old Fashioned",
+     "ppu":0.55,
+     "batters":{
+        "batter":[
+           {
+              "id":"1001",
+              "type":"Regular"
+           },
+           {
+              "id":"1002",
+              "type":"Chocolate"
+           }
+        ]
+     },
+     "topping":[
+        {
+           "id":"5001",
+           "type":"None"
+        },
+        {
+           "id":"5002",
+           "type":"Glazed"
+        },
+        {
+           "id":"5003",
+           "type":"Chocolate"
+        },
+        {
+           "id":"5004",
+           "type":"Maple"
+        }
+     ]
+  }
+]`;
+
 describe(__filename,  () => {
 
   beforeEach(() => {
-    cy.loadAndVisitProject('food.small');
 
-    cy.get("div#notification > div[bind=undoDiv] > span[bind=undoDescription]")
-      .as("notificationDescription");
-    cy.get("#summary-bar > span")
-      .as("summaryText");
-    cy.get("a#or-proj-undoRedo > span.count")
-      .as("historyCount");
-    cy.get("div#view-panel > div.data-table-container > table > tbody > tr")
-      .as("tableRows");
+
+
 
   });
 
   describe("All menu", () => {
+
+    beforeEach(() => {
+      cy.loadAndVisitProject('food.small');
+    });
 
     it("contains a item called \"Add blank rows\"", () => {
       cy.columnActionClick("All", []);
@@ -68,7 +196,14 @@ describe(__filename,  () => {
       cy.wait(1000);  // wait for response
     }
 
+    beforeEach(() => {
+      cy.loadAndVisitProject('food.small');
+    });
+
     it("displays a notification message", () => {
+      cy.get("div#notification > div[bind=undoDiv] > span[bind=undoDescription]")
+        .as("notificationDescription");
+
       // Verify default state
       cy.get("@notificationDescription")
         .invoke("text")
@@ -82,6 +217,9 @@ describe(__filename,  () => {
     });
 
     it("increments history count by 1", () => {
+      cy.get("a#or-proj-undoRedo > span.count")
+        .as("historyCount");
+
       // Verify default state
       cy.get("@historyCount")
         .invoke("text")
@@ -95,6 +233,9 @@ describe(__filename,  () => {
     });
 
     it("increments row count summary by 1", () => {
+      cy.get("#summary-bar > span")
+        .as("summaryText");
+
       // Verify default state
       cy.get("@summaryText")
         .invoke("text")
@@ -148,6 +289,7 @@ describe(__filename,  () => {
   describe("Modal dialog", () => {
 
     beforeEach(() => {
+      cy.loadAndVisitProject('food.small');
       cy.columnActionClick("All", [ "Add blank rows", "Add multiple rows" ]);
       cy.waitForDialogPanel();
 
@@ -226,6 +368,11 @@ describe(__filename,  () => {
 
     describe("Prepended blank row", () => {
       beforeEach(() => {
+        cy.loadAndVisitProject('food.small');
+
+        cy.get("div#view-panel > div.data-table-container > table > tbody > tr")
+          .as("tableRows");
+
         cy.columnActionClick("All", [ "Add blank rows", "Prepend one row" ]);
         cy.wait(1000);  // Wait 1 second for data to be retrieved
       });
@@ -249,41 +396,103 @@ describe(__filename,  () => {
     });
 
     describe("Appended blank row", () => {
-      beforeEach(() => {
+
+      function appendRow() {
         cy.columnActionClick("All", [ "Add blank rows", "Append one row" ]);
         cy.wait(1000);  // Wait 1 second for data to be retrieved
-      });
+      }
 
-      it("is the last row in the project", () => {
-        cy.get("div#view-panel div.viewpanel-pagesize a.viewPanel-pagingControls-page.action")
-          .last()
-          .click();
-        cy.get("@tableRows")
-          .last()
-          .find("td > div.data-table-cell-content > div > span")
-          .each($span => {
-            expect($span.text()).to.equal("null");
-            expect($span.attr("class")).to.equal("data-table-null");
-            expect($span.css("display")).to.equal("none");
+      function assertRowIsBlank($tr) {
+        $tr.find("td > div.data-table-cell-content > div > span")
+          .each((i, span) => {
+            expect(span.innerText).to.equal("null");
+            expect(span.className).to.equal("data-table-null");
+            expect(span.style.display).to.equal("none");
           });
-      });
+      }
 
-      it("does not modify the first row", () => {
-        cy.get("@tableRows")
-          .first()
-          .find("td > div.data-table-cell-content > div > span")
-          .first()
-          .then($span => {
-            expect($span.text()).to.equal("01001");
-            expect($span.attr("class")).to.not.equal("data-table-null");
-            expect($span.css("display")).to.not.equal("none");
-          })
-      });
+      describe("In row mode", () => {
 
-      it("does not increase the number of visible rows", () => {
-        cy.get("@tableRows")
-          .should("have.length", 10);
-      });
+        beforeEach(() => {
+          cy.loadAndVisitProject('food.small');
+
+          cy.get("div#view-panel > div.data-table-container > table > tbody > tr")
+            .as("tableRows");
+
+          appendRow();
+          // TODO
+          // cy.get("@tableRows")
+          //   .last()
+          //   .find("td > div.data-table-cell-content > div > span")
+          //   .as("lastRow");
+        });
+
+        it("is the last row in the project", () => {
+          cy.get("div#view-panel div.viewpanel-pagesize a.viewPanel-pagingControls-page.action")
+            .last()
+            .click();
+          cy.get("@tableRows")
+            .last()
+            .then(assertRowIsBlank);
+        });
+
+        it("does not modify the first row", () => {
+          cy.get("@tableRows")
+            .first()
+            .find("td > div.data-table-cell-content > div > span")
+            .first()
+            .then($span => {
+              expect($span.text()).to.equal("01001");
+              expect($span.attr("class")).to.not.equal("data-table-null");
+              expect($span.css("display")).to.not.equal("none");
+            })
+        });
+
+        it("does not increase the number of visible rows", () => {
+          cy.get("@tableRows")
+            .should("have.length", 10);
+        });
+
+      }); // end describe data table in row mode
+
+      describe("in records mode", () => {
+        const pricerunner = [
+          ["Cluster Label", "Product Title"],
+          ["Apple iPhone 8 Plus 64GB", "apple iphone 8 plus 64gb silver"],
+          ["Apple iPhone 8 Plus 64GB", "apple iphone 8 plus 64 gb spacegrau"],
+          ["Apple iPhone 7 128GB",     "iphone 7 128gb silver"],
+          ["Apple iPhone 7 128GB",     "iphone 7 128gb silver mn932b/a wc01"]
+        ];
+
+        beforeEach(() => {
+          cy.loadProject(pricerunner, "pricerunner.mini", "fooTag").then(projectId => {
+            cy.visit(`${Cypress.env("OPENREFINE_URL")}/project?project=${projectId}`);
+            cy.waitForProjectTable();
+          });
+
+          cy.get("div#view-panel > div.data-table-container > table > tbody > tr")
+            .as("tableRows");
+
+          // Format data for records mode
+          cy.columnActionClick("All", [ "Edit columns", "Blank down" ]);
+          cy.get('span[bind="modeSelectors"]')
+            .contains('records')
+            .click();
+
+        });
+
+        it("appends a new row in records mode", () => {
+          appendRow();
+          // cy.assertGridEquals(pricerunner.push([null, null]));
+          cy.get("@tableRows")
+            .last()
+            .then(assertRowIsBlank);
+            // .find("td > div.data-table-cell-content > div > span")
+            // .each(expectRowToBeBlank);
+        });
+
+      }); // end describe data table in records
+
     });
   });
 });  // end Cypress tests
