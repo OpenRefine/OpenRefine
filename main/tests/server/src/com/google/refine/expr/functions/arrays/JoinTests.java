aae39587de3159ace55e754449e07482f941d404
Tom Morris <tfmorris@gmail.com>
2024-05-15 12:53:43 -0400

    Lint

--- main/tests/server/src/com/google/refine/expr/functions/arrays/JoinTests.java
+++ main/tests/server/src/com/google/refine/expr/functions/arrays/JoinTests.java
@@ -53,18 +53,18 @@ public class JoinTests extends RefineTest {
         parseEval(bindings, test4);
 
         // Issue 3290 - Make sure leading empty strings don't get skipped
-        parseEval(bindings, new String[] { "['',2,3].join('|')", "|2|3" } );
-        parseEval(bindings, new String[] { "[1,'',3].join('|')", "1||3" } );
-        parseEval(bindings, new String[] { "[1,2,''].join('|')", "1|2|" } );
-        parseEval(bindings, new String[] { "['','',''].join('|')", "||" } );
+        parseEval(bindings, new String[] { "['',2,3].join('|')", "|2|3" });
+        parseEval(bindings, new String[] { "[1,'',3].join('|')", "1||3" });
+        parseEval(bindings, new String[] { "[1,2,''].join('|')", "1|2|" });
+        parseEval(bindings, new String[] { "['','',''].join('|')", "||" });
 
         // Tests for JSON array
         // Both of the following tests throw NPEs. First is desired behavior, second is current array behavior
 //        parseEval(bindings, new String[] { "'[null,null,null]'.parseJson().join('|')", "||" } );
 //        parseEval(bindings, new String[] { "'[null,null,null]'.parseJson().join('|')", "" } );
-        parseEval(bindings, new String[] { "'[\"\",2,3]'.parseJson().join('|')", "|2|3" } );
-        parseEval(bindings, new String[] { "'[1,2,\"\"]'.parseJson().join('|')", "1|2|" } );
-        parseEval(bindings, new String[] { "'[\"\",\"\",\"\"]'.parseJson().join('|')", "||" } );
+        parseEval(bindings, new String[] { "'[\"\",2,3]'.parseJson().join('|')", "|2|3" });
+        parseEval(bindings, new String[] { "'[1,2,\"\"]'.parseJson().join('|')", "1|2|" });
+        parseEval(bindings, new String[] { "'[\"\",\"\",\"\"]'.parseJson().join('|')", "||" });
 
         // TODO: Add tests for List<Object> returned from ExpressionUtils.toObjectList()
     }
