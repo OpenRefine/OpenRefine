423ef725f4f8a0dd61a24f90a1861fc0691dc113
Zyad Taha <158375948+zyadtaha@users.noreply.github.com>
2024-06-14 10:18:07 +0300

    clustering: User defined distances and keyers (#6612)
    * Added new backend classes + unit tests
    
    * Passing the expression to the Keyer or Distance
    
    * Formatted the code
    
    * Registered GREL in tests

---
+++ main/src/com/google/refine/clustering/binning/UserDefinedKeyer.java
@@ -0,0 +1,68 @@
+/*
+
+Copyright 2010, Google Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+    * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package com.google.refine.clustering.binning;
+
+import java.util.Properties;
+
+import com.google.refine.expr.Evaluable;
+import com.google.refine.expr.MetaParser;
+import com.google.refine.expr.ParsingException;
+
+public class UserDefinedKeyer extends Keyer {
+
+    private Evaluable eval;
+    private Properties bindings;
+
+    public UserDefinedKeyer(String expression) throws ParsingException {
+        eval = MetaParser.parse("grel:" + expression);
+        bindings = new Properties();
+
+        bindings.put("true", true);
+        bindings.put("false", false);
+        bindings.put("PI", Math.PI);
+    }
+
+    @Override
+    public String key(String s, Object... o) {
+        if (s == null || o != null && o.length > 0) {
+            throw new IllegalArgumentException("Keying functions accepts a single string parameter");
+        }
+
+        bindings.put("value", s);
+
+        Object result = eval.evaluate(bindings);
+
+        return result.toString();
+    }
+}
