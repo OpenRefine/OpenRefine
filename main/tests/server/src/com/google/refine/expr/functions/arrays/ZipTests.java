5e41f56499acf5a5be122e5414746978a29fa3d9
sunilnatraj <30485122+sunilnatraj@users.noreply.github.com>
2024-08-05 10:08:33 +0530

    Update main/tests/server/src/com/google/refine/expr/functions/arrays/ZipTests.java
    Co-authored-by: Tom Morris <tfmorris@gmail.com>

--- main/tests/server/src/com/google/refine/expr/functions/arrays/ZipTests.java
+++ main/tests/server/src/com/google/refine/expr/functions/arrays/ZipTests.java
@@ -60,10 +60,7 @@ public class ZipTests extends GrelTestBase {
         output.add(Lists.newArrayList(2, 8.90));
         output.add(Lists.newArrayList(3, 9.01));
 
-        List<List> testOutput =  (List<List>) invoke("zip", arg1, arg2);
-        for(int i=0; i < output.size(); ++i) {
-            Assert.assertTrue(testOutput.get(i).equals(output.get(i)));
-        }
+        assertEquals(invoke("zip", arg1, arg2), expected);
     }
 
     @Test
