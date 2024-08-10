770a339587f3e00f12bd937c7c73d741d1fc8325
Thad Guidry <thadguidry@gmail.com>
2024-08-10 16:20:33 +0800

    new alt-view panels and menu & Service launch example - Added boilerplate (from Stackoverflow) to Controller to help figure out launching OpenRefine as Service

--- launcher/src/main/java/org/openrefine/launcher/App.java
+++ launcher/src/main/java/org/openrefine/launcher/App.java
@@ -1,23 +1,28 @@
 package org.openrefine.launcher;
 
 import javafx.application.Application;
+import javafx.concurrent.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("alt-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage.setTitle("OpenRefine Launcher");
         stage.setScene(scene);
         stage.show();
     }
 
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
+
