a4270722a9d6ddd20207f2628adf34745891fc86
Thad Guidry <thadguidry@gmail.com>
2024-08-13 19:31:33 +0800

    Made stopping process more robust with checking. - Also added Confirm close dialog that calls process destroy that should hopefully terminate as CTRL-C did in terminal.

--- launcher/src/main/java/org/openrefine/launcher/Controller.java
+++ launcher/src/main/java/org/openrefine/launcher/Controller.java
@@ -1,23 +1,25 @@
 package org.openrefine.launcher;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import eu.hansolo.tilesfx.Command;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
-import javafx.beans.property.SimpleBooleanProperty;
 
 public class Controller {
 
-    private static final ExecutorService WATCH = Executors.newSingleThreadExecutor(r -> { var t = new Thread(r); t.setDaemon(true); return t; });
+    private static final ExecutorService WATCH = Executors.newSingleThreadExecutor(r -> {
+        var t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    });
     private static SimpleBooleanProperty running = new SimpleBooleanProperty();
     private Process process;
     private Button btStart;
@@ -26,59 +28,80 @@ public class Controller {
     private AnchorPane view;
     private AnchorPane details;
 
-    @FXML Label msgStart;
-    @FXML TextArea outputArea;
-    @FXML TextArea refineOut;
-
-
-    @FXML protected void onStartButtonClick() {
-        outputArea.setText("OpenRefine is starting\n and will launch your browser...");
-
-        // Path to the OpenRefine application we want to start
-
-        String refinePath = "F:/Downloads/openrefine-3.8.2";
-        // String jrePath = refinePath + "/server/target/jre";
-        String classPath = refinePath + "/server/target/lib/*";
-        String mainClass = "com.google.refine.Refine";
-        // String javaAppPathClass = refinePath + "/server/target/lib/openrefine-3.8.2-server.jar " + mainClass;
-        // String javaOptions = "-Djava.library.path=" + refinePath + "/server/target/lib/native/windows";
-
-        ProcessBuilder processBuilder = new ProcessBuilder("java", "-cp", classPath, mainClass);
-
-        try {
-            process = processBuilder.start();
-
-            // Wwe can handle the process's input/output streams here
-            WATCH.submit(() -> {
-                runLater(() -> refineOut.clear());
-                runLater(() -> refineOut.setText("Started OpenRefine:\n\n"));
-                try(BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));) {
-                    String buffer;
-                    while((buffer = br.readLine()) != null) {
-                        final String bufferLine = buffer+"\n";
-                        runLater(() -> refineOut.appendText(bufferLine));
-                    }
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-                runLater(() -> running.set(true));
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-          }
-   }
+    @FXML
+    Label msgStart;
+    @FXML
+    TextArea outputArea;
+    @FXML
+    TextArea refineOut;
 
     @FXML
-    protected void onStopButtonClick() {
-        outputArea.setText("OpenRefine is stopping...");
-        try {
-            boolean alive = process.isAlive();
-            process.destroy();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    protected void onStartButtonClick() {
+        if(process.isAlive()) {
+            this.outputArea.setText("OpenRefine is already running.\n");
+
+        } else {
+
+            outputArea.setText("OpenRefine is starting\n and will launch your browser...\n");
+
+            // Path to the OpenRefine application we want to start
+
+            String refinePath = "F:/Downloads/openrefine-3.8.2";
+            // String jrePath = refinePath + "/server/target/jre";
+            String classPath = refinePath + "/server/target/lib/*";
+            String mainClass = "com.google.refine.Refine";
+            // String javaAppPathClass = refinePath + "/server/target/lib/openrefine-3.8.2-server.jar " + mainClass;
+            // String javaOptions = "-Djava.library.path=" + refinePath + "/server/target/lib/native/windows";
+
+            ProcessBuilder processBuilder = new ProcessBuilder("java", "-cp", classPath, mainClass);
+
+            try {
+                process = processBuilder.start();
+                //TODO: OpenRefine COULD already be running at this point outside of Launcher control
+                // and we need to check the error coming back from the process start?
+                outputArea.appendText("OpenRefine STARTED.\n ");
+
+                // We handle the process's input/output streams here
+                WATCH.submit(() -> {
+                    runLater(() -> refineOut.clear());
+
+                    try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));) {
+                        String buffer;
+                        while ((buffer = br.readLine()) != null) {
+                            final String bufferLine = buffer + "\n";
+                            runLater(() -> refineOut.appendText(bufferLine));
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+//                    runLater(() -> running.set(true));
+
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        finally {
-            outputArea.appendText("OpenRefine has STOPPED.");
+    }
+
+    @FXML
+    public void onStopButtonClick() {
+        if(!(process == null)) {
+            if (process.isAlive()) {
+                this.outputArea.setText("OpenRefine is stopping...\n");
+
+                try {
+                    process.destroy();
+                    outputArea.appendText("OpenRefine STOPPED.\n");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else
+                this.outputArea.setText("""
+                        No running OpenRefine process found.
+                        There might have been an error.
+                        Check Linux `ps` or Windows Task Manager for "OpenJDK openrefine" and kill the PID manually.\n
+                        """
+                );
         }
     }
 
@@ -86,26 +109,32 @@ public class Controller {
     protected void onLogButtonClick() {
         msgStop.setText("Showing Logs...");
     }
+
     @FXML
     protected void search() {
         msgStop.setText("search...");
     }
+
     @FXML
     protected void close() {
         msgStop.setText("close...");
     }
+
     @FXML
     protected void newProxy() {
         msgStop.setText("exampleText1...");
     }
+
     @FXML
     protected void removeProxy() {
         msgStop.setText("exampleText2...");
     }
+
     @FXML
     protected void settings() {
         msgStop.setText("Show Settings/Preferences?...");
     }
+
     @FXML
     protected void aboutBox() {
         msgStop.setText("Showing About...");
@@ -115,61 +144,3 @@ public class Controller {
         Platform.runLater(r::run);
     }
 }
-
-//   TODO:  Maybe instead launch via a TASK ? or SERVICE ?  I think Service() ?
-//.  SEE EXAMPLES:
-//   https://github.com/mynttt/UpdateTool/blob/bf4a21a2a7f63a78a1401f38969a74a37a74a9f0/updatetool-gui/src/main/java/updatetool/gui/App.java
-//
-//
-//    @FXML
-//    private Label refineRunIndicator;
-//
-//    public void  initialize() throws Exception {
-//        refineAccessor = new OpenRefine();
-//
-//        // create executor that uses daemon threads:
-//        exec = Executors.newCachedThreadPool(runnable -> {
-//            Thread t = new Thread(runnable);
-//            t.setDaemon(true);
-//            return t;
-//        });
-//    }
-
-//    @FXML
-//    public class OpenRefineService extends Service<String> {
-//        /**
-//         * Constructor
-//         */
-//        public OpenRefineServiceStatus () {
-//            // if succeeded
-//            setOnSucceeded(s -> {
-//                //code if Service succeeds
-//            });
-//
-//            // if failed
-//            setOnFailed(fail -> {
-//                //code it Service fails
-//            });
-//
-//            //if cancelled
-//            setOnCancelled(cancelled->{
-//                //code if Service get's cancelled
-//            });
-//        }
-//
-//        /**
-//         * This method starts the Service
-//         */
-//        public void startOpenRefineService(){
-//            if(!isRunning()){
-//                //...
-//                // reset();
-//                start();
-//            }
-//
-//        }
-//
-//    }
-
-
-
