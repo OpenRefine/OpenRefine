0a5cdcaa539cc64bb01e6dae351b075158c523ce
Thad Guidry <thadguidry@gmail.com>
2024-08-14 19:40:57 +0800

    begin fixing build issues and adding translations

--- launcher/src/main/java/org/openrefine/launcher/App.java
+++ launcher/src/main/java/org/openrefine/launcher/App.java
@@ -1,27 +1,35 @@
 package org.openrefine.launcher;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Locale userLocale = Locale.getDefault();
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", userLocale);
+
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        stage.setTitle("OpenRefine Launcher");
+        Label titleLabel = new Label(bundle.getString("title"));
+        StackPane root = new StackPane(titleLabel);
+        stage.setTitle(bundle.getString("title"));
         stage.setScene(scene);
-
 
         // FINALLY REALLY START THE APP
         stage.show();
@@ -32,26 +40,26 @@ public class App extends Application {
 
             // Show confirmation dialog
             Alert alert = new Alert(AlertType.CONFIRMATION);
-            alert.setTitle("Confirm");
+            alert.setTitle(bundle.getString("confirm_title"));
             alert.getButtonTypes().clear();
             alert.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-            //Deactivate Defaultbehavior for yes-Button:
+            // Deactivate Defaultbehavior for OK Button:
             Button OK = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
-            OK.setDefaultButton( false );
-            //Activate Defaultbehavior for no-Button:
-            Button CANCEL = (Button) alert.getDialogPane().lookupButton( ButtonType.CANCEL );
-            CANCEL.setDefaultButton( true );
+            OK.setDefaultButton(false);
+            // Activate Defaultbehavior for CANCEL Button:
+            Button CANCEL = (Button) alert.getDialogPane().lookupButton(ButtonType.CANCEL);
+            CANCEL.setDefaultButton(true);
 
-            alert.setHeaderText("Close OpenRefine and save projects");
-            alert.setContentText("Are you sure?");
+            alert.setHeaderText(bundle.getString("confirm_header"));
+            alert.setContentText(bundle.getString("confirm_content"));
 
             // Wait for user response
             Optional<ButtonType> response = alert.showAndWait();
             response.ifPresent(btnType -> {
                 // Gracefully shutdown OpenRefine process through Controller
                 // and close the application if the user confirms
-                ButtonBar.ButtonData confirm = btnType.getButtonData();
-                if(!confirm.isCancelButton()) {
+                ButtonBar.ButtonData confirmBtn = btnType.getButtonData();
+                if (!confirmBtn.isCancelButton()) {
                     final Runnable runnable = () -> new Controller().onStopButtonClick();
                     stage.close();
                 }
