211281b66133be491971bf0a07c286e6ae5488a9
Tom Morris <tfmorris@gmail.com>
2024-09-02 15:32:45 -0400

    DRY up Jython/Clojure selection

--- main/tests/cypress/cypress/e2e/project/grid/column/edit-column/add_columns_based_on_this_column.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/column/edit-column/add_columns_based_on_this_column.cy.js
@@ -30,13 +30,10 @@ describe(__filename, function () {
     cy.get('input[bind="columnNameInput"]').type('Test_Python_toLower');
 
     cy.get('textarea.expression-preview-code').clear()
-    cy.get('select[bind="expressionPreviewLanguageSelect"]').select('jython');
-    // Wait for Jython interpreter to load (as indicated by changed error message)
-    cy.get('.expression-preview-parsing-status').contains('Internal error');
-
+    cy.selectPython();
     cy.typeExpression('return value.lower()');
     cy.get(
-      '.expression-preview-table-wrapper tr:nth-child(2) td:last-child',{timeout: 10000}
+      '.expression-preview-table-wrapper tr:nth-child(2) td:last-child'
     ).should('to.contain', 'butter,with salt');
     cy.confirmDialogPanel();
 
@@ -53,12 +50,7 @@ describe(__filename, function () {
     cy.waitForDialogPanel();
 
     cy.get('input[bind="columnNameInput"]').type('Test_Clojure_toLower');
-
-    cy.get('textarea.expression-preview-code').clear().type('(');
-    cy.get('select[bind="expressionPreviewLanguageSelect"]').select('clojure');
-    // Wait for Clojure interpreter to load (as indicated by changed error message)
-    cy.get('.expression-preview-parsing-status').contains('Syntax error reading source');
-
+    cy.selectClojure();
     cy.typeExpression('(.. value (toLowerCase) )');
     cy.get(
       '.expression-preview-table-wrapper tr:nth-child(2) td:last-child'
