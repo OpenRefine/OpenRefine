23bc0792b66afc6298f38b918101301ce3317926
akashinde <akashvshinde2001@gmail.com>
2024-09-21 13:27:48 +0200

    fixes #6164 remove waitForOrOperation() usage and the helper itself

--- main/tests/cypress/cypress/e2e/project/grid/column/facet/facets.numeric.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/column/facet/facets.numeric.cy.js
@@ -41,20 +41,24 @@ describe(__filename, function () {
     .trigger('mouseup',{ force: true })
     cy.get('#summary-bar').contains('72 matching rows');
 
-    //sliding the middle portion in numeric facet
+    // wait for the numeric facet to be refreshed before next mouse events
+    cy.get('.browsing-panel-indicator').should("not.be.visible");
+
     cy.get('.slider-widget-draggable').eq(0)
-      // FIXME: These are the last two uses of the hated waitForOrOperation
-    .trigger('mousedown',{ force: true }).waitForOrOperation()
+    .trigger('mousedown',{ force: true })
     .trigger('mousemove',130,0,{ force: true })
-    .trigger('mouseup',{ force: true }).waitForOrOperation();
-    cy.get('#summary-bar').contains('70 matching rows');
+    .trigger('mouseup',{ force: true });
+    cy.get('#summary-bar').contains('74 matching rows');
+
+    // wait for the numeric facet to be refreshed before next mouse events
+    cy.get('.browsing-panel-indicator').should("not.be.visible");
 
     //sliding the left slider
     cy.get('.slider-widget-bracket').eq(0)
     .trigger('mousedown',{ force: true })
     .trigger('mousemove',130,0,{ force: true })
     .trigger('mouseup',{ force: true })
-    cy.get('#summary-bar').contains('4 matching rows');
+    cy.get('#summary-bar').contains('3 matching rows');
   });
 
   it('Test for checkboxes and reset button', function () {
