9227cfd30eaed6bfbfb62dfa8c5553ad5850e586
Tom Morris <tfmorris@gmail.com>
2024-10-29 16:08:05 -0400

    Set version to 3.8.5

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -64,7 +64,7 @@ import com.google.refine.io.FileProjectManager;
 
 public class RefineServlet extends Butterfly {
 
-    static private String ASSIGNED_VERSION = "3.8.4";
+    static private String ASSIGNED_VERSION = "3.8.5";
 
     static public String VERSION = "";
     static public String REVISION = "";
