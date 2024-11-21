b1819fcc6e4374e22d1c42ad43eb08503bcd3894
Tom Morris <tfmorris@gmail.com>
2024-11-21 15:45:14 -0500

    Bump version to 3.8.7 to fix unversioned RefineServlet

--- main/src/com/google/refine/RefineServlet.java
+++ main/src/com/google/refine/RefineServlet.java
@@ -64,7 +64,7 @@ import com.google.refine.io.FileProjectManager;
 
 public class RefineServlet extends Butterfly {
 
-    static private String ASSIGNED_VERSION = "3.8.5";
+    static private String ASSIGNED_VERSION = "3.8.7";
 
     static public String VERSION = "";
     static public String REVISION = "";
