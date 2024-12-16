24104bdb662d1c970f640749a13c8f105b6ec873
sunilnatraj <30485122+sunilnatraj@users.noreply.github.com>
2024-12-16 16:46:55 +0530

    Addressed feedback
    - fixed UI styling issues
    - updated unit test

--- main/src/com/google/refine/operations/row/RowDuplicatesRemovalOperation.java
+++ main/src/com/google/refine/operations/row/RowDuplicatesRemovalOperation.java
@@ -1,8 +1,29 @@
-/*
-
-Copyright 2024 OpenRefine
-All rights reserved.
-*/
+/*******************************************************************************
+ * Copyright (C) 2024, OpenRefine contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 
 package com.google.refine.operations.row;
 
@@ -95,26 +116,26 @@ public class RowDuplicatesRemovalOperation extends EngineDependentOperation {
                 // nothing to do
             }
 
+            private boolean isDuplicate(Row row) {
+                List<String> key = criteriaColumns.stream()
+                        .map(col -> normalizeValue(row.getCell(col.getCellIndex())))
+                        .collect(Collectors.toList());
+                int keyHash = key.hashCode();
+
+                if (!rowUniqueKeys.add(keyHash)) {
+                    return true;
+                }
+                return false;
+            }
+
             @Override
             public boolean visit(Project project, int rowIndex, Row row) {
-                if (isDuplicate(row, rowUniqueKeys)) {
+                if (isDuplicate(row)) {
                     rowIndices.add(rowIndex);
                 }
                 return false;
             }
         }.init(rowIndices);
-    }
-
-    private boolean isDuplicate(Row row, HashSet<Object> rowUniqueKeys) {
-        List<String> key = criteriaColumns.stream()
-                .map(col -> normalizeValue(row.getCell(col.getCellIndex())))
-                .collect(Collectors.toList());
-        int keyHash = key.hashCode();
-
-        if (!rowUniqueKeys.add(keyHash)) {
-            return true;
-        }
-        return false;
     }
 
     private static String normalizeValue(Object value) {
--- main/tests/server/src/com/google/refine/commands/row/RemoveDuplicateRowsCommandTest.java
+++ main/tests/server/src/com/google/refine/commands/row/RemoveDuplicateRowsCommandTest.java
@@ -23,185 +23,12 @@ import com.google.refine.operations.row.RowDuplicatesRemovalOperation;
 public class RemoveDuplicateRowsCommandTest extends CommandTestBase {
 
     private Project project = null;
-    private final List<String> duplicateRowCriteria = List.of(
-            new String[] { "SITE_ID", "SITE_NUM", "SITE_NAME", "ACTIVE", "INACTIVE", "AGENCY", "STATE", "COUNTY", "TIME_ZONE", "LATITUDE",
-                    "LONGITUDE", "ELEVATION", "MAPID", "LAND_USE", "TERRAIN", "JSONDATA", "NADP_ID", "NADP_DISTANCE", "UPDATE_DATE" });
-    private final String _engineConfig = "{\"facets\":[{\"type\":\"list\",\"name\":\"SITE_ID\",\"columnName\":\"SITE_ID\",\"expression\":\"value\",\"omitBlank\":false,\"omitError\":false,\"selection\":[{\"v\":{\"v\":\"ABT147\",\"l\":\"ABT147\"}}],\"selectBlank\":false,\"selectError\":false,\"invert\":false}],\"mode\":\"row-based\"}";
-
     protected RemoveDuplicateRowsCommand command;
 
     @BeforeMethod
     public void setUpCommand() {
         MetaParser.registerLanguageParser("grel", "GREL", Parser.grelParser, "value");
         command = new RemoveDuplicateRowsCommand();
-        project = createProject(
-                new String[] { "SITE_ID",
-                        "SITE_NUM",
-                        "SITE_NAME",
-                        "ACTIVE",
-                        "INACTIVE",
-                        "AGENCY",
-                        "STATE",
-                        "COUNTY",
-                        "TIME_ZONE",
-                        "COUNTY",
-                        "LATITUDE",
-                        "LONGITUDE",
-                        "ELEVATION",
-                        "MAPID",
-                        "LAND_USE",
-                        "TERRAIN",
-                        "JSONDATA",
-                        "NADP_ID",
-                        "NADP_DISTANCE",
-                        "UPDATE_DATE" },
-                new Serializable[][] {
-                        { "ABT147",
-                                147,
-                                "Abington",
-                                "domp",
-                                "w",
-                                "EPA",
-                                "CT",
-                                "Windham",
-                                "EA",
-                                41.8402,
-                                -72.01,
-                                209,
-                                "Hampton",
-                                "CT",
-                                "Urban/Agric",
-                                "Rolling",
-                                "{\"\"SITE_ID\"\": \"\"ABT147\"\",\n" +
-                                        "    \"\"SITE_NUM\"\": 147,\n" +
-                                        "    \"\"SITE_NAME\"\": \"\"Abington\"\",\n" +
-                                        "    \"\"ACTIVE\"\": \"\"domp\"\",\n" +
-                                        "    \"\"INACTIVE\"\": \"\"w\"\",\n" +
-                                        "    \"\"AGENCY\"\": \"\"EPA\"\",\n" +
-                                        "    \"\"STATE\"\": \"\"CT\"\",\n" +
-                                        "    \"\"COUNTY\"\": \"\"Windham\"\",\n" +
-                                        "    \"\"TIME_ZONE\"\": \"\"EA\"\",\n" +
-                                        "    \"\"LATITUDE\"\": 41.8402,\n" +
-                                        "    \"\"LONGITUDE\"\": -72.01,\n" +
-                                        "    \"\"ELEVATION\"\": 209,\n" +
-                                        "    \"\"MAPID\"\": \"\"Hampton, CT\"\",\n" +
-                                        "    \"\"LAND_USE\"\": \"\"Urban/Agric\"\",\n" +
-                                        "    \"\"TERRAIN\"\": \"\"Rolling\"\",\n" +
-                                        "    \"\"MLM\"\": \"\"Mixed\"\",\n" +
-                                        "    \"\"NADP_ID\"\": \"\"CT15\"\",\n" +
-                                        "    \"\"NADP_DISTANCE\"\": \"\".0236\"\",\n" +
-                                        "    \"\"UPDATE_DATE\"\": \"\"2004-11-03 08:41:33\"\"}",
-                                "CT15",
-                                .0236,
-                                "2004-11-03 08:41:33" },
-                        { "ABT147",
-                                147,
-                                "Abington",
-                                "domp",
-                                "w",
-                                "EPA",
-                                "CT",
-                                "Windham",
-                                "EA",
-                                41.8402,
-                                -72.01, 209,
-                                "Hampton",
-                                "CT",
-                                "Urban/Agric",
-                                "Rolling",
-                                "{\"\"SITE_ID\"\": \"\"ABT147\"\",\n" +
-                                        "    \"\"SITE_NUM\"\": 147,\n" +
-                                        "    \"\"SITE_NAME\"\": \"\"Abington\"\",\n" +
-                                        "    \"\"ACTIVE\"\": \"\"domp\"\",\n" +
-                                        "    \"\"INACTIVE\"\": \"\"w\"\",\n" +
-                                        "    \"\"AGENCY\"\": \"\"EPA\"\",\n" +
-                                        "    \"\"STATE\"\": \"\"CT\"\",\n" +
-                                        "    \"\"COUNTY\"\": \"\"Windham\"\",\n" +
-                                        "    \"\"TIME_ZONE\"\": \"\"EA\"\",\n" +
-                                        "    \"\"LATITUDE\"\": 41.8402,\n" +
-                                        "    \"\"LONGITUDE\"\": -72.01,\n" +
-                                        "    \"\"ELEVATION\"\": 209,\n" +
-                                        "    \"\"MAPID\"\": \"\"Hampton, CT\"\",\n" +
-                                        "    \"\"LAND_USE\"\": \"\"Urban/Agric\"\",\n" +
-                                        "    \"\"TERRAIN\"\": \"\"Rolling\"\",\n" +
-                                        "    \"\"MLM\"\": \"\"Mixed\"\",\n" +
-                                        "    \"\"NADP_ID\"\": \"\"CT15\"\",\n" +
-                                        "    \"\"NADP_DISTANCE\"\": \"\".0236\"\",\n" +
-                                        "    \"\"UPDATE_DATE\"\": \"\"2004-11-03 08:41:33\"\"}",
-                                "CT15", .0236, "2004-11-03 08:41:33" },
-                        { "ABT147",
-                                147,
-                                "Abington",
-                                "domp",
-                                "w",
-                                "EPA",
-                                "CT",
-                                "Windham",
-                                "EA",
-                                41.8402,
-                                -72.01, 209,
-                                "Hampton",
-                                "CT",
-                                "Urban/Agric",
-                                "Rolling",
-                                "{\"\"SITE_ID\"\": \"\"ABT147\"\",\n" +
-                                        "    \"\"SITE_NUM\"\": 147,\n" +
-                                        "    \"\"SITE_NAME\"\": \"\"Abington\"\",\n" +
-                                        "    \"\"ACTIVE\"\": \"\"domp\"\",\n" +
-                                        "    \"\"INACTIVE\"\": \"\"w\"\",\n" +
-                                        "    \"\"AGENCY\"\": \"\"EPA\"\",\n" +
-                                        "    \"\"STATE\"\": \"\"CT\"\",\n" +
-                                        "    \"\"COUNTY\"\": \"\"Windham\"\",\n" +
-                                        "    \"\"TIME_ZONE\"\": \"\"EA\"\",\n" +
-                                        "    \"\"LATITUDE\"\": 41.8402,\n" +
-                                        "    \"\"LONGITUDE\"\": -72.01,\n" +
-                                        "    \"\"ELEVATION\"\": 209,\n" +
-                                        "    \"\"MAPID\"\": \"\"Hampton, CT\"\",\n" +
-                                        "    \"\"LAND_USE\"\": \"\"Urban/Agric\"\",\n" +
-                                        "    \"\"TERRAIN\"\": \"\"Rolling\"\",\n" +
-                                        "    \"\"MLM\"\": \"\"Mixed\"\",\n" +
-                                        "    \"\"NADP_ID\"\": \"\"CT15\"\",\n" +
-                                        "    \"\"NADP_DISTANCE\"\": \"\".0236\"\",\n" +
-                                        "    \"\"UPDATE_DATE\"\": \"\"2004-11-03 08:41:33\"\"}",
-                                "CT15", .0236, "2004-11-03 08:41:34" },
-                        { "ABT147",
-                                147,
-                                "Abington",
-                                "domp",
-                                "w",
-                                "EPA",
-                                "CT",
-                                "Windham",
-                                "EA",
-                                41.8402,
-                                -72.01, 209,
-                                "Hampton",
-                                "CT",
-                                "Urban/Agric",
-                                "Rolling",
-                                "{\"\"SITE_ID\"\": \"\"ABT147\"\",\n" +
-                                        "    \"\"SITE_NUM\"\": 147,\n" +
-                                        "    \"\"SITE_NAME\"\": \"\"Abington\"\",\n" +
-                                        "    \"\"ACTIVE\"\": \"\"domp\"\",\n" +
-                                        "    \"\"INACTIVE\"\": \"\"w\"\",\n" +
-                                        "    \"\"AGENCY\"\": \"\"EPA\"\",\n" +
-                                        "    \"\"STATE\"\": \"\"CT\"\",\n" +
-                                        "    \"\"COUNTY\"\": \"\"Windham\"\",\n" +
-                                        "    \"\"TIME_ZONE\"\": \"\"EA\"\",\n" +
-                                        "    \"\"LATITUDE\"\": 41.8402,\n" +
-                                        "    \"\"LONGITUDE\"\": -72.01,\n" +
-                                        "    \"\"ELEVATION\"\": 209,\n" +
-                                        "    \"\"MAPID\"\": \"\"Hampton, CT\"\",\n" +
-                                        "    \"\"LAND_USE\"\": \"\"Urban/Agric\"\",\n" +
-                                        "    \"\"TERRAIN\"\": \"\"Rolling\"\",\n" +
-                                        "    \"\"MLM\"\": \"\"Mixed\"\",\n" +
-                                        "    \"\"NADP_ID\"\": \"\"CT15\"\",\n" +
-                                        "    \"\"NADP_DISTANCE\"\": \"\".0236\"\",\n" +
-                                        "    \"\"UPDATE_DATE\"\": \"\"2004-11-03 08:41:34\"\"}",
-                                "CT15", .0236, "2004-11-03 08:41:33" }
-
-                });
-        when(request.getParameter("project")).thenReturn(Long.toString(project.id));
     }
 
     @Test
@@ -212,11 +39,131 @@ public class RemoveDuplicateRowsCommandTest extends CommandTestBase {
     }
 
     @Test
-    public void testRemoveDuplicateRows() throws Exception {
+    public void testWithFacetRemoveDuplicateRows() throws Exception {
+        initProject();
+        List<String> duplicateRowCriteria = List.of(
+                new String[] { "SITE_ID", "SITE_NUM", "LATITUDE",
+                        "LONGITUDE", "ELEVATION", "UPDATE_DATE" });
+        String _engineConfig = "{\"facets\":[{\"type\":\"list\",\"name\":\"SITE_ID\",\"columnName\":\"SITE_ID\",\"expression\":\"value\",\"omitBlank\":false,\"omitError\":false,\"selection\":[{\"v\":{\"v\":\"ABT147\",\"l\":\"ABT147\"}}],\"selectBlank\":false,\"selectError\":false,\"invert\":false}],\"mode\":\"row-based\"}";
+
         EngineConfig engineConfig = EngineConfig.reconstruct(_engineConfig);
         RowDuplicatesRemovalOperation operation = new RowDuplicatesRemovalOperation(engineConfig, duplicateRowCriteria);
         runOperation(operation, project);
-        assertEquals(project.rows.size(), 3);
-        assertEquals(project.history.getLastPastEntries(1).get(0).description, "Remove 1 rows");
+        assertEquals(project.rows.size(), 7);
+        assertEquals(project.history.getLastPastEntries(1).get(0).description, "Remove 2 rows");
+
+        Project expected = createProject(
+                new String[] { "SITE_ID", "SITE_NUM", "SITE_NAME",
+                        "LATITUDE", "LONGITUDE", "ELEVATION", "UPDATE_DATE" },
+                new Serializable[][] {
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:34" },
+                        { "ABT148", 148, "Blighton",
+                                41.8402, -72.01, 209, "2004-11-23 08:41:33" },
+                        { "ABT149", 149, "Coppertown",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT150", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT151", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT152", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" }
+
+                });
+        assertProjectEquals(project, expected);
+    }
+
+    @Test
+    public void testWithSingleCriteriaRemoveDuplicateRows() throws Exception {
+        initProject();
+        List<String> duplicateRowCriteria = List.of(
+                new String[] { "SITE_ID" });
+        String _engineConfig = "{\"facets\":[],\"mode\":\"row-based\"}";
+
+        EngineConfig engineConfig = EngineConfig.reconstruct(_engineConfig);
+        RowDuplicatesRemovalOperation operation = new RowDuplicatesRemovalOperation(engineConfig, duplicateRowCriteria);
+        runOperation(operation, project);
+        assertEquals(project.rows.size(), 6);
+        assertEquals(project.history.getLastPastEntries(1).get(0).description, "Remove 3 rows");
+
+        Project expected = createProject(
+                new String[] { "SITE_ID", "SITE_NUM", "SITE_NAME",
+                        "LATITUDE", "LONGITUDE", "ELEVATION", "UPDATE_DATE" },
+                new Serializable[][] {
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT148", 148, "Blighton",
+                                41.8402, -72.01, 209, "2004-11-23 08:41:33" },
+                        { "ABT149", 149, "Coppertown",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT150", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT151", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT152", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" }
+
+                });
+        assertProjectEquals(project, expected);
+    }
+
+    @Test
+    public void testWithSingleNumberCriteriaRemoveDuplicateRows() throws Exception {
+        initProject();
+        List<String> duplicateRowCriteria = List.of(
+                new String[] { "SITE_NUM" });
+        String _engineConfig = "{\"facets\":[],\"mode\":\"row-based\"}";
+
+        EngineConfig engineConfig = EngineConfig.reconstruct(_engineConfig);
+        RowDuplicatesRemovalOperation operation = new RowDuplicatesRemovalOperation(engineConfig, duplicateRowCriteria);
+        runOperation(operation, project);
+        assertEquals(project.rows.size(), 4);
+        assertEquals(project.history.getLastPastEntries(1).get(0).description, "Remove 5 rows");
+
+        Project expected = createProject(
+                new String[] { "SITE_ID", "SITE_NUM", "SITE_NAME",
+                        "LATITUDE", "LONGITUDE", "ELEVATION", "UPDATE_DATE" },
+                new Serializable[][] {
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT148", 148, "Blighton",
+                                41.8402, -72.01, 209, "2004-11-23 08:41:33" },
+                        { "ABT149", 149, "Coppertown",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT150", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" }
+                });
+
+        assertProjectEquals(project, expected);
+    }
+
+    private void initProject() {
+        project = createProject(
+                new String[] { "SITE_ID", "SITE_NUM", "SITE_NAME",
+                        "LATITUDE", "LONGITUDE", "ELEVATION", "UPDATE_DATE" },
+                new Serializable[][] {
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:34" },
+                        { "ABT147", 147, "Abington",
+                                41.8402, -72.01, 209, "2004-11-03 08:41:33" },
+                        { "ABT148", 148, "Blighton",
+                                41.8402, -72.01, 209, "2004-11-23 08:41:33" },
+                        { "ABT149", 149, "Coppertown",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT150", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT151", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" },
+                        { "ABT152", 150, "Duluth",
+                                41.8402, -72.01, 209, "2004-11-13 08:41:33" }
+
+                });
+        when(request.getParameter("project")).thenReturn(Long.toString(project.id));
     }
 }
