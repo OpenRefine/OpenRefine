5f780b45ffa7fd4c8c1f54c412485849ab8f087d
Antonin Delpeuch <antonin@delpeuch.eu>
2025-01-04 08:22:40 +0100

    ui: Remove usage of table for layout in menu system (#7059)

--- main/webapp/modules/core/scripts/util/menu.js
+++ main/webapp/modules/core/scripts/util/menu.js
@@ -137,14 +137,9 @@ MenuSystem.createAndShowStandardMenu = function(items, elmt, options) {
   var createMenuItem = function(item) {
     if ("label" in item) {
       var menuItem = MenuSystem.createMenuItem().appendTo(menu);
+      menuItem.text(item.label);
       if ("submenu" in item) {
-        menuItem.html(
-          '<table role="presentation" width="100%" cellspacing="0" cellpadding="0" class="menu-item-layout"><tr>' +
-          '<td>' + item.label + '</td>' +
-          '<td width="1%"><img src="images/right-arrow.png" /></td>' +
-          '</tr></table>'
-        );
-
+        menuItem.addClass('submenu');
         menuItem.on('mouseenter click', function () {
         clearTimeout(MenuSystem._hoverTimeout);
         MenuSystem._hoverTimeout = setTimeout(function () {
@@ -162,11 +157,10 @@ MenuSystem.createAndShowStandardMenu = function(items, elmt, options) {
 
       } else {
         if ("download" in item) {
-          menuItem.html(item.label);
           menuItem.attr("href",item.download);
           menuItem.attr("download","");
         } else {
-          menuItem.html(item.label).on('click', function (evt) {
+          menuItem.on('click', function (evt) {
             item.click.call(this, evt);
             MenuSystem.dismissAll();
           });
