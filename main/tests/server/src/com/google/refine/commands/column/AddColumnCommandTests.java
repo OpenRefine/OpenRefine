d447a050c115783ee079fc03417348b7b6e24bbe
amansingh-swe <amansingh03184@gmail.com>
2025-06-09 10:43:55 -0700

    test: add regression test for AddColumnCommand parameter parsing (#7310)
    * test: add regression test for AddColumnCommand parameter parsing
    
    * Made changes according to commit on PR
    1. Made the AddColumnCommandTests extend CommandTestBase.
    2. Optimized imports
    
    * Made changes according to comment on PR.
     1. Removed setUpRequestResponse call from AddColumnCommandTests.

---
+++ main/tests/server/src/com/google/refine/commands/column/AddColumnCommandTests.java
@@ -0,0 +1,55 @@
+
+package com.google.refine.commands.column;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Collections;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.refine.browsing.Engine;
+import com.google.refine.browsing.EngineConfig;
+import com.google.refine.commands.CommandTestBase;
+import com.google.refine.model.AbstractOperation;
+import com.google.refine.model.Project;
+import com.google.refine.operations.OnError;
+import com.google.refine.operations.column.ColumnAdditionOperation;
+
+public class AddColumnCommandTests extends CommandTestBase {
+
+    private AddColumnCommand command;
+    private Project project;
+    private EngineConfig engineConfig;
+
+    @BeforeMethod
+    public void setUpCommand() {
+        command = new AddColumnCommand();
+        project = new Project();
+        engineConfig = new EngineConfig(Collections.emptyList(), Engine.Mode.RowBased);
+    }
+
+    @Test
+    public void testCreateOperationWithValidParameters() throws Exception {
+        when(request.getParameter("baseColumnName")).thenReturn("originalCol");
+        when(request.getParameter("expression")).thenReturn("value + 1");
+        when(request.getParameter("newColumnName")).thenReturn("newCol");
+        when(request.getParameter("columnInsertIndex")).thenReturn("2");
+        when(request.getParameter("onError")).thenReturn("set-to-blank");
+
+        AbstractOperation op = command.createOperation(project, request, engineConfig);
+
+        assertNotNull(op);
+        assertTrue(op instanceof ColumnAdditionOperation);
+
+        ColumnAdditionOperation columnOp = (ColumnAdditionOperation) op;
+        assertEquals(columnOp.getNewColumnName(), "newCol");
+        assertEquals(columnOp.getBaseColumnName(), "originalCol");
+        assertEquals(columnOp.getExpression(), "value + 1");
+        assertEquals(columnOp.getColumnInsertIndex(), 2);
+        assertEquals(columnOp.getOnError(), OnError.SetToBlank);
+    }
+}
