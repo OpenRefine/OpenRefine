72ac22545d1c02f9e764d8f4ddf2728668f76d75
Rory Sawyer <rory@sawyer.dev>
2025-08-14 15:23:02 -0400

    resolve remaining cypress failures

--- main/tests/cypress/cypress/e2e/project/grid/column/edit-column/split_into_several_columns.cy.js
+++ main/tests/cypress/cypress/e2e/project/grid/column/edit-column/split_into_several_columns.cy.js
@@ -17,9 +17,9 @@ describe(__filename, function () {
     );
 
     cy.get('.data-table-header').find('th').should('have.length', 7)
-    cy.get('th:nth-child(3) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 1');
-    cy.get('th:nth-child(4) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 2');
-    cy.get('th:nth-child(5) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 3');
+    cy.get('th:nth-child(3) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 1');
+    cy.get('th:nth-child(4) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 2');
+    cy.get('th:nth-child(5) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 3');
 
     cy.assertCellEquals(0, 'Shrt_Desc 1', 'BUTTER');
     cy.assertCellEquals(0, 'Shrt_Desc 2', 'WITH SALT');
@@ -44,9 +44,9 @@ describe(__filename, function () {
     );
 
     cy.get('.data-table-header').find('th').should('have.length', 8);
-    cy.get('th:nth-child(3) > div.column-header-title > span').should('to.contain', 'Shrt_Desc');
-    cy.get('th:nth-child(5) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 2');
-    cy.get('th:nth-child(6) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 3');
+    cy.get('th:nth-child(3) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc');
+    cy.get('th:nth-child(5) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 2');
+    cy.get('th:nth-child(6) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 3');
 
     cy.assertCellEquals(0, 'Shrt_Desc', 'BUTTER,WITH SALT');
     cy.assertCellEquals(0, 'Shrt_Desc 1', 'BUTTER');
@@ -72,9 +72,9 @@ describe(__filename, function () {
     );
 
     cy.get('.data-table-header').find('th').should('have.length', 7);
-    cy.get('th:nth-child(3) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 1');
-    cy.get('th:nth-child(4) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 2');
-    cy.get('th:nth-child(5) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 3');
+    cy.get('th:nth-child(3) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 1');
+    cy.get('th:nth-child(4) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 2');
+    cy.get('th:nth-child(5) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 3');
 
     cy.assertCellEquals(0, 'Shrt_Desc 1', 'B');
     cy.assertCellEquals(0, 'Shrt_Desc 2', 'UT');
@@ -100,10 +100,10 @@ describe(__filename, function () {
     );
 
     cy.get('.data-table-header').find('th').should('have.length', 9);
-    cy.get('th:nth-child(3) > div.column-header-title > span').should('to.contain', 'Shrt_Desc');
-    cy.get('th:nth-child(5) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 2');
-    cy.get('th:nth-child(6) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 3');
-    cy.get('th:nth-child(7) > div.column-header-title > span').should('to.contain', 'Shrt_Desc 4');
+    cy.get('th:nth-child(3) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc');
+    cy.get('th:nth-child(5) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 2');
+    cy.get('th:nth-child(6) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 3');
+    cy.get('th:nth-child(7) > div.column-header-title > .column-header-name').should('to.contain', 'Shrt_Desc 4');
 
     cy.assertCellEquals(0, 'Shrt_Desc', 'BUTTER,WITH SALT');
     cy.assertCellEquals(0, 'Shrt_Desc 1', 'B');
