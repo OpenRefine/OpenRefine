77e03a51f2e37dcd854d63e3bd14c9e0ad4e48d2
Tom Morris <tfmorris@gmail.com>
2025-09-02 15:35:37 -0400

    Set custom user agent for Wikidata calls. Fixes #7383 (#7412)
    - Set custom user agent on Wikidata Toolkit calls
    - Set custom user agent on raw okhttp calls

---
+++ extensions/wikibase/src/org/openrefine/wikibase/utils/HttpClient.java
@@ -0,0 +1,37 @@
+
+package org.openrefine.wikibase.utils;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import com.google.refine.RefineServlet;
+
+public class HttpClient {
+
+    public static final String USER_AGENT = "OpenRefine-Wikibase-extension/" + RefineServlet.FULL_VERSION
+            + " (https://openrefine.org)";
+
+    public static OkHttpClient getClient() {
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new UserAgentInterceptor(USER_AGENT + " okhttp/unknown")).build();
+        return client;
+    }
+
+    static class UserAgentInterceptor implements Interceptor {
+
+        private final String userAgent;
+
+        public UserAgentInterceptor(String userAgent) {
+            this.userAgent = userAgent;
+        }
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            return chain.proceed(chain.request().newBuilder()
+                    .header("User-Agent", userAgent)
+                    .build());
+        }
+    }
+}
