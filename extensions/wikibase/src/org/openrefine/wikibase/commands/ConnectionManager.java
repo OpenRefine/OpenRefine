15898ce1de55fc1f356f207e792f3b18426f716c
Tom Morris <tfmorris@gmail.com>
2025-09-02 14:11:33 -0400

    Format

--- extensions/wikibase/src/org/openrefine/wikibase/commands/ConnectionManager.java
+++ extensions/wikibase/src/org/openrefine/wikibase/commands/ConnectionManager.java
@@ -34,7 +34,6 @@ import javax.servlet.http.Cookie;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wikidata.wdtk.wikibaseapi.ApiConnection;
