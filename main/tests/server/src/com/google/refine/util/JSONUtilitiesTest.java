ce5a09508a3d3359b6d6cd9aa755011a146d0df0
Dhyan011 <pateldhyan011@gmail.com>
2025-12-06 20:48:19 +0530

    Add unit tests for JSONUtilities.getIntElement safety behavior

---
+++ main/tests/server/src/com/google/refine/util/JSONUtilitiesTest.java
@@ -0,0 +1,41 @@
+package com.google.refine.util;
+
+import static org.testng.Assert.assertEquals;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import org.testng.annotations.Test;
+
+public class JSONUtilitiesTest {
+
+    @Test
+    public void testGetIntElement_NullArray() {
+        int result = JSONUtilities.getIntElement(null, 0, 99);
+        assertEquals(result, 99);
+    }
+
+    @Test
+    public void testGetIntElement_NegativeIndex() {
+        ArrayNode array = JsonNodeFactory.instance.arrayNode();
+        array.add(10);
+        array.add(20);
+        int result = JSONUtilities.getIntElement(array, -1, 99);
+        assertEquals(result, 99);
+    }
+
+    @Test
+    public void testGetIntElement_IndexOutOfBounds() {
+        ArrayNode array = JsonNodeFactory.instance.arrayNode();
+        array.add(10);
+        int result = JSONUtilities.getIntElement(array, 5, 99);
+        assertEquals(result, 99);
+    }
+
+    @Test
+    public void testGetIntElement_ValidIndex() {
+        ArrayNode array = JsonNodeFactory.instance.arrayNode();
+        array.add(42);
+        int result = JSONUtilities.getIntElement(array, 0, 99);
+        assertEquals(result, 42);
+    }
+}
