0da8e70fa2dabe0884995c502705c3d2c4363eb3
Manik Chauhan <manikchauhan029@gmail.com>
2026-02-11 03:59:36 +0530

    Undo whitespace changes and keep deprecation focused

--- extensions/database/module/MOD-INF/controller.js
+++ extensions/database/module/MOD-INF/controller.js
@@ -2,34 +2,34 @@
  * Copyright (c) 2017, Tony Opara
  *        All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
+ * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
- * - Redistributions of source code must retain the above copyright notice, this
+ * - Redistributions of source code must retain the above copyright notice, this 
  *   list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
  *   and/or other materials provided with the distribution.
- *
- * Neither the name of Google nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific
+ * 
+ * Neither the name of Google nor the names of its contributors may be used to 
+ * endorse or promote products derived from this software without specific 
  * prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /*
  * Controller for JDBC Database extension.
- *
+ * 
  * This is run in the Butterfly (ie Refine) server context using the Rhino
  * Javascript interpreter.
  */
@@ -47,7 +47,7 @@ refineServlet = Packages.com.google.refine.RefineServlet;
  * Register our custom commands.
  */
 function registerCommands() {
-
+  
   logger.trace("Registering Database Extension Commands......");
   var RS = Packages.com.google.refine.RefineServlet;
   RS.registerCommand(module, "test-connect", Packages.com.google.refine.extension.database.cmd.TestConnectCommand());
@@ -71,18 +71,18 @@ function registerFunctions() {
  * Function invoked to initialize the extension.
  */
 function init() {
-
+  
   logger.trace("Initializing OpenRefine Database Extension...");
   logger.trace("Database Extension Mount point " + module.getMountPoint());
 
   registerCommands();
   registerOperations();
   registerFunctions();
-
+ 
 
   // Register importer and exporter
   var IM = Packages.com.google.refine.importing.ImportingManager;
-
+  
   IM.registerController(
     module,
     "database-import-controller",
@@ -112,7 +112,7 @@ function init() {
 
     ]
   );
-
+  
   // Script files to inject into /project page
   ClientSideResourceManager.addPaths(
     "project/scripts",
@@ -128,17 +128,17 @@ function init() {
  * Function invoked to handle each request in a custom way.
  */
 function process(path, request, response) {
-
-
+  
+  
    var method = request.getMethod();
-
-   logger.trace('receiving request for ' + path);
-
+    
+   logger.trace('receiving request for ' + path);	
+  
   if (path == "/" || path == "") {
       var context = {};
       context.version = version;
       send(request, response, "index.vt", context);
-  }
+  } 
 }
 
 function send(request, response, template, context) {
